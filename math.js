d2cb5dfc24d97d88779f31dd6f07f08806492fcb
josdejong <wjosdejong@gmail.com>
2013-02-18 21:34:37 +0100

    Added description of the icon

--- math.js
+++ math.js
@@ -4,8 +4,8 @@
  * matrices, strings, and a large set of functions and constants.
  * https://github.com/josdejong/mathjs
  *
- * @version @@version
- * @date    @@date
+ * @version 0.1.0-SNAPSHOT
+ * @date    2013-02-18
  *
  * @license
  * Copyright (C) 2013 Jos de Jong <wjosdejong@gmail.com>
