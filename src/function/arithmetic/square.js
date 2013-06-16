b34b525cf2d5a367fd900bcebc4c3282fc7def32
Oskar Börjesson <o.borjesson@gmail.com>
2013-06-16 18:41:42 +0200

    square implmented

--- src/function/arithmetic/square.js
+++ src/function/arithmetic/square.js
@@ -6,8 +6,8 @@
  *
  * For matrices, the function is evaluated element wise.
  *
- * @param {Number | Complex | Array | Matrix} x
- * @return {Number | Complex | Array | Matrix} res
+ * @param {Number | Complex | Array | Matrix | Unit} x
+ * @return {Number | Complex | Array | Matrix | Unit} res
  */
 math.square = function square(x) {
     if (arguments.length != 1) {
@@ -24,6 +24,16 @@ math.square = function square(x) {
 
     if (x instanceof Array || x instanceof Matrix) {
         return util.map(x, math.square);
+    }
+
+    if(x instanceof Unit){
+        var value = x.value * x.value;
+        var dimensions = math.clone(x.unit.base.dimensions);
+        for (var dim in dimensions){
+            dimensions[dim] *=  2;
+        }
+        return new Unit(value, dimensions);
+
     }
 
     if (x.valueOf() !== x) {
