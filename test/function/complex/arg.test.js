f4e1ad3d6d4925f0308a2a6472343ff58c804014
Sebastien Piquemal <sebpiq@gmail.com>
2013-08-09 13:36:29 +0400

    refactored test/function/complex to mocha

--- test/function/complex/arg.test.js
+++ test/function/complex/arg.test.js
@@ -1,28 +1,45 @@
-// test arg
 var assert = require('assert');
 var math = require('../../../src/index.js');
 
-assert.equal(math.arg(math.complex('0')) / math.pi, 0);
-assert.equal(math.arg(math.complex('1 + 0i')) / math.pi, 0);
-assert.equal(math.arg(math.complex('1 + i')) / math.pi, 0.25);
-assert.equal(math.arg(math.complex('0 + i')) / math.pi, 0.5);
-assert.equal(math.arg(math.complex('-1 + i')) / math.pi, 0.75);
-assert.equal(math.arg(math.complex('-1 + 0i')) / math.pi, 1);
-assert.equal(math.arg(math.complex('-1 - i')) / math.pi, -0.75);
-assert.equal(math.arg(math.complex('0 - i')) / math.pi, -0.5);
-assert.equal(math.arg(math.complex('1 - i')) / math.pi, -0.25);
-assert.equal(math.arg(math.i) / math.pi, 0.5);
-assert.deepEqual(math.divide(math.arg([
-  math.i, math.unary(math.i), math.add(1,math.i)
-]), math.pi), [
-  0.5, -0.5, 0.25
-]);
-assert.deepEqual(math.matrix(math.divide(math.arg([
-  math.i, math.unary(math.i), math.add(1,math.i)
-]), math.pi)).valueOf(), [
-  0.5, -0.5, 0.25
-]);
-assert.equal(math.arg(2) / math.pi, 0);
-assert.equal(math.arg(-2) / math.pi, 1);
-assert.throws(function () {math.arg('string')});
-assert.throws(function () {math.arg(math.unit('5cm'))});
+describe('arg', function() {
+
+  it('should compute the argument of a complex number correctly', function() {
+    assert.equal(math.arg(math.complex('0')) / math.pi, 0);
+    assert.equal(math.arg(math.complex('1 + 0i')) / math.pi, 0);
+    assert.equal(math.arg(math.complex('1 + i')) / math.pi, 0.25);
+    assert.equal(math.arg(math.complex('0 + i')) / math.pi, 0.5);
+    assert.equal(math.arg(math.complex('-1 + i')) / math.pi, 0.75);
+    assert.equal(math.arg(math.complex('-1 + 0i')) / math.pi, 1);
+    assert.equal(math.arg(math.complex('-1 - i')) / math.pi, -0.75);
+    assert.equal(math.arg(math.complex('0 - i')) / math.pi, -0.5);
+    assert.equal(math.arg(math.complex('1 - i')) / math.pi, -0.25);
+    assert.equal(math.arg(math.i) / math.pi, 0.5);
+  });
+
+  it('should calculate the argument for each element in a matrix', function() {
+    assert.deepEqual(math.divide(math.arg([
+      math.i, math.unary(math.i), math.add(1,math.i)
+    ]), math.pi), [
+      0.5, -0.5, 0.25
+    ]);
+    assert.deepEqual(math.matrix(math.divide(math.arg([
+      math.i, math.unary(math.i), math.add(1,math.i)
+    ]), math.pi)).valueOf(), [
+      0.5, -0.5, 0.25
+    ]);
+  });
+
+  it('should compute the argument of a real number correctly', function() {
+    assert.equal(math.arg(2) / math.pi, 0);
+    assert.equal(math.arg(-2) / math.pi, 1);
+  });
+
+  it('should throw an error if used with a string', function() {
+    assert.throws(function () {math.arg('string')});
+  });
+
+  it('should throw an error if used with a unit', function() {
+    assert.throws(function () {math.arg(math.unit('5cm'))});
+  });
+
+});
--- test/function/complex/im.test.js
+++ test/function/complex/im.test.js
@@ -1,12 +1,29 @@
-// test im
 var assert = require('assert');
 var math = require('../../../src/index.js');
 
-assert.equal(math.im(math.complex(2,3)), 3);
-assert.equal(math.im(math.complex(-2,-3)), -3);
-assert.equal(math.im(math.i), 1);
-assert.equal(math.im(2), 0);
-assert.equal(math.im('string'), 0);
-assert.equal(math.im(true), 0);
-assert.deepEqual(math.im([2, math.complex('3-6i')]), [0, -6]);
-assert.deepEqual(math.im(math.matrix([2, math.complex('3-6i')])).valueOf(), [0, -6]);
+describe('im', function() {
+
+  it('should return the imaginary part of a complex number', function() {
+    assert.equal(math.im(math.complex(2,3)), 3);
+    assert.equal(math.im(math.complex(-2,-3)), -3);
+    assert.equal(math.im(math.i), 1);
+  });
+
+  it('should return 0 for a real number', function() {
+    assert.equal(math.im(2), 0);
+  });
+
+  it('should return 0 for a string', function() {
+    assert.equal(math.im('string'), 0);
+  });
+
+  it('should return 0 for a boolean', function() {
+    assert.equal(math.im(true), 0);
+  });
+
+  it('should return the imaginary part for each element in a matrix', function() {
+    assert.deepEqual(math.im([2, math.complex('3-6i')]), [0, -6]);
+    assert.deepEqual(math.im(math.matrix([2, math.complex('3-6i')])).valueOf(), [0, -6]);
+  });
+
+});
