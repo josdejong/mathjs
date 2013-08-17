6174eb00d7b99bbd6dce4d2959b818b9d26b7d8a
Sebastien Piquemal <sebpiq@gmail.com>
2013-08-17 13:42:40 +0400

    refactored test/function/statistics and test/function/trigonometry to mocha

--- test/function/trigonometry/cot.test.js
+++ test/function/trigonometry/cot.test.js
@@ -1,4 +1,3 @@
-// test cot
 var assert = require('assert'),
     math = require('../../../index.js'),
     approx = require('../../../tools/approx.js'),
@@ -8,39 +7,54 @@ var assert = require('assert'),
     unit = math.unit,
     cot = math.cot;
 
-// number
-approx.equal(cot(0), Infinity);
-approx.equal(1 / cot(pi*1/4), 1);
-approx.equal(1 / cot(pi*1/8), 0.414213562373095);
-approx.equal(cot(pi*2/4), 0);
-approx.equal(1 / cot(pi*3/4), -1);
-approx.equal(1 / cot(pi*4/4), 0);
-approx.equal(1 / cot(pi*5/4), 1);
-approx.equal(cot(pi*6/4), 0);
-approx.equal(1 / cot(pi*7/4), -1);
-approx.equal(1 / cot(pi*8/4), 0);
+describe('cot', function() {
 
-// complex
-var re = 0.00373971037633696;
-var im = 0.99675779656935837;
-approx.deepEqual(cot(complex('2+3i')), complex(-re, -im));
-approx.deepEqual(cot(complex('2-3i')), complex(-re, im));
-approx.deepEqual(cot(complex('-2+3i')), complex(re, -im));
-approx.deepEqual(cot(complex('-2-3i')), complex(re, im));
-approx.deepEqual(cot(complex('i')), complex(0, -1.313035285499331));
-approx.deepEqual(cot(complex('1')), complex(0.642092615934331, 0));
-approx.deepEqual(cot(complex('1+i')), complex(0.217621561854403, -0.868014142895925));
+  it('should return the cotan of a number', function() {
+    approx.equal(cot(0), Infinity);
+    approx.equal(1 / cot(pi*1/4), 1);
+    approx.equal(1 / cot(pi*1/8), 0.414213562373095);
+    approx.equal(cot(pi*2/4), 0);
+    approx.equal(1 / cot(pi*3/4), -1);
+    approx.equal(1 / cot(pi*4/4), 0);
+    approx.equal(1 / cot(pi*5/4), 1);
+    approx.equal(cot(pi*6/4), 0);
+    approx.equal(1 / cot(pi*7/4), -1);
+    approx.equal(1 / cot(pi*8/4), 0);
+  });
 
-// unit
-approx.equal(cot(unit('45deg')), 1);
-approx.equal(cot(unit('-45deg')), -1);
-assert.throws(function () {cot(unit('5 celsius'))});
+  it('should return the cotan of a complex number', function() {
+    var re = 0.00373971037633696;
+    var im = 0.99675779656935837;
+    approx.deepEqual(cot(complex('2+3i')), complex(-re, -im));
+    approx.deepEqual(cot(complex('2-3i')), complex(-re, im));
+    approx.deepEqual(cot(complex('-2+3i')), complex(re, -im));
+    approx.deepEqual(cot(complex('-2-3i')), complex(re, im));
+    approx.deepEqual(cot(complex('i')), complex(0, -1.313035285499331));
+    approx.deepEqual(cot(complex('1')), complex(0.642092615934331, 0));
+    approx.deepEqual(cot(complex('1+i')), complex(0.217621561854403, -0.868014142895925));
+  });
 
-// string
-assert.throws(function () {cot('string')});
+  it('should return the cotan of an angle', function() {
+    approx.equal(cot(unit('45deg')), 1);
+    approx.equal(cot(unit('-45deg')), -1);
+  });
 
-// array, matrix, range
-var cot123 = [0.642092615934331, -0.457657554360286, -7.015252551434534];
-approx.deepEqual(cot([1,2,3]), cot123);
-approx.deepEqual(cot(math.range('1:4')), cot123);
-approx.deepEqual(cot(matrix([1,2,3])), matrix(cot123));
+  it('should throw an error if called with an invalid unit', function() {
+    assert.throws(function () {cot(unit('5 celsius'))});
+  });
+
+  it('should throw an error if called with a string', function() {
+    assert.throws(function () {cot('string')});
+  });
+
+  var cot123 = [0.642092615934331, -0.457657554360286, -7.015252551434534];
+
+  it('should return the cotan of each element of an array', function() {
+    approx.deepEqual(cot([1,2,3]), cot123);
+  });
+
+  it('should return the cotan of each element of a matrix', function() {
+    approx.deepEqual(cot(matrix([1,2,3])), matrix(cot123));
+  });
+
+});
--- test/function/trigonometry/tan.test.js
+++ test/function/trigonometry/tan.test.js
@@ -1,4 +1,3 @@
-// test tan
 var assert = require('assert'),
     math = require('../../../index.js'),
     approx = require('../../../tools/approx.js'),
@@ -10,44 +9,54 @@ var assert = require('assert'),
     cos = math.cos,
     tan = math.tan;
 
-// number
-approx.equal(tan(0), 0);
-approx.equal(tan(pi*1/4), 1);
-approx.equal(tan(pi*1/8), 0.414213562373095);
-assert.ok(tan(pi*2/4) > 1e10);
-approx.equal(tan(pi*3/4), -1);
-approx.equal(tan(pi*4/4), 0);
-approx.equal(tan(pi*5/4), 1);
-assert.ok(tan(pi*6/4) > 1e10);
-approx.equal(tan(pi*7/4), -1);
-approx.equal(tan(pi*8/4), 0);
+describe('tan', function() {
 
-// complex
-var re = 0.00376402564150425,
-    im = 1.00323862735360980;
-approx.deepEqual(tan(complex('2+3i')), complex(-re, im));
-approx.deepEqual(tan(complex('2-3i')), complex(-re, -im));
-approx.deepEqual(tan(complex('-2+3i')), complex(re, im));
-approx.deepEqual(tan(complex('-2-3i')), complex(re, -im));
-approx.deepEqual(tan(complex('i')), complex(0, 0.761594155955765));
-approx.deepEqual(tan(complex('1')), complex(1.55740772465490, 0));
-approx.deepEqual(tan(complex('1+i')), complex(0.271752585319512, 1.083923327338695));
+  it('should return the tan of a number', function() {
+    approx.equal(tan(0), 0);
+    approx.equal(tan(pi*1/4), 1);
+    approx.equal(tan(pi*1/8), 0.414213562373095);
+    assert.ok(tan(pi*2/4) > 1e10);
+    approx.equal(tan(pi*3/4), -1);
+    approx.equal(tan(pi*4/4), 0);
+    approx.equal(tan(pi*5/4), 1);
+    assert.ok(tan(pi*6/4) > 1e10);
+    approx.equal(tan(pi*7/4), -1);
+    approx.equal(tan(pi*8/4), 0);
+  });
 
-// unit
-approx.equal(tan(unit(' 60deg')), math.sqrt(3));
-approx.equal(tan(unit('-135deg')), 1);
-assert.throws(function () {tan(unit('5 celsius'))});
+  it('should return the tan of a complex number', function() {
+    var re = 0.00376402564150425,
+        im = 1.00323862735360980;
+    approx.deepEqual(tan(complex('2+3i')), complex(-re, im));
+    approx.deepEqual(tan(complex('2-3i')), complex(-re, -im));
+    approx.deepEqual(tan(complex('-2+3i')), complex(re, im));
+    approx.deepEqual(tan(complex('-2-3i')), complex(re, -im));
+    approx.deepEqual(tan(complex('i')), complex(0, 0.761594155955765));
+    approx.deepEqual(tan(complex('1')), complex(1.55740772465490, 0));
+    approx.deepEqual(tan(complex('1+i')), complex(0.271752585319512, 1.083923327338695));
+  });
 
-// string
-assert.throws(function () {tan('string')});
+  it('should return the tan of an angle', function() {
+    approx.equal(tan(unit(' 60deg')), math.sqrt(3));
+    approx.equal(tan(unit('-135deg')), 1);
+  });
 
-// compare
-math.range(0, 0.1, 1).forEach(function (value) {
-  approx.equal(tan(value), sin(value) / cos(value));
+  it('should throw an error if called with an invalid unit', function() {
+    assert.throws(function () {tan(unit('5 celsius'))});
+  });
+
+  it('should throw an error if called with a string', function() {
+    assert.throws(function () {tan('string')});
+  });
+
+  var tan123 = [1.557407724654902, -2.185039863261519, -0.142546543074278];
+
+  it('should return the tan of each element of an array', function() {
+    approx.deepEqual(tan([1,2,3]), tan123);
+  });
+
+  it('should return the tan of each element of a matrix', function() {
+    approx.deepEqual(tan(matrix([1,2,3])), matrix(tan123));
+  });
+
 });
-
-// array, matrix, range
-var tan123 = [1.557407724654902, -2.185039863261519, -0.142546543074278];
-approx.deepEqual(tan([1,2,3]), tan123);
-approx.deepEqual(tan(math.range('1:4')), tan123);
-approx.deepEqual(tan(matrix([1,2,3])), matrix(tan123));
