e705a99411a81d93adb96a964eeed72a15d71a55
Sebastien Piquemal <sebpiq@gmail.com>
2013-08-17 22:57:13 +0400

    moved the remaining tests to mocha

--- test/contstants.test.js
+++ test/contstants.test.js
@@ -2,37 +2,48 @@ var assert = require('assert'),
     math = require('../index.js'),
     approx = require('../tools/approx.js');
 
-// pi
-approx.equal(math.pi, 3.14159265358979);
-approx.equal(math.sin(math.pi / 2), 1);
-approx.equal(math.PI, math.pi);
-approx.equal(math.eval('pi'), 3.14159265358979);
+describe('constants', function() {
 
-// tau
-approx.equal(math.tau, 6.28318530717959);
-approx.equal(math.eval('tau'), 6.28318530717959);
+  it('should have PI', function() {
+    approx.equal(math.pi, 3.14159265358979);
+    approx.equal(math.sin(math.pi / 2), 1);
+    approx.equal(math.PI, math.pi);
+    approx.equal(math.eval('pi'), 3.14159265358979);
+  });
 
-// e
-approx.equal(math.e, 2.71828182845905);
-approx.equal(math.eval('e'), 2.71828182845905);
+  it('should have tau', function() {
+    approx.equal(math.tau, 6.28318530717959);
+    approx.equal(math.eval('tau'), 6.28318530717959);
+  });
 
-// i
-assert.equal(math.i.re, 0);
-assert.equal(math.i.im, 1);
-assert.deepEqual(math.i, math.complex(0,1));
-assert.deepEqual(math.sqrt(-1), math.i);
-assert.deepEqual(math.eval('i'), math.complex(0, 1));
+  it('should have euler constant', function() {
+    approx.equal(math.e, 2.71828182845905);
+    approx.equal(math.eval('e'), 2.71828182845905);
+    assert.equal(math.round(math.add(1,math.pow(math.e, math.multiply(math.pi, math.i))), 5), 0);
+    assert.equal(math.round(math.eval('1+e^(pi*i)'), 5), 0);
+  });
 
-// test euler 1+e^(pi*i) == 0
-assert.equal(math.round(math.add(1,math.pow(math.e, math.multiply(math.pi, math.i))), 5), 0);
-assert.equal(math.round(math.eval('1+e^(pi*i)'), 5), 0);
+  it('should have i', function() {
+    assert.equal(math.i.re, 0);
+    assert.equal(math.i.im, 1);
+    assert.deepEqual(math.i, math.complex(0,1));
+    assert.deepEqual(math.sqrt(-1), math.i);
+    assert.deepEqual(math.eval('i'), math.complex(0, 1));
+  });
 
-// true, false
-assert.strictEqual(math.true, true);
-assert.strictEqual(math.false, false);
-assert.strictEqual(math.eval('true'), true);
-assert.strictEqual(math.eval('false'), false);
+  it('should have true and false', function() {
+    assert.strictEqual(math.true, true);
+    assert.strictEqual(math.false, false);
+    assert.strictEqual(math.eval('true'), true);
+    assert.strictEqual(math.eval('false'), false);
+  });
 
-// inifinity, NaN
-assert.strictEqual(math.Infinity, Infinity);
-assert.ok(isNaN(math.NaN));
+  it('should have Infinity', function() {
+    assert.strictEqual(math.Infinity, Infinity);
+  });
+
+  it('should have NaN', function() {
+    assert.ok(isNaN(math.NaN));
+  });
+
+});
--- test/type/matrix.test.js
+++ test/type/matrix.test.js
@@ -3,231 +3,279 @@
 var assert = require('assert');
 var math = require('../../index.js');
 
-var m = math.matrix();
-assert.equal(m.isScalar(), true);
-assert.equal(m.isVector(), true);
-assert.deepEqual(m.size(), [0]);
-assert.deepEqual(m.toScalar(), null);
-assert.throws(function () { m.get([-1]); });
+describe('matrix', function() {
 
-m = math.matrix([[23]]);
-assert.equal(m.isScalar(), true);
-assert.equal(m.isVector(), true);
-assert.deepEqual(m.size(), [1,1]);
-assert.deepEqual(m.toScalar(), 23);
-assert.deepEqual(m.get([0,0]), 23);
+  describe('constructor', function() {
 
-m = math.matrix([[1,2,3],[4,5,6]]);
-assert.equal(m.isScalar(), false);
-assert.equal(m.isVector(), false);
-assert.deepEqual(m.size(), [2,3]);
-assert.equal(m.get([1,2]), 6);
-assert.equal(m.get([0,1]), 2);
-assert.throws(function () { m.get([1,2,3]); });
-assert.throws(function () { m.get([3,0]); });
-assert.throws(function () { m.get([1]); });
+    it('should build an emtpy if called with no argument', function() {
+      var m = math.matrix();
+      assert.deepEqual(m.toArray(), []);
+    });
 
-m = math.matrix([1,2,3]);
-assert.equal(m.isScalar(), false);
-assert.equal(m.isVector(), true);
-assert.deepEqual(m.size(), [3]);
-assert.deepEqual(m.toVector(), [1,2,3]);
+  });
 
-m = math.matrix([[1],[2],[3]]);
-assert.equal(m.isScalar(), false);
-assert.equal(m.isVector(), true);
-assert.deepEqual(m.size(), [3,1]);
-assert.deepEqual(m.toVector(), [1,2,3]);
+  describe('isScalar', function() {
 
-m = math.matrix([[[1],[2],[3]]]);
-assert.equal(m.isScalar(), false);
-assert.equal(m.isVector(), true);
-assert.deepEqual(m.size(), [1,3,1]);
-assert.deepEqual(m.toVector(), [1,2,3]);
+    it('should be true for single values', function() {
+      assert.equal(math.matrix([[23]]).isScalar(), true);
+      assert.equal(math.matrix().isScalar(), true);
+      assert.equal(math.matrix([[[3]]]).isScalar(), true);
+      assert.equal(math.matrix([[]]).isScalar(), true);
+    });
 
-m = math.matrix([[[3]]]);
-assert.equal(m.isScalar(), true);
-assert.equal(m.isVector(), true);
-assert.deepEqual(m.size(), [1,1,1]);
-assert.deepEqual(m.toVector(), [3]);
+    it('should be false for matrices', function() {
+      assert.equal(math.matrix([[1,2,3],[4,5,6]]).isScalar(), false);
+      assert.equal(math.matrix([1,2,3]).isScalar(), false);
+      assert.equal(math.matrix([[1],[2],[3]]).isScalar(), false);
+      assert.equal(math.matrix([[[1],[2],[3]]]).isScalar(), false);
+    });
 
-m = math.matrix([[]]);
-assert.equal(m.isScalar(), true);
-assert.equal(m.isVector(), true);
-assert.deepEqual(m.size(), [1,0]);
-assert.deepEqual(m.toVector(), []);
+  });
 
-m = math.matrix();
-assert.equal(m.isScalar(), true);
-assert.equal(m.isVector(), true);
-assert.deepEqual(m.size(), [0]);
-assert.deepEqual(m.toVector(), []);
+  describe('isVector', function() {
 
-// test resizing
-m = math.matrix([[1,2,3],[4,5,6]]);
-assert.deepEqual(m.size(), [2,3]);
-m.resize([2,4]);
-assert.deepEqual(m.size(), [2,4]);
-assert.deepEqual(m.valueOf(), [[1,2,3,0],[4,5,6,0]]);
-m.resize([1,2]);
-assert.deepEqual(m.size(), [1,2]);
-assert.deepEqual(m.valueOf(), [[1,2]]);
-m.resize([1,2,2], 8);
-assert.deepEqual(m.size(), [1,2,2]);
-assert.deepEqual(m.valueOf(), [[[1,8],[2,8]]]);
-m.resize([2,3], 9);
-assert.deepEqual(m.size(), [2,3]);
-assert.deepEqual(m.valueOf(), [[1, 2, 9], [9, 9, 9]]);
-m = math.matrix();
-assert.deepEqual(m.size(), [0]);
-m.resize([3,3,3], 6);
-assert.deepEqual(m.size(), [3,3,3]);
-assert.deepEqual(m.valueOf(), [
-  [[6,6,6],[6,6,6],[6,6,6]],
-  [[6,6,6],[6,6,6],[6,6,6]],
-  [[6,6,6],[6,6,6],[6,6,6]]
-]);
-m.set([2,2,1], 3);
-assert.deepEqual(m.valueOf(), [
-  [[6,6,6],[6,6,6],[6,6,6]],
-  [[6,6,6],[6,6,6],[6,6,6]],
-  [[6,6,6],[6,6,6],[6,3,6]]
-]);
-m.resize([2,2]);
-assert.deepEqual(m.size(), [2,2]);
-assert.deepEqual(m.valueOf(), [[6,6],[6,6]]);
-m.set([1,0], 3);
-assert.deepEqual(m.valueOf(), [[6,6],[3,6]]);
-m.resize([0]);
-assert.deepEqual(m.size(), [0]);
-assert.deepEqual(m.valueOf(), []);
+    it('should be true for vectors', function() {
+      assert.equal(math.matrix().isVector(), true);
+      assert.equal(math.matrix([[23]]).isVector(), true);
+      assert.equal(math.matrix([1,2,3]).isVector(), true);
+      assert.equal(math.matrix([[1],[2],[3]]).isVector(), true);
+      assert.equal(math.matrix([[[1],[2],[3]]]).isVector(), true);
+      assert.equal(math.matrix([[[3]]]).isVector(), true);
+      assert.equal(math.matrix([[]]).isVector(), true);
+      assert.equal(math.matrix().isVector(), true);
+    });
 
-m = math.matrix();
-m.set([1,2], 5);
-assert.deepEqual(m.size(), [2,3]);
-assert.deepEqual(m.valueOf(), [[0,0,0],[0,0,5]]);
+    it('should be false for matrices', function() {
+      assert.equal(math.matrix([[1,2,3],[4,5,6]]).isVector(), false);
+    });
 
-// get 1-dimensional
-m = math.matrix(math.range(0,10));
-assert.deepEqual(m.size(), [10]);
-assert.deepEqual(m.get([[2,5,3,4]]).valueOf(), [2,5,3,4]);
+  });
 
-// get 2-dimensional
-m = math.matrix([[1,2,3],[4,5,6],[7,8,9]]);
-assert.deepEqual(m.size(), [3,3]);
-assert.deepEqual(m.get([1,1]), 5);
-assert.deepEqual(m.get([[0,1],[0,1]]).valueOf(), [[1,2],[4,5]]);
-assert.deepEqual(m.get([[1], math.range(1,3)]).valueOf(), [[5,6]]);
-assert.deepEqual(m.get([0, math.range(1,3)]).valueOf(), [[2,3]]);
-assert.deepEqual(m.get([math.range(1,3), [1]]).valueOf(), [[5],[8]]);
-assert.deepEqual(m.get([math.range(1,3), 2]).valueOf(), [[6],[9]]);
+  describe('size', function() {
 
-// get n-dimensional
-m = math.matrix([[[1,2],[3,4]], [[5,6],[7,8]]]);
-assert.deepEqual(m.size(), [2,2,2]);
-assert.deepEqual(m.get([[0,1],[0,1],[0,1]]).valueOf(), m.valueOf());
-assert.deepEqual(m.get([0,0,0]), 1);
-assert.deepEqual(m.get([[1],[1],[1]]).valueOf(), [[[8]]]);
-assert.deepEqual(m.get([[1],[1],[0,1]]).valueOf(), [[[7,8]]]);
-assert.deepEqual(m.get([[1],[0,1],[1]]).valueOf(), [[[6],[8]]]);
+    it('should return the expected size', function() {
+      assert.deepEqual(math.matrix().size(), [0]);
+      assert.deepEqual(math.matrix([[23]]).size(), [1,1]);
+      assert.deepEqual(math.matrix([[1,2,3],[4,5,6]]).size(), [2,3]);
+      assert.deepEqual(math.matrix([1,2,3]).size(), [3]);
+      assert.deepEqual(math.matrix([[1],[2],[3]]).size(), [3,1]);
+      assert.deepEqual(math.matrix([[[1],[2],[3]]]).size(), [1,3,1]);
+      assert.deepEqual(math.matrix([[[3]]]).size(), [1,1,1]);
+      assert.deepEqual(math.matrix([[]]).size(), [1,0]);
+    });
 
-// set 1-dimensional
-m = math.matrix(math.range(0,7));
-m.set([[2,3]], [20,30]);
-assert.deepEqual(m.size(), [7]);
-assert.deepEqual(m.valueOf(), [0,1,20,30,4,5,6]);
-m.set([4], 40);
-assert.deepEqual(m.size(), [7]);
-assert.deepEqual(m.valueOf(), [0,1,20,30,40,5,6]);
+  });
 
-// set 2-dimensional
-m = math.matrix();
-m.resize([3,3]);
-assert.deepEqual(m.size(), [3,3]);
-assert.deepEqual(m.valueOf(), [[0,0,0],[0,0,0],[0,0,0]]);
-m.set([[1,2], [1,2]], [[1,2],[3,4]]);
-assert.deepEqual(m.size(), [3,3]);
-assert.deepEqual(m.valueOf(), [[0,0,0],[0,1,2],[0,3,4]]);
+  describe('toScalar', function() {
 
-// set 2-dimensional with resize
-m = math.matrix([[123]]);
-assert.deepEqual(m.size(), [1,1]);
-m.set([[1,2], [1,2]], [[1,2],[3,4]]);
-assert.deepEqual(m.size(), [3,3]);
-assert.deepEqual(m.valueOf(), [[123,0,0],[0,1,2],[0,3,4]]);
+    it('should return a scalar if the matrix is a scalar', function() {
+      assert.deepEqual(math.matrix().toScalar(), null);
+      assert.deepEqual(math.matrix([[23]]).toScalar(), 23);
+    });
 
-// set resize dimensions
-m = math.matrix([123]);
-assert.deepEqual(m.size(), [1]);
-m.set([[1,2], [1,2]], [[1,2],[3,4]]);
-assert.deepEqual(m.size(), [3,3]);
-assert.deepEqual(m.valueOf(), [[123,0,0],[0,1,2],[0,3,4]]);
-m.set([math.range(0,2), math.range(0,2)], [[55,55],[55,55]]);
-assert.deepEqual(m.size(), [3,3]);
-assert.deepEqual(m.valueOf(), [[55,55,0],[55,55,2],[0,3,4]]);
+    it('should return null if the matrix is not a scalar', function() {
+      assert.equal(math.matrix([[1,2,3],[4,5,6]]).toScalar(), null);
+    });
 
-m = math.matrix();
-assert.deepEqual(m.size(), [0]);
-m.set([[1,2], [1,2], [1,2]], [[[1,2],[3,4]],[[5,6],[7,8]]]);
-assert.deepEqual(m.size(), [3,3,3]);
-assert.deepEqual(m.valueOf(), [
-  [
-    [0,0,0],
-    [0,0,0],
-    [0,0,0]
-  ],
-  [
-    [0,0,0],
-    [0,1,2],
-    [0,3,4]
-  ],
-  [
-    [0,0,0],
-    [0,5,6],
-    [0,7,8]
-  ]
-]);
+  });
+
+  describe('toVector', function() {
+
+    it('should return a vector if the matrix is a vector', function() {
+      assert.deepEqual(math.matrix([1,2,3]).toVector(), [1,2,3]);
+      assert.deepEqual(math.matrix([[1],[2],[3]]).toVector(), [1,2,3]);
+      assert.deepEqual(math.matrix([[[1],[2],[3]]]).toVector(), [1,2,3]);
+      assert.deepEqual(math.matrix([[[3]]]).toVector(), [3]);
+      assert.deepEqual(math.matrix([[]]).toVector(), []);
+      assert.deepEqual(math.matrix().toVector(), []);
+    });
+
+    it('should return null if the matrix is not a vector', function() {
+      assert.deepEqual(math.matrix([[1,2,3],[4,5,6]]).toVector(), null);
+    });
+
+  });
+
+  describe('resize', function() {
+
+    it('should resize the matrix correctly', function() {
+      var m = math.matrix([[1,2,3],[4,5,6]]);
+      m.resize([2,4]);
+      assert.deepEqual(m.valueOf(), [[1,2,3,0],[4,5,6,0]]);
+
+      m.resize([1,2]);
+      assert.deepEqual(m.valueOf(), [[1,2]]);
+
+      m.resize([1,2,2], 8);
+      assert.deepEqual(m.valueOf(), [[[1,8],[2,8]]]);
+
+      m.resize([2,3], 9);
+      assert.deepEqual(m.valueOf(), [[1, 2, 9], [9, 9, 9]]);
+
+      m = math.matrix();
+      m.resize([3,3,3], 6);
+      assert.deepEqual(m.valueOf(), [
+        [[6,6,6],[6,6,6],[6,6,6]],
+        [[6,6,6],[6,6,6],[6,6,6]],
+        [[6,6,6],[6,6,6],[6,6,6]]
+      ]);
+
+      m.resize([2,2]);
+      assert.deepEqual(m.valueOf(), [[6,6],[6,6]]);
+
+      m.resize([0]);
+      assert.deepEqual(m.valueOf(), []);
+    });
+
+  });
+
+  describe('get', function() {
+
+    it('should get the right subset of the matrix', function() {
+      // get 1-dimensional
+      var m = math.matrix([0, 1, 2, 3, 4, 5, 6, 7, 8, 9]);
+      assert.deepEqual(m.get([[2,5,3,4]]).valueOf(), [2,5,3,4]);
+
+      // get 2-dimensional
+      m = math.matrix([[1,2,3],[4,5,6],[7,8,9]]);
+      assert.deepEqual(m.get([1,1]), 5);
+      assert.deepEqual(m.get([[0,1],[0,1]]).valueOf(), [[1,2],[4,5]]);
+      assert.deepEqual(m.get([[1], math.range(1,3)]).valueOf(), [[5,6]]);
+      assert.deepEqual(m.get([0, math.range(1,3)]).valueOf(), [[2,3]]);
+      assert.deepEqual(m.get([math.range(1,3), [1]]).valueOf(), [[5],[8]]);
+      assert.deepEqual(m.get([math.range(1,3), 2]).valueOf(), [[6],[9]]);
+
+      // get n-dimensional
+      m = math.matrix([[[1,2],[3,4]], [[5,6],[7,8]]]);
+      assert.deepEqual(m.get([[0,1],[0,1],[0,1]]).valueOf(), m.valueOf());
+      assert.deepEqual(m.get([0,0,0]), 1);
+      assert.deepEqual(m.get([[1],[1],[1]]).valueOf(), [[[8]]]);
+      assert.deepEqual(m.get([[1],[1],[0,1]]).valueOf(), [[[7,8]]]);
+      assert.deepEqual(m.get([[1],[0,1],[1]]).valueOf(), [[[6],[8]]]);
+    });
+
+    it('should throw an error if the given subset is invalid', function() {
+      var m = math.matrix();
+      assert.throws(function () { m.get([-1]); });
+
+      var m = math.matrix([[1,2,3],[4,5,6]]);
+      assert.throws(function () { m.get([1,2,3]); });
+      assert.throws(function () { m.get([3,0]); });
+      assert.throws(function () { m.get([1]); });
+    });
+
+  });
+
+  describe('set', function() {
+
+    it('should set the given subset', function() {
+      // set 1-dimensional
+      m = math.matrix(math.range(0,7));
+      m.set([[2,3]], [20,30]);
+      assert.deepEqual(m.valueOf(), [0,1,20,30,4,5,6]);
+      m.set([4], 40);
+      assert.deepEqual(m.valueOf(), [0,1,20,30,40,5,6]);
+
+      // set 2-dimensional
+      m = math.matrix();
+      m.resize([3,3]);
+      assert.deepEqual(m.valueOf(), [[0,0,0],[0,0,0],[0,0,0]]);
+      m.set([[1,2], [1,2]], [[1,2],[3,4]]);
+      assert.deepEqual(m.valueOf(), [[0,0,0],[0,1,2],[0,3,4]]); 
+    });
+
+    it('should resize the matrix if the replacement subset is different size than selected subset', function() {
+      // set 2-dimensional with resize
+      m = math.matrix([[123]]);
+      m.set([[1,2], [1,2]], [[1,2],[3,4]]);
+      assert.deepEqual(m.valueOf(), [[123,0,0],[0,1,2],[0,3,4]]);
+
+      // set resize dimensions
+      m = math.matrix([123]);
+      assert.deepEqual(m.size(), [1]);
+
+      m.set([[1,2], [1,2]], [[1,2],[3,4]]);
+      assert.deepEqual(m.valueOf(), [[123,0,0],[0,1,2],[0,3,4]]);
+
+      m.set([math.range(0,2), math.range(0,2)], [[55,55],[55,55]]);
+      assert.deepEqual(m.valueOf(), [[55,55,0],[55,55,2],[0,3,4]]);
+
+      m = math.matrix();
+      m.set([[1,2], [1,2], [1,2]], [[[1,2],[3,4]],[[5,6],[7,8]]]);
+      assert.deepEqual(m.valueOf(), [
+        [
+          [0,0,0],
+          [0,0,0],
+          [0,0,0]
+        ],
+        [
+          [0,0,0],
+          [0,1,2],
+          [0,3,4]
+        ],
+        [
+          [0,0,0],
+          [0,5,6],
+          [0,7,8]
+        ]
+      ]);
+
+    });
+
+  });
 
 
-// test matrix.map
-m = math.matrix([[1,2,3], [4,5,6]]);
-m2 = m.map(function (value) {
-  return value * 2;
+  describe('map', function() {
+
+    it('should apply the given function to all elements in the matrix', function() {
+      m = math.matrix([[1,2,3], [4,5,6]]);
+      m2 = m.map(function (value) { return value * 2; });
+      assert.deepEqual(m2.valueOf(), [[2,4,6],[8,10,12]]);
+    });
+
+    it('???', function() {
+
+      var output = [];
+      m.forEach(function (value, index, obj) {
+        output.push(math.clone([value, index, obj === m]));
+      });
+      assert.deepEqual(output, [
+        [1, [0, 0], true ],
+        [2, [0, 1], true ],
+        [3, [0, 2], true ],
+        [4, [1, 0], true ],
+        [5, [1, 1], true ],
+        [6, [1, 2], true ]
+      ]);
+      assert.deepEqual(m.map(function (value, index, obj) {
+        return math.clone([value, index, obj === m]);
+      }).valueOf(), [
+        [
+          [1, [0, 0], true ],
+          [2, [0, 1], true ],
+          [3, [0, 2], true ]
+        ],
+        [
+          [4, [1, 0], true ],
+          [5, [1, 1], true ],
+          [6, [1, 2], true ]
+        ]
+      ]);
+
+    });
+
+  });
+
+  describe('clone', function() {
+
+    it('should clone the matrix properly', function() {
+      m4 = m.clone();
+      assert.deepEqual(m4.size(), [2,3]);
+      assert.deepEqual(m4.valueOf(), [[1,2,3], [4,5,6]]);
+    });
+
+  });
+
 });
-assert.deepEqual(m2.size(), [2,3]);
-assert.deepEqual(m2.valueOf(), [[2,4,6],[8,10,12]]);
-
-// test matrix.indexOf
-var output = [];
-m.forEach(function (value, index, obj) {
-  output.push(math.clone([value, index, obj === m]));
-});
-assert.deepEqual(output, [
-  [1, [0, 0], true ],
-  [2, [0, 1], true ],
-  [3, [0, 2], true ],
-  [4, [1, 0], true ],
-  [5, [1, 1], true ],
-  [6, [1, 2], true ]
-]);
-assert.deepEqual(m.map(function (value, index, obj) {
-  return math.clone([value, index, obj === m]);
-}).valueOf(), [
-  [
-    [1, [0, 0], true ],
-    [2, [0, 1], true ],
-    [3, [0, 2], true ]
-  ],
-  [
-    [4, [1, 0], true ],
-    [5, [1, 1], true ],
-    [6, [1, 2], true ]
-  ]
-]);
-
-// test clone
-m4 = m.clone();
-assert.deepEqual(m4.size(), [2,3]);
-assert.deepEqual(m4.valueOf(), [[1,2,3], [4,5,6]]);
 
 // TODO: extensively test Matrix
