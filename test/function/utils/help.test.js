a77a94b0d03ba990b2d8e529077786dd0bb6c3cd
Sebastien Piquemal <sebpiq@gmail.com>
2013-08-17 16:45:52 +0400

    refactored test/function/unit and test/function/utils to mocha

--- test/function/utils/help.test.js
+++ test/function/utils/help.test.js
@@ -1,43 +1,48 @@
-// test help
 var assert = require('assert');
 var math = require('../../../index.js');
 var prop;
 
-var help = math.help('sin');
-assert.ok(help instanceof math.type.Help);
-assert.deepEqual(help.doc, math.docs.sin);
+describe('help ???', function() {
 
-// names to ignore
-var ignore = ['workspace', 'parse', 'parser', 'select', 'unaryminus'];
+  it('should ???', function() {
+    var help = math.help('sin');
+    assert.ok(help instanceof math.type.Help);
+    assert.deepEqual(help.doc, math.docs.sin);
 
-// test whether all functions are documented
-var undocumented = [];
-for (prop in math) {
-  if (math.hasOwnProperty(prop)) {
-    var obj = math[prop];
-    if (math['typeof'](obj) != 'object') {
-      if (!math.docs[prop] && (ignore.indexOf(prop) == -1)) {
-        undocumented.push(prop);
+    // names to ignore
+    var ignore = ['workspace', 'parse', 'parser', 'select', 'unaryminus'];
+
+    // test whether all functions are documented
+    var undocumented = [];
+    for (prop in math) {
+      if (math.hasOwnProperty(prop)) {
+        var obj = math[prop];
+        if (math['typeof'](obj) != 'object') {
+          if (!math.docs[prop] && (ignore.indexOf(prop) == -1)) {
+            undocumented.push(prop);
+          }
+        }
       }
     }
-  }
-}
-if (undocumented.length) {
-  console.log('WARNING: The following objects are undocumented: ' +
-      undocumented.join(', '));
-}
-
-// test whether there is documentation for non existing functions
-var nonexisting = [];
-var docs = math.docs;
-for (prop in docs) {
-  if (docs.hasOwnProperty(prop)) {
-    if (math[prop] === undefined && !math.type[prop]) {
-      nonexisting.push(prop);
+    if (undocumented.length) {
+      console.log('WARNING: The following objects are undocumented: ' +
+          undocumented.join(', '));
     }
-  }
-}
-if (nonexisting.length) {
-  console.log('WARNING: There is documentation available on the following ' +
-      'non-existing objects: ' + nonexisting.join(', '));
-}
+
+    // test whether there is documentation for non existing functions
+    var nonexisting = [];
+    var docs = math.docs;
+    for (prop in docs) {
+      if (docs.hasOwnProperty(prop)) {
+        if (math[prop] === undefined && !math.type[prop]) {
+          nonexisting.push(prop);
+        }
+      }
+    }
+    if (nonexisting.length) {
+      console.log('WARNING: There is documentation available on the following ' +
+          'non-existing objects: ' + nonexisting.join(', '));
+    }
+  });
+
+});
