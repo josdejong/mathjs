0bd2db9f2bb21fbba4f70ccd3b8529b1e3e7037b
Guillermo Indalecio Fernandez <guillermobox@gmail.com>
2013-10-13 15:49:45 +0200

    Fixed bugs and deployed tests

---
+++ test/function/statistics/mean.test.js
@@ -0,0 +1,43 @@
+var assert = require('assert');
+var math = require('../../../index.js');
+
+describe('mean', function() {
+
+  it('should return the mean value of some numbers', function() {
+    assert.equal(math.mean(5), 5);
+    assert.equal(math.mean(3,1), 2);
+    assert.equal(math.mean(0,3), 1.5);
+    assert.equal(math.mean(1,3,5,2,-5), 1.2);
+    assert.equal(math.mean(0,0,0,0), 0);
+  });
+
+  it('should return the mean value from a vector', function() {
+    assert.equal(math.mean(math.matrix([1,3,5,2,-5])), 1.2);
+  });
+
+  it('should return the mean for each vectorvector on the last dimension', function() {
+    assert.deepEqual(math.mean([
+      [ 1, 6,  7],
+      [ 3, 0,  6],
+      [-1, 9, 11]
+    ]), [ 1, 5, 8]);
+    assert.deepEqual(math.mean(math.matrix([
+      [ 1, 6,  7],
+      [ 3, 0,  6],
+      [-1, 9, 11]
+    ])), math.matrix([ 1, 5, 8]));
+  });
+
+  it('should throw an error if called with invalid number of arguments', function() {
+    assert.throws(function() {math.mean()});
+  });
+
+  it('should throw an error if called with arguments of different types', function() {
+    assert.throws(function() {math.mean([5,2], 3)});
+  });
+
+  it('should throw an error if called with an empty array', function() {
+    assert.throws(function() {math.mean([])});
+  });
+
+});
