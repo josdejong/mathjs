fb59d5c7e6e51f3f94645cbe8eb6e62b67096c6c
josdejong <wjosdejong@gmail.com>
2013-10-19 22:06:44 +0200

    Removed global file options

--- index.js
+++ index.js
@@ -1,5 +1,43 @@
+// math.js factory
+
 // options (global configuration settings)
-exports.options = require('./lib/options');
+exports.options = {
+  format: {
+    precision: 5   // number of digits in formatted output
+  },
+  matrix: {
+    'defaultType': 'matrix' // type of default matrix output. Choose 'array' or 'matrix' (default)
+  }
+};
+
+// TODO: change options to properties with getters to validate the input value
+
+// TODO: remove deprecated options some day (deprecated since version 0.15.0)
+if (Object.defineProperty) {
+  var fnPrecision = function () {
+    throw new Error('Option math.options.precision is deprecated. ' +
+        'Use math.options.format.precision instead.')
+  };
+
+  Object.defineProperty(exports.options, 'precision', {
+    get: fnPrecision,
+    set: fnPrecision,
+    enumerable: false,
+    configurable: false
+  });
+
+  var fnDefault = function () {
+    throw new Error('Option math.options.matrix.default is deprecated. ' +
+        'Use math.options.matrix.defaultType instead.')
+  };
+
+  Object.defineProperty(exports.options.matrix, 'default', {
+    get: fnDefault,
+    set: fnDefault,
+    enumerable: false,
+    configurable: false
+  });
+}
 
 // expression (Parser, Scope, nodes, docs)
 exports.expression = {};
