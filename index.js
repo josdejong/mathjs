c40a327b10a38b1f00d88559a03f6435ac596bf3
josdejong <wjosdejong@gmail.com>
2013-10-19 22:17:58 +0200

    Implemented a factory function for the library

--- index.js
+++ index.js
@@ -1,183 +1,217 @@
-// math.js factory
+/**
+ * math.js factory function.
+ *
+ * Usage:
+ *
+ *     var math = mathFactory();
+ *     var math = mathFactory(options);
+ *
+ * @param {Object} [options]  Available options:
+ *                            {Number} format.precision
+ *                              A number in the range 0-16. Default value is 5.
+ *                            {String} matrix.defaultType
+ *                              A string 'array' or 'matrix' (default).
+ */
+function mathFactory (options) {
+  // create new namespace
+  var math = {};
 
-// options (global configuration settings)
-exports.options = {
-  format: {
-    precision: 5   // number of digits in formatted output
-  },
-  matrix: {
-    'defaultType': 'matrix' // type of default matrix output. Choose 'array' or 'matrix' (default)
+  // options (global configuration settings)
+  math.options = {
+    format: {
+      precision: 5   // number of digits in formatted output
+    },
+    matrix: {
+      'defaultType': 'matrix' // type of default matrix output. Choose 'array' or 'matrix' (default)
+    }
+  };
+
+  // merge options
+  if (options && options.format && options.format.precision) {
+    math.options.format.precision = options.format.precision;
   }
-};
+  if (options && options.matrix && options.matrix.defaultType) {
+    math.options.matrix.defaultType = options.matrix.defaultType;
+  }
 
-// TODO: change options to properties with getters to validate the input value
+  // TODO: change options to properties with getters to validate the input value
 
-// TODO: remove deprecated options some day (deprecated since version 0.15.0)
-if (Object.defineProperty) {
-  var fnPrecision = function () {
-    throw new Error('Option math.options.precision is deprecated. ' +
-        'Use math.options.format.precision instead.')
+  // TODO: remove deprecated options some day (deprecated since version 0.15.0)
+  if (Object.defineProperty) {
+    var fnPrecision = function () {
+      throw new Error('Option math.options.precision is deprecated. ' +
+          'Use math.options.format.precision instead.')
+    };
+
+    Object.defineProperty(math.options, 'precision', {
+      get: fnPrecision,
+      set: fnPrecision,
+      enumerable: false,
+      configurable: false
+    });
+
+    var fnDefault = function () {
+      throw new Error('Option math.options.matrix.default is deprecated. ' +
+          'Use math.options.matrix.defaultType instead.')
+    };
+
+    Object.defineProperty(math.options.matrix, 'default', {
+      get: fnDefault,
+      set: fnDefault,
+      enumerable: false,
+      configurable: false
+    });
+  }
+
+
+  // expression (Parser, Scope, nodes, docs)
+  math.expression = {};
+  math.expression.node = require('./lib/expression/node/index.js');
+  math.expression.Scope = require('./lib/expression/Scope.js');
+  math.expression.Parser = require('./lib/expression/Parser.js');
+  math.expression.docs = require('./lib/expression/docs/index.js');
+
+  // TODO: deprecated since version 0.13.0. cleanup deprecated stuff some day
+  math.expr = {};
+  math.expr.Scope = function () {
+    throw new Error('Moved to math.expression.Scope');
+  };
+  math.expr.Parser = function () {
+    throw new Error('Moved to math.expression.Parser');
   };
 
-  Object.defineProperty(exports.options, 'precision', {
-    get: fnPrecision,
-    set: fnPrecision,
-    enumerable: false,
-    configurable: false
-  });
 
-  var fnDefault = function () {
-    throw new Error('Option math.options.matrix.default is deprecated. ' +
-        'Use math.options.matrix.defaultType instead.')
+  // types (Matrix, Complex, Unit, ...)
+  math.type = {};
+  math.type.Complex = require('./lib/type/Complex.js');
+  math.type.Range = require('./lib/type/Range.js');
+  math.type.Index = require('./lib/type/Index.js');
+  math.type.Matrix = require('./lib/type/Matrix.js');
+  math.type.Unit = require('./lib/type/Unit.js');
+  math.type.Help = require('./lib/type/Help.js');
+
+  math.collection = require('./lib/type/collection.js');
+
+  // expression parser
+  require('./lib/function/expression/eval.js')(math);
+  require('./lib/function/expression/help.js')(math);
+  require('./lib/function/expression/parse.js')(math);
+
+  // functions - arithmetic
+  require('./lib/function/arithmetic/abs.js')(math);
+  require('./lib/function/arithmetic/add.js')(math);
+  require('./lib/function/arithmetic/add.js')(math);
+  require('./lib/function/arithmetic/ceil.js')(math);
+  require('./lib/function/arithmetic/cube.js')(math);
+  require('./lib/function/arithmetic/divide.js')(math);
+  require('./lib/function/arithmetic/edivide.js')(math);
+  require('./lib/function/arithmetic/emultiply.js')(math);
+  require('./lib/function/arithmetic/epow.js')(math);
+  require('./lib/function/arithmetic/equal.js')(math);
+  require('./lib/function/arithmetic/exp.js')(math);
+  require('./lib/function/arithmetic/fix.js')(math);
+  require('./lib/function/arithmetic/floor.js')(math);
+  require('./lib/function/arithmetic/gcd.js')(math);
+  require('./lib/function/arithmetic/larger.js')(math);
+  require('./lib/function/arithmetic/largereq.js')(math);
+  require('./lib/function/arithmetic/lcm.js')(math);
+  require('./lib/function/arithmetic/log.js')(math);
+  require('./lib/function/arithmetic/log10.js')(math);
+  require('./lib/function/arithmetic/mod.js')(math);
+  require('./lib/function/arithmetic/multiply.js')(math);
+  require('./lib/function/arithmetic/pow.js')(math);
+  require('./lib/function/arithmetic/round.js')(math);
+  require('./lib/function/arithmetic/sign.js')(math);
+  require('./lib/function/arithmetic/smaller.js')(math);
+  require('./lib/function/arithmetic/smallereq.js')(math);
+  require('./lib/function/arithmetic/sqrt.js')(math);
+  require('./lib/function/arithmetic/square.js')(math);
+  require('./lib/function/arithmetic/subtract.js')(math);
+  require('./lib/function/arithmetic/unary.js')(math);
+  require('./lib/function/arithmetic/unequal.js')(math);
+  require('./lib/function/arithmetic/xgcd.js')(math);
+
+  // functions - complex
+  require('./lib/function/complex/arg.js')(math);
+  require('./lib/function/complex/conj.js')(math);
+  require('./lib/function/complex/re.js')(math);
+  require('./lib/function/complex/im.js')(math);
+
+  // functions - construction
+  require('./lib/function/construction/boolean.js')(math);
+  require('./lib/function/construction/complex.js')(math);
+  require('./lib/function/construction/index.js')(math);
+  require('./lib/function/construction/matrix.js')(math);
+  require('./lib/function/construction/number.js')(math);
+  require('./lib/function/construction/parser.js')(math);
+  require('./lib/function/construction/string.js')(math);
+  require('./lib/function/construction/unit.js')(math);
+
+  // functions - matrix
+  require('./lib/function/matrix/concat.js')(math);
+  require('./lib/function/matrix/det.js')(math);
+  require('./lib/function/matrix/diag.js')(math);
+  require('./lib/function/matrix/eye.js')(math);
+  require('./lib/function/matrix/inv.js')(math);
+  require('./lib/function/matrix/ones.js')(math);
+  require('./lib/function/matrix/range.js')(math);
+  require('./lib/function/matrix/size.js')(math);
+  require('./lib/function/matrix/squeeze.js')(math);
+  require('./lib/function/matrix/subset.js')(math);
+  require('./lib/function/matrix/transpose.js')(math);
+  require('./lib/function/matrix/zeros.js')(math);
+
+  // functions - probability
+  require('./lib/function/probability/factorial.js')(math);
+  require('./lib/function/probability/random.js')(math);
+
+  // functions - statistics
+  require('./lib/function/statistics/min.js')(math);
+  require('./lib/function/statistics/max.js')(math);
+  require('./lib/function/statistics/mean.js')(math);
+
+  // functions - trigonometry
+  require('./lib/function/trigonometry/acos.js')(math);
+  require('./lib/function/trigonometry/asin.js')(math);
+  require('./lib/function/trigonometry/atan.js')(math);
+  require('./lib/function/trigonometry/atan2.js')(math);
+  require('./lib/function/trigonometry/cos.js')(math);
+  require('./lib/function/trigonometry/cot.js')(math);
+  require('./lib/function/trigonometry/csc.js')(math);
+  require('./lib/function/trigonometry/sec.js')(math);
+  require('./lib/function/trigonometry/sin.js')(math);
+  require('./lib/function/trigonometry/tan.js')(math);
+
+  // functions - units
+  require('./lib/function/units/in.js')(math);
+
+  // functions - utils
+  require('./lib/function/utils/clone.js')(math);
+  require('./lib/function/utils/format.js')(math);
+  require('./lib/function/utils/import.js')(math);
+  require('./lib/function/utils/map.js')(math);
+  require('./lib/function/utils/print.js')(math);
+  require('./lib/function/utils/select.js')(math);
+  require('./lib/function/utils/typeof.js')(math);
+  require('./lib/function/utils/forEach.js')(math);
+
+  // constants
+  require('./lib/constants.js')(math);
+
+  // selector (we initialize after all functions are loaded)
+  math.chaining = {};
+  math.chaining.Selector = require('./lib/chaining/Selector.js')(math);
+
+  // TODO: deprecated since version 0.13.0. Cleanup some day
+  math.expr.Selector = function () {
+    throw new Error('Moved to math.expression.Selector');
   };
 
-  Object.defineProperty(exports.options.matrix, 'default', {
-    get: fnDefault,
-    set: fnDefault,
-    enumerable: false,
-    configurable: false
-  });
+  // return the new instance
+  return math;
 }
 
-// expression (Parser, Scope, nodes, docs)
-exports.expression = {};
-exports.expression.node = require('./lib/expression/node/index.js');
-exports.expression.Scope = require('./lib/expression/Scope.js');
-exports.expression.Parser = require('./lib/expression/Parser.js');
-exports.expression.docs = require('./lib/expression/docs/index.js');
 
-// TODO: deprecated since version 0.13.0. cleanup deprecated stuff some day
-exports.expr = {};
-exports.expr.Scope = function () {
-  throw new Error('Moved to math.expression.Scope');
-};
-exports.expr.Parser = function () {
-  throw new Error('Moved to math.expression.Parser');
-};
-
-
-// types (Matrix, Complex, Unit, ...)
-exports.type = {};
-exports.type.Complex = require('./lib/type/Complex.js');
-exports.type.Range = require('./lib/type/Range.js');
-exports.type.Index = require('./lib/type/Index.js');
-exports.type.Matrix = require('./lib/type/Matrix.js');
-exports.type.Unit = require('./lib/type/Unit.js');
-exports.type.Help = require('./lib/type/Help.js');
-
-exports.collection = require('./lib/type/collection.js');
-
-// expression parser
-require('./lib/function/expression/eval.js')(exports);
-require('./lib/function/expression/help.js')(exports);
-require('./lib/function/expression/parse.js')(exports);
-
-// functions - arithmetic
-require('./lib/function/arithmetic/abs.js')(exports);
-require('./lib/function/arithmetic/add.js')(exports);
-require('./lib/function/arithmetic/add.js')(exports);
-require('./lib/function/arithmetic/ceil.js')(exports);
-require('./lib/function/arithmetic/cube.js')(exports);
-require('./lib/function/arithmetic/divide.js')(exports);
-require('./lib/function/arithmetic/edivide.js')(exports);
-require('./lib/function/arithmetic/emultiply.js')(exports);
-require('./lib/function/arithmetic/epow.js')(exports);
-require('./lib/function/arithmetic/equal.js')(exports);
-require('./lib/function/arithmetic/exp.js')(exports);
-require('./lib/function/arithmetic/fix.js')(exports);
-require('./lib/function/arithmetic/floor.js')(exports);
-require('./lib/function/arithmetic/gcd.js')(exports);
-require('./lib/function/arithmetic/larger.js')(exports);
-require('./lib/function/arithmetic/largereq.js')(exports);
-require('./lib/function/arithmetic/lcm.js')(exports);
-require('./lib/function/arithmetic/log.js')(exports);
-require('./lib/function/arithmetic/log10.js')(exports);
-require('./lib/function/arithmetic/mod.js')(exports);
-require('./lib/function/arithmetic/multiply.js')(exports);
-require('./lib/function/arithmetic/pow.js')(exports);
-require('./lib/function/arithmetic/round.js')(exports);
-require('./lib/function/arithmetic/sign.js')(exports);
-require('./lib/function/arithmetic/smaller.js')(exports);
-require('./lib/function/arithmetic/smallereq.js')(exports);
-require('./lib/function/arithmetic/sqrt.js')(exports);
-require('./lib/function/arithmetic/square.js')(exports);
-require('./lib/function/arithmetic/subtract.js')(exports);
-require('./lib/function/arithmetic/unary.js')(exports);
-require('./lib/function/arithmetic/unequal.js')(exports);
-require('./lib/function/arithmetic/xgcd.js')(exports);
-
-// functions - complex
-require('./lib/function/complex/arg.js')(exports);
-require('./lib/function/complex/conj.js')(exports);
-require('./lib/function/complex/re.js')(exports);
-require('./lib/function/complex/im.js')(exports);
-
-// functions - construction
-require('./lib/function/construction/boolean.js')(exports);
-require('./lib/function/construction/complex.js')(exports);
-require('./lib/function/construction/index.js')(exports);
-require('./lib/function/construction/matrix.js')(exports);
-require('./lib/function/construction/number.js')(exports);
-require('./lib/function/construction/parser.js')(exports);
-require('./lib/function/construction/string.js')(exports);
-require('./lib/function/construction/unit.js')(exports);
-
-// functions - matrix
-require('./lib/function/matrix/concat.js')(exports);
-require('./lib/function/matrix/det.js')(exports);
-require('./lib/function/matrix/diag.js')(exports);
-require('./lib/function/matrix/eye.js')(exports);
-require('./lib/function/matrix/inv.js')(exports);
-require('./lib/function/matrix/ones.js')(exports);
-require('./lib/function/matrix/range.js')(exports);
-require('./lib/function/matrix/size.js')(exports);
-require('./lib/function/matrix/squeeze.js')(exports);
-require('./lib/function/matrix/subset.js')(exports);
-require('./lib/function/matrix/transpose.js')(exports);
-require('./lib/function/matrix/zeros.js')(exports);
-
-// functions - probability
-require('./lib/function/probability/factorial.js')(exports);
-require('./lib/function/probability/random.js')(exports);
-
-// functions - statistics
-require('./lib/function/statistics/min.js')(exports);
-require('./lib/function/statistics/max.js')(exports);
-require('./lib/function/statistics/mean.js')(exports);
-
-// functions - trigonometry
-require('./lib/function/trigonometry/acos.js')(exports);
-require('./lib/function/trigonometry/asin.js')(exports);
-require('./lib/function/trigonometry/atan.js')(exports);
-require('./lib/function/trigonometry/atan2.js')(exports);
-require('./lib/function/trigonometry/cos.js')(exports);
-require('./lib/function/trigonometry/cot.js')(exports);
-require('./lib/function/trigonometry/csc.js')(exports);
-require('./lib/function/trigonometry/sec.js')(exports);
-require('./lib/function/trigonometry/sin.js')(exports);
-require('./lib/function/trigonometry/tan.js')(exports);
-
-// functions - units
-require('./lib/function/units/in.js')(exports);
-
-// functions - utils
-require('./lib/function/utils/clone.js')(exports);
-require('./lib/function/utils/format.js')(exports);
-require('./lib/function/utils/import.js')(exports);
-require('./lib/function/utils/map.js')(exports);
-require('./lib/function/utils/print.js')(exports);
-require('./lib/function/utils/select.js')(exports);
-require('./lib/function/utils/typeof.js')(exports);
-require('./lib/function/utils/forEach.js')(exports);
-
-// constants
-require('./lib/constants.js')(exports);
-
-// selector (we initialize after all functions are loaded)
-exports.chaining = {};
-exports.chaining.Selector = require('./lib/chaining/Selector.js')(exports);
-
-// TODO: deprecated since version 0.13.0. Cleanup some day
-exports.expr.Selector = function () {
-  throw new Error('Moved to math.expression.Selector');
-};
+// return a new instance
+// TODO: return the mathFactory itself
+module.exports = mathFactory();
