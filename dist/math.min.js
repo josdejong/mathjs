985a79d62d8a6471e29ba1e75b662e4039dc9536
josdejong <wjosdejong@gmail.com>
2013-11-28 21:58:32 +0100

    Whooops accidentally merged the develop branch

--- dist/math.min.js
+++ dist/math.min.js
@@ -6,7 +6,7 @@
  * It features real and complex numbers, units, matrices, a large set of
  * mathematical functions, and a flexible expression parser.
  *
- * @version 0.16.1-SNAPSHOT
+ * @version 0.16.0
  * @date    2013-11-28
  *
  * @license
