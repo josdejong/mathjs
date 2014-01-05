5f81246136d85a4d933036d41402e9ac9c6aac3a
josdejong <wjosdejong@gmail.com>
2014-01-05 15:01:07 +0100

    Implemented global definitions internally in expression compilation

--- test/expression/node/ParamsNode.test.js
+++ test/expression/node/ParamsNode.test.js
@@ -6,6 +6,7 @@ var assert = require('assert'),
     Scope = require('../../../lib/expression/Scope'),
     ConstantNode = require('../../../lib/expression/node/ConstantNode'),
     SymbolNode = require('../../../lib/expression/node/SymbolNode'),
+    RangeNode = require('../../../lib/expression/node/RangeNode'),
     ParamsNode = require('../../../lib/expression/node/ParamsNode');
 
 describe('ParamsNode', function() {
@@ -25,6 +26,52 @@ describe('ParamsNode', function() {
 
     var scope = {};
     assert.equal(n.compile(math).eval(scope), 2);
+  });
+
+  it.skip ('should compile a ParamsNode acting on a matrix', function () {
+    var s = new SymbolNode('a');
+    var n = new ParamsNode(math, s, [
+      new ConstantNode('number', '2'),
+      new ConstantNode('number', '1')
+    ]);
+
+    var scope = {
+      a: [[1, 2], [3, 4]]
+    };
+    assert.equal(n.compile(math).eval(scope), 3);
+  });
+
+  it.skip ('should compile a ParamsNode acting on a matrix (2)', function () {
+    var s = new SymbolNode('a');
+    var n = new ParamsNode(math, s, [
+      new ConstantNode('number', '2'),
+      new RangeNode(math, [
+        new ConstantNode('number', '1'),
+        new SymbolNode('end')
+      ])
+    ]);
+
+    var scope = {
+      a: [[1, 2], [3, 4]]
+    };
+    assert.deepEqual(n.compile(math).eval(scope), [3, 4]);
+  });
+
+  it.skip ('should compile a ParamsNode acting on a matrix (3)', function () {
+    var s = new SymbolNode('a');
+    var n = new ParamsNode(math, s, [
+      new ConstantNode('number', '2'),
+      new RangeNode(math, [
+        new SymbolNode('end'),
+        new ConstantNode('number', '1'),
+        new ConstantNode('number', '-1')
+      ])
+    ]);
+
+    var scope = {
+      a: [[1, 2], [3, 4]]
+    };
+    assert.deepEqual(n.compile(math).eval(scope), [4, 3]);
   });
 
   it ('should find a ParamsNode', function () {
