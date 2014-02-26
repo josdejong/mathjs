30d9d4653c7d74ab23c49032792178de2e06cfb1
josdejong <wjosdejong@gmail.com>
2014-02-26 21:07:22 +0100

    Some more unit tests added

---
+++ test/deprecated.test.js
@@ -0,0 +1,54 @@
+// test error messages for deprecated functions
+var assert = require('assert'),
+    mathjs = require('../index');
+
+describe('deprecated stuff', function() {
+
+  it ('should throw an error when using deprecated setting number.defaultType', function () {
+    assert.throws(function () {
+      mathjs({
+        number: {
+          defaultType: 'number'
+        }
+      })
+    }, /is deprecated/);
+  });
+
+  it ('should throw an error when using deprecated setting number.precision', function () {
+    assert.throws(function () {
+      mathjs({
+        number: {
+          precision: 14
+        }
+      })
+    }, /is deprecated/);
+  });
+
+  it ('should throw an error when using deprecated setting matrix.defaultType', function () {
+    assert.throws(function () {
+      mathjs({
+        matrix: {
+          defaultType: 'array'
+        }
+      })
+    }, /is deprecated/);
+  });
+
+  it ('should throw an error when using deprecated setting matrix.default', function () {
+    assert.throws(function () {
+      mathjs({
+        matrix: {
+          'default': 'array'
+        }
+      })
+    }, /is deprecated/);
+  });
+
+  it ('should throw an error when using deprecated math.expression.Scope', function () {
+    var math = mathjs();
+    assert.throws(function () {
+      new math.expression.Scope();
+    }, /is deprecated/);
+  });
+
+});
