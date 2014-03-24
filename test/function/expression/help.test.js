585c5c378457112de039c628d5fef3d2eb3ca39c
josdejong <wjosdejong@gmail.com>
2014-03-24 20:32:56 +0100

    Moved the validation script from tests to the build script

--- test/function/expression/help.test.js
+++ test/function/expression/help.test.js
@@ -3,39 +3,16 @@ var assert = require('assert'),
 
 describe('help', function() {
 
-  it('should contain documentation for all available functions', function() {
+  it('should find documentation for a function by its name', function() {
     var help = math.help('sin');
     assert.ok(help instanceof math.type.Help);
     assert.deepEqual(help.doc, math.expression.docs.sin);
-
-    // names to ignore
-    var ignore = ['workspace', 'compile', 'parse', 'parser', 'select', 'unary', 'print', 'config', 'in'];
-
-    // test whether all functions are documented
-    for (var prop in math) {
-      if (math.hasOwnProperty(prop)) {
-        var obj = math[prop];
-        if (math['typeof'](obj) != 'object') {
-          if (!math.expression.docs[prop] && (ignore.indexOf(prop) == -1)) {
-            // TODO: find a better solution for this
-            console.log('WARNING: Function ' + prop + ' is undocumented');
-          }
-        }
-      }
-    }
   });
 
-  it('should contain functions for all available documentation', function() {
-    // test whether there is documentation for non existing functions
-    var docs = math.expression.docs;
-    for (var prop in docs) {
-      if (docs.hasOwnProperty(prop)) {
-        if (math[prop] === undefined && !math.type[prop]) {
-          // TODO: find a better solution for this
-          console.log('WARNING: Documentation for a non-existing function "' + prop + '"');
-        }
-      }
-    }
+  it('should find documentation for a function by the function itself', function() {
+    var help = math.help(math.sin);
+    assert.ok(help instanceof math.type.Help);
+    assert.deepEqual(help.doc, math.expression.docs.sin);
   });
 
   it('should throw an error on wrong number of arguments', function() {
