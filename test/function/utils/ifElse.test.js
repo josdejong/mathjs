45982fbba0b625bdb2bdc45395d99d648fa686ff
jos <wjosdejong@gmail.com>
2014-06-08 21:22:57 +0200

    Removed array/matrix support from function `ifElse`

--- test/function/utils/ifElse.test.js
+++ test/function/utils/ifElse.test.js
@@ -50,43 +50,6 @@ describe('ifElse', function() {
     assert.equal(ifElse(undefined, 1, 0), 0);
   });
 
-  it('should evaluate array conditions', function() {
-    assert.deepEqual(ifElse([1, 0, 1], 1, 0), [1, 0, 1]);
-    assert.deepEqual(ifElse([[1, 0], [0, 0]], 1, 0), [[1, 0], [0, 0]]);
-
-    assert.deepEqual(ifElse([[1, 1], [1, 1]], [[1,2],[3,4]], [[5,6],[7,8]]), [[1,2],[3,4]]);
-    assert.deepEqual(ifElse([[1, 0], [0, 1]], 123, [[5,6],[7,8]]), [[123,6],[7,123]]);
-    assert.deepEqual(ifElse([[1, 0], [0, 1]], [[1,2],[3,4]], 123), [[1,123],[123,4]]);
-    assert.deepEqual(ifElse([[0, 0], [0, 0]], [[1,2],[3,4]], [[5,6],[7,8]]), [[5,6],[7,8]]);
-  });
-
-  it('should evaluate matrix conditions', function() {
-    assert.deepEqual(ifElse(math.matrix([1, 0, 1]), 1, 0), math.matrix([1, 0, 1]));
-    assert.deepEqual(ifElse(math.matrix([[1, 0], [0, 0]]), 1, 0),
-        math.matrix([[1, 0], [0, 0]]));
-
-    assert.deepEqual(ifElse(math.matrix([[1, 1], [1, 1]]), math.matrix([[1,2],[3,4]]), math.matrix([[5,6],[7,8]])),
-        math.matrix([[1,2],[3,4]]));
-    assert.deepEqual(ifElse(math.matrix([[1, 0], [0, 1]]), 123, [[5,6],[7,8]]),
-        math.matrix([[123,6],[7,123]]));
-    assert.deepEqual(ifElse(math.matrix([[1, 0], [0, 1]]), math.matrix([[1,2],[3,4]]), 123),
-        math.matrix([[1,123],[123,4]]));
-    assert.deepEqual(ifElse(math.matrix([[0, 0], [0, 0]]), math.matrix([[1,2],[3,4]]), math.matrix([[5,6],[7,8]])),
-        math.matrix([[5,6],[7,8]]));
-  });
-
-  it('should throw an error when matrix dimensions mismatch', function() {
-    assert.throws(function () {
-      ifElse(math.matrix([[1, 1], [1, 1]]), math.matrix([[1,2,3],[4,5,6]]), 1);
-    });
-    assert.throws(function () {
-      ifElse(math.matrix([[1, 1], [1, 1]]), 1, math.matrix([[1,2,3],[4,5,6]]));
-    });
-    assert.throws(function () {
-      ifElse(math.matrix([[1, 1], [1, 1]]), 1, math.matrix([1,2]));
-    });
-  });
-
   it('should throw an error if called with invalid number of arguments', function() {
     assert.throws(function() { ifElse(true); });
     assert.throws(function() { ifElse(true, true); });
@@ -94,6 +57,8 @@ describe('ifElse', function() {
   });
 
   it('should throw an error if called with invalid type of arguments', function() {
+    assert.throws(function() { ifElse([], 1, 0); }, math.type.UnsupportedTypeError);
+    assert.throws(function() { ifElse(math.matrix(), 1, 0); }, math.type.UnsupportedTypeError);
     assert.throws(function() { ifElse(new Date(), 1, 0); }, math.type.UnsupportedTypeError);
     assert.throws(function() { ifElse(/regexp/, 1, 0); }, math.type.UnsupportedTypeError);
   });
