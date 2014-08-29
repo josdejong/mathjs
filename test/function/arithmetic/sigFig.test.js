3eafe9e38ae31467b4ee1af55314b2bdafd520ee
Dave Simons <david.simons@softwire.com>
2014-08-29 16:40:23 +0100

    Add significant figure calculation for javascript numbers

---
+++ test/function/arithmetic/sigFig.test.js
@@ -0,0 +1,22 @@
+// test round
+var assert = require('assert'),
+  approx = require('../../../tools/approx'),
+  error = require('../../../lib/error/index'),
+  math = require('../../../index'),
+  bignumber = math.bignumber,
+  sigFig = math.sigFig;
+
+describe('round', function() {
+
+  it('should round a number to the given number of significant figures', function() {
+    approx.equal(sigFig(math.pi, 1), 3);
+    approx.equal(sigFig(math.pi * 10, 1), 30);
+    approx.equal(sigFig(math.pi * 10, 2), 31);
+    approx.equal(sigFig(math.pi * 1000, 3), 3140);
+    approx.equal(sigFig(math.pi * 1000, 6), 3141.59);
+    approx.equal(sigFig(math.pi, 3), 3.14);
+    approx.equal(sigFig(-1 * math.pi, 3), -3.14);
+    approx.equal(sigFig(-1 * math.pi * 1000, 3), -3140);
+  });
+
+});
