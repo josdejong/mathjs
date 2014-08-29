831ba571a0c63e38e6d0d409871a6824fbb378f6
Dave Simons <david.simons@softwire.com>
2014-08-29 16:45:13 +0100

    Add sigFig support for booleans

--- test/function/arithmetic/sigFig.test.js
+++ test/function/arithmetic/sigFig.test.js
@@ -6,7 +6,7 @@ var assert = require('assert'),
   bignumber = math.bignumber,
   sigFig = math.sigFig;
 
-describe('round', function() {
+describe('sigFig', function() {
 
   it('should round a number to the given number of significant figures', function() {
     approx.equal(sigFig(math.pi, 1), 3);
@@ -18,5 +18,11 @@ describe('round', function() {
     approx.equal(sigFig(-1 * math.pi, 3), -3.14);
     approx.equal(sigFig(-1 * math.pi * 1000, 3), -3140);
   });
+
+  it('should round booleans (yeah, not really useful but it should be supported)', function() {
+    approx.equal(sigFig(true, 2), 1);
+    approx.equal(sigFig(false, 2), 0);
+  });
+
 
 });
