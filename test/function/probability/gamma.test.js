19e6ce057aa36da3e2280d7b5b816aeb02da8660
Favian Contreras <fnc4@cornell.edu>
2014-12-17 06:34:23 -0800

    Removed BigNumber bad implementation. Also, cleaned up the code, switched the coefficients for Lanczos, and added extended Stirling approx.

--- test/function/probability/gamma.test.js
+++ test/function/probability/gamma.test.js
@@ -27,29 +27,31 @@ describe('gamma', function () {
   });
 
   it('should calculate the gamma of a rational number', function () {
-    approx.deepEqual(gamma(0.125), 7.5339415987976119046992);
-    approx.deepEqual(gamma(0.25), 3.625609908221908311930685);
-    approx.deepEqual(gamma(0.5), 1.77245385090551602729816748);
-    approx.deepEqual(gamma(1.5), 0.88622692545275801364908374);
-    approx.deepEqual(gamma(2.5), 1.32934038817913702047362561);
-    approx.deepEqual(gamma(3.5), 3.32335097044784255118406403);
-    approx.deepEqual(gamma(30.5), 4.8226969334909086010917483e+31);
+    approx.equal(gamma(0.125), 7.5339415987976119046992);
+    approx.equal(gamma(0.25), 3.625609908221908311930685);
+    approx.equal(gamma(0.5), 1.77245385090551602729816748);
+    approx.equal(gamma(1.5), 0.88622692545275801364908374);
+    approx.equal(gamma(2.5), 1.32934038817913702047362561);
+    approx.equal(gamma(3.5), 3.32335097044784255118406403);
+    approx.equal(gamma(30.5), 4.8226969334909086010917483e+31);
+    approx.equal(gamma(144.9), 3.37554680943478639050191e+249);
 
-    approx.deepEqual(gamma(-0.5), -3.54490770181103205459633);
-    approx.deepEqual(gamma(-1.5), 2.3632718012073547030642233);
-    approx.deepEqual(gamma(-2.5), -0.945308720482941881225689);
+    approx.equal(gamma(-0.5), -3.54490770181103205459633);
+    approx.equal(gamma(-1.5), 2.3632718012073547030642233);
+    approx.equal(gamma(-2.5), -0.945308720482941881225689);
+    approx.equal(gamma(-144.9), -2.078523735791760166777e-251);
   });
 
   it('should calculate the gamma of an irrational number', function () {
-    approx.deepEqual(gamma(Math.sqrt(2)), 0.8865814287192591250809176); 
-    approx.deepEqual(gamma(Math.PI), 2.2880377953400324179595889);
-    approx.deepEqual(gamma(Math.E), 1.56746825577405307486334);
+    approx.equal(gamma(Math.sqrt(2)), 0.8865814287192591250809176); 
+    approx.equal(gamma(Math.PI), 2.2880377953400324179595889);
+    approx.equal(gamma(Math.E), 1.56746825577405307486334);
 
-    approx.deepEqual(gamma(-Math.sqrt(2)), 2.599459907524570073533756846);
-    approx.deepEqual(gamma(-Math.PI), 1.01569714446021834110892259347);
-    approx.deepEqual(gamma(-Math.E), -0.952681729748073099220537210195);
+    approx.equal(gamma(-Math.sqrt(2)), 2.599459907524570073533756846);
+    approx.equal(gamma(-Math.PI), 1.01569714446021834110892259347);
+    approx.equal(gamma(-Math.E), -0.952681729748073099220537210195);
   });
-
+/*
   it('should calculate the gamma of a whole bignumber', function () {
     assert.deepEqual(gamma(bignumber(1)), bignumber(1));
     assert.deepEqual(gamma(bignumber(2)), bignumber(1));
@@ -68,34 +70,34 @@ describe('gamma', function () {
     assert.deepEqual(gamma(bignumber('-1.0e10223')), bignumber(Infinity));
     assert.deepEqual(gamma(bignumber(-Infinity)), bignumber(NaN));
   });
- 
+
   it('should calculate the gamma of a rational bignumber', function () {
-    assert.deepEqual(gamma(bignumber(0.125)).toDP(13), bignumber('7.5339415987976'));
-    assert.deepEqual(gamma(bignumber(0.25)).toDP(14), bignumber('3.62560990822191'));
-    assert.deepEqual(gamma(bignumber(0.5)).toDP(14), bignumber('1.77245385090552'));
-    assert.deepEqual(gamma(bignumber(1.5)).toDP(15), bignumber('0.886226925452758'));
-    assert.deepEqual(gamma(bignumber(2.5)).toDP(14), bignumber('1.32934038817914'));
+    assert.deepEqual(gamma(bignumber(0.125)), bignumber('7.5339415987976'));
+    assert.deepEqual(gamma(bignumber(0.25)), bignumber('3.62560990822191'));
+    assert.deepEqual(gamma(bignumber(0.5)), bignumber('1.77245385090552'));
+    assert.deepEqual(gamma(bignumber(1.5)), bignumber('0.886226925452758'));
+    assert.deepEqual(gamma(bignumber(2.5)), bignumber('1.32934038817914'));
 
     var bigmath = math.create({ precision: 15 });
-    assert.deepEqual(bigmath.gamma(bignumber(30.5)).toExponential(14), '4.82269693349091e+31');
+    assert.deepEqual(bigmath.gamma(bignumber(30.5)), '4.82269693349091e+31');
 
     bigmath.config({ precision: 13 });
-    assert.deepEqual(bigmath.gamma(bignumber(-1.5)).toDP(12), bigmath.bignumber('2.363271801207'));
-    assert.deepEqual(gamma(bignumber(-2.5)).toDP(10), bignumber('-0.9453087205'));
+    assert.deepEqual(bigmath.gamma(bignumber(-1.5)), bigmath.bignumber('2.363271801207'));
+    assert.deepEqual(gamma(bignumber(-2.5)), bignumber('-0.9453087205'));
   });
 
   it('should calculate the gamma of an irrational bignumber', function () {
-    assert.deepEqual(gamma(bigUtil.phi(math.precision).neg()).toDP(10), bignumber('2.3258497469'));
-    assert.deepEqual(gamma(bigUtil.phi(math.precision)).toDP(15), bignumber('0.895673151705288'));
+    assert.deepEqual(gamma(bigUtil.phi(math.precision).neg()), bignumber('2.3258497469'));
+    assert.deepEqual(gamma(bigUtil.phi(math.precision)), bignumber('0.895673151705288'));
 
-    assert.deepEqual(gamma(bigUtil.pi(16)).toDP(14), bignumber('2.28803779534003'));
-    assert.deepEqual(gamma(bigUtil.e(math.precision)).toDP(14), bignumber('1.56746825577405'));
+    assert.deepEqual(gamma(bigUtil.pi(20)), bignumber('2.28803779534003'));
+    assert.deepEqual(gamma(bigUtil.e(math.precision)), bignumber('1.56746825577405'));
 
     var bigmath = math.create({ number: 'bignumber' });
-    assert.deepEqual(gamma(bigmath.SQRT2).toDP(15), bignumber('0.886581428719259'));
-    assert.deepEqual(gamma(bigmath.SQRT2.neg()).toDP(11), bignumber('2.59945990753'));
+    assert.deepEqual(gamma(bigmath.SQRT2), bignumber('0.886581428719259'));
+    assert.deepEqual(gamma(bigmath.SQRT2.neg()), bignumber('2.59945990753'));
   });
-
+*/
   it('should calculate the gamma of an imaginary unit', function () {
     approx.deepEqual(gamma(math.i), math.complex(-0.154949828301810685124955130,
                                                  -0.498015668118356042713691117));
