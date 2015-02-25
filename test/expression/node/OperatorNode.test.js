36d4a28e23fb0cd02c357370cddf9b13fab06df3
Max Bruckner <max@maxbruckner.de>
2015-02-25 22:46:09 +0100

    Throw an error when creating invalid identifier
    OperatorNode now throws an error when trying to create an identifier
    while the function name for the OperatorNode isn't properly set.

--- test/expression/node/OperatorNode.test.js
+++ test/expression/node/OperatorNode.test.js
@@ -335,6 +335,18 @@ describe('OperatorNode', function() {
     assert.equal(n.getIdentifier(), 'OperatorNode:add');
   });
 
+  it ('should throw an error if creating identifier while missing a function name', function () {
+    var n1 = new OperatorNode('-', 4, []);
+    var n2 = new OperatorNode('-', '', []);
+
+    assert.throws(function () {
+      n1.getIdentifier();
+    }, /Can't create an identifier for OperatorNode, missing function name./);
+    assert.throws(function () {
+      n2.getIdentifier();
+    }, /Can't create an identifier for OperatorNode, missing function name./);
+  });
+
   it ('should have a precedence', function () {
     var a = new ConstantNode(1);
     var b = new ConstantNode(2);
