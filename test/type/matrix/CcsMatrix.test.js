43adf9fed836749d8bb8d82d469265345c16cb81
rjbaucells <rjbaucells@gmail.com>
2015-03-15 18:33:53 -0400

    CCS tests

--- test/type/matrix/CcsMatrix.test.js
+++ test/type/matrix/CcsMatrix.test.js
@@ -1545,6 +1545,119 @@ describe('CcsMatrix', function() {
           [0, 2, 0, 0]
         ]);
     });
+    
+    it('should get CCS matrix diagonal (n x n)', function () {
+
+      var m = new CcsMatrix(
+        [
+          [1, 0, 0],
+          [0, 1, 0],
+          [0, 0, 1]
+        ]);
+
+      assert.deepEqual(m.diagonal(), [1, 1, 1]);
+    });
+    
+    it('should get CCS matrix diagonal (n x n), k > 0', function () {
+
+      var m = new CcsMatrix(
+        [
+          [1, 2, 0],
+          [0, 1, 3],
+          [0, 0, 1]
+        ]);
+
+      assert.deepEqual(m.diagonal(1), [2, 3]);
+    });
+    
+    it('should get CCS matrix diagonal (n x n), k < 0', function () {
+
+      var m = new CcsMatrix(
+        [
+          [1, 0, 0],
+          [2, 1, 0],
+          [0, 3, 1]
+        ]);
+
+      assert.deepEqual(m.diagonal(-1), [2, 3]);
+    });
+    
+    it('should get CCS matrix diagonal (m x n), m > n', function () {
+
+      var m = new CcsMatrix(
+        [
+          [1, 0, 0],
+          [0, 1, 0],
+          [0, 0, 1],
+          [0, 0, 0]
+        ]);
+
+      assert.deepEqual(m.diagonal(), [1, 1, 1]);
+    });
+
+    it('should get CCS matrix diagonal (m x n), m > n, k > 0', function () {
+
+      var m = new CcsMatrix(
+        [
+          [1, 2, 0],
+          [0, 1, 3],
+          [0, 0, 1],
+          [0, 0, 0]
+        ]);
+
+      assert.deepEqual(m.diagonal(1), [2, 3]);
+    });
+    
+    it('should get CCS matrix diagonal (m x n), m > n, k < 0', function () {
+
+      var m = new CcsMatrix(
+        [
+          [1, 0, 0],
+          [2, 1, 0],
+          [0, 3, 1],
+          [0, 0, 4]
+        ]);
+
+      assert.deepEqual(m.diagonal(-1), [2, 3, 4]);
+    });
+    
+    it('should get CCS matrix diagonal (m x n), m < n', function () {
+
+      var m = new CcsMatrix(
+        [
+          [1, 0, 0, 0],
+          [0, 1, 0, 0],
+          [0, 0, 1, 0]
+        ]);
+
+      assert.deepEqual(m.diagonal(), [1, 1, 1]);
+    });
+    
+    it('should get CCS matrix diagonal (m x n), m < n, k > 0', function () {
+
+      var m = new CcsMatrix(
+        [
+          [1, 2, 0, 0],
+          [0, 1, 3, 0],
+          [0, 0, 1, 4]
+        ]);
+      
+      assert.deepEqual(m.diagonal(1), [2, 3, 4]);
+    });
+    
+    it('should get CCS matrix diagonal (m x n), m < n, k < 0', function () {
+
+      var m = new CcsMatrix(
+        [
+          [1, 0, 0, 0],
+          [2, 1, 0, 0],
+          [4, 3, 1, 0]
+        ]);
+
+      assert.deepEqual(m.diagonal(-1), [2, 3]);
+      
+      assert.deepEqual(m.diagonal(-2), [4]);
+    });
   });
   
   describe('transpose', function () {
