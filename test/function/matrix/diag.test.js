f144b73c14b0c298ac7032ec12d87557a509eff2
rjbaucells <rjbaucells@gmail.com>
2015-03-16 20:40:45 -0400

    math.diag()

--- test/function/matrix/diag.test.js
+++ test/function/matrix/diag.test.js
@@ -9,6 +9,21 @@ describe('diag', function() {
     assert.deepEqual(math.diag([1,2,3]), [[1,0,0],[0,2,0],[0,0,3]]);
     assert.deepEqual(math.diag([[1,2,3],[4,5,6]]), [1,5]);
   });
+  
+  it('should return a diagonal matrix on the default diagonal, dense matrix', function() {
+    assert.deepEqual(math.diag([1,2,3], 'dense'), math.matrix([[1,0,0],[0,2,0],[0,0,3]], 'dense'));
+    assert.deepEqual(math.diag(math.matrix([[1,2,3],[4,5,6]], 'dense')), math.matrix([1,5], 'dense'));
+  });
+  
+  it('should return a diagonal matrix on the default diagonal, ccs matrix', function() {
+    assert.deepEqual(math.diag([1,2,3], 'ccs'), math.matrix([[1,0,0],[0,2,0],[0,0,3]], 'ccs'));
+    assert.deepEqual(math.diag(math.matrix([[1,2,3],[4,5,6]], 'ccs')), math.matrix([1,5], 'ccs'));
+  });
+  
+  it('should return a diagonal matrix on the default diagonal, crs matrix', function() {
+    assert.deepEqual(math.diag([1,2,3], 'crs'), math.matrix([[1,0,0],[0,2,0],[0,0,3]], 'crs'));
+    assert.deepEqual(math.diag(math.matrix([[1,2,3],[4,5,6]], 'crs')), math.matrix([1,5], 'crs'));
+  });
 
   it('should return a array output on array input', function() {
     assert.deepEqual(math.diag([1,2]), [[1,0],[0,2]]);
@@ -31,8 +46,8 @@ describe('diag', function() {
   });
 
   it('should throw an error in case of invalid k', function() {
-    assert.throws(function () {math.diag([[1,2,3],[4,5,6]], 'a')}, /Second parameter in function diag must be an integer/);
-    assert.throws(function () {math.diag([[1,2,3],[4,5,6]], 2.4)}, /Second parameter in function diag must be an integer/);
+    assert.throws(function () {math.diag([[1,2,3],[4,5,6]], 'a', '123');}, /Second parameter in function diag must be an integer/);
+    assert.throws(function () {math.diag([[1,2,3],[4,5,6]], 2.4);}, /Second parameter in function diag must be an integer/);
   });
 
   describe('bignumber', function () {
@@ -96,18 +111,18 @@ describe('diag', function() {
   });
 
   it('should throw an error of the input matrix is not valid', function() {
-    assert.throws(function () {math.diag([[[1],[2]],[[3],[4]]])});
+    assert.throws(function () {math.diag([[[1],[2]],[[3],[4]]]);});
     // TODO: test diag for all types of input (also scalar)
   });
 
   it('should throw an error in case of wrong number of arguments', function() {
-    assert.throws(function () {math.diag()}, error.ArgumentsError);
-    assert.throws(function () {math.diag([], 2, 3, 4)}, error.ArgumentsError);
+    assert.throws(function () {math.diag();}, error.ArgumentsError);
+    assert.throws(function () {math.diag([], 2, 3, 4);}, error.ArgumentsError);
   });
 
   it('should throw an error in case of invalid type of arguments', function() {
-    assert.throws(function () {math.diag(2)}, math.error.TypeError);
-    assert.throws(function () {math.diag([], 'str')}, math.error.TypeError);
+    assert.throws(function () {math.diag(2);}, math.error.TypeError);
+    assert.throws(function () {math.diag([], 'a', 'str');}, math.error.TypeError);
   });
 
 });
