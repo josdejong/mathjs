88d69117aa7e01ced7a1b141500a56e5d908c27e
Nick Foti <nfoti01@gmail.com>
2015-04-14 14:32:29 -0700

    Included more gammaln tests.

--- test/function/probability/gammaln.test.js
+++ test/function/probability/gammaln.test.js
@@ -9,15 +9,26 @@ var assert = require('assert'),
 describe('gammaln', function () {
 
     it('should calculate the gammaln of a whole number', function () {
+      approx.equal(gammaln(1.), 0.);
+      approx.equal(gammaln(2.), 0.);
       approx.equal(gammaln(3), 0.69314718055994529);
+      approx.equal(gammaln(4.), 1.791759469228055000812477);
+      approx.equal(gammaln(8.), 8.525161361065414300165531);
+      approx.equal(gammaln(64.), 201.00931639928152667928);
+      approx.equal(gammaln(256.), 1161.71210111840065079);
     });
 
     it('should calculate the gammaln of a rational number', function () {
+      approx.equal(gammaln(0.1), 2.2527126517342059598697);
       approx.equal(gammaln(0.5), 0.57236494292469997);
+      approx.equal(gammaln(0.6), 0.39823385806923489961685);
+      approx.equal(gammaln(0.7), 0.26086724653166651438573);
+      approx.equal(gammaln(3.4), 1.0923280598027415674947);
     });
 
     it('should calculate the gammaln of a irrational number', function () {
       approx.equal(gammaln(Math.PI), 0.82769459232343701);
+      approx.equal(gammaln(Math.E), 0.449461741820069);
     });
 
     it('should calculate the gamma of each element in a matrix', function () {
