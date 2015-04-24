070000633a20919ecb06438e1dbbbf340d31a38a
Rogelio J. Baucells <rogelio.javier@baucells.net>
2015-04-24 15:01:03 -0400

    renamed sparse related files

--- test/function/algebra/sparse/cs_lu.test.js
+++ test/function/algebra/sparse/cs_lu.test.js
@@ -3,15 +3,15 @@ var assert = require('assert'),
     approx = require('../../../../tools/approx'),
     math = require('../../../../index');
 
-var sparse_lu = math.import(require('../../../../lib/function/algebra/sparse/sparse_lu'));
+var cs_lu = math.import(require('../../../../lib/function/algebra/sparse/cs_lu'));
 
-describe('sparse_lu', function () {
+describe('cs_lu', function () {
 
   it('should decompose matrix, 2 x 2, no permutations, array', function () {
     
-    var m = math.matrix([[2, 1], [1, 4]], 'ccs');
+    var m = math.sparse([[2, 1], [1, 4]]);
 
-    var r = sparse_lu(m, null, 1);
+    var r = cs_lu(m, null, 1);
     // L
     assert.deepEqual(r.L.valueOf(), [[1, 0], [0.5, 1]]);
     // U
