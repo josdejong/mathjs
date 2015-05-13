83e090d015c063990ec9e4417f3d6c666746bd66
rjbaucells <rjbaucells@gmail.com>
2015-05-12 22:07:55 -0400

    tests - partial

--- test/type/index/Index.test.js
+++ test/type/index/Index.test.js
@@ -8,28 +8,39 @@ var Set = math.type.Set;
 describe('Index', function () {
 
   it('should create an Index', function () {
-    assert.deepEqual(new Index(0, 2)._ranges, [new Set([0]), new Set([2])]);
+    assert.deepEqual(new Index(0, 2)._dimensions, [new Set([0]), new Set([2])]);
 
-    assert.deepEqual(new Index(new Range(0, 10))._ranges, [{start:0, end:10, step:1}]);
-    assert.deepEqual(new Index(new Range(0, 10, 2))._ranges, [{start:0, end:10, step:2}]);
-    assert.deepEqual(new Index(new Range(0, 10), new Range(4, 6))._ranges, [
+    assert.deepEqual(new Index(new Range(0, 10))._dimensions, [{start:0, end:10, step:1}]);
+    assert.deepEqual(new Index(new Range(0, 10, 2))._dimensions, [{start:0, end:10, step:2}]);
+    assert.deepEqual(new Index(new Range(0, 10), new Range(4, 6))._dimensions, [
       {start:0, end:10, step:1},
       {start:4, end:6, step:1}
     ]);
+    assert.deepEqual(new Index(new Set([0, 10]))._dimensions, [new Set([0, 10])]);
+    assert.deepEqual(new Index([0, 10])._dimensions, [new Set([0, 10])]);
+    assert.deepEqual(new Index(10)._dimensions, [new Set([10])]);
   });
 
   it('should create an Index from a Range', function () {
-    assert.deepEqual(new Index(new Range(0, 10))._ranges, [{start:0, end:10, step:1}]);
+    assert.deepEqual(new Index(new Range(0, 10))._dimensions, [{start:0, end:10, step:1}]);
   });
 
   it('should create an Index from a Matrix', function () {
-    assert.deepEqual(new Index(math.matrix([0, 10]))._ranges, [new Set([0, 10])]);
+    assert.deepEqual(new Index(math.matrix([0, 10]))._dimensions, [new Set([0, 10])]);
+  });
+  
+  it('should create an Index from an Array', function () {
+    assert.deepEqual(new Index([0, 10])._dimensions, [new Set([0, 10])]);
+  });
+  
+  it('should create an Index from a Set', function () {
+    assert.deepEqual(new Index(new Set([0, 10]))._dimensions, [new Set([0, 10])]);
   });
 
   it('should create an Index from an array with ranges', function () {
     var index = Index.create([new Range(0, 10), 4]);
     assert(index instanceof Index);
-    assert.deepEqual(index._ranges, [new Range(0, 10), new Set([4])]);
+    assert.deepEqual(index._dimensions, [new Range(0, 10), new Set([4])]);
   });
 
   it('should calculate the size of an Index', function () {
@@ -40,6 +51,9 @@ describe('Index', function () {
     assert.deepEqual(new Index(new Range(0, 10), new Range(4, 6), new Range(3, -1, -1)).size(), [10, 2, 4]);
     assert.deepEqual(new Index(new Range(0, 0)).size(), [0]);
     assert.deepEqual(new Index(new Range(0, -1)).size(), [0]);
+    assert.deepEqual(new Index([1, 2, 3]).size(), [3]);
+    assert.deepEqual(new Index(math.matrix([1, 2, 3])).size(), [3]);
+    assert.deepEqual(new Index(new Set([1, 2, 3])).size(), [3]);
     assert.deepEqual(new Index().size(), []);
   });
 
@@ -48,14 +62,18 @@ describe('Index', function () {
     assert.deepEqual(new Index(new Range(4, 0, -1)).min(), [1]);
     assert.deepEqual(new Index(new Range(0, 10), new Range(4, 6)).min(), [0, 4]);
     assert.deepEqual(new Index(new Range(0, 10), new Range(4, 6), new Range(3, -1, -1)).min(), [0, 4, 0]);
+    assert.deepEqual(new Index(2).min(), [2]);
+    assert.deepEqual(new Index(new Range(0, 10), new Set([4, 6]), new Range(3, -1, -1)).min(), [0, 4, 0]);
     assert.deepEqual(new Index().min(), []);
   });
 
-  it('should calculate the minimum values of an Index', function () {
+  it('should calculate the maximum values of an Index', function () {
     assert.deepEqual(new Index(new Range(2, 10)).max(), [9]);
     assert.deepEqual(new Index(new Range(4, 0, -1)).max(), [4]);
     assert.deepEqual(new Index(new Range(0, 10), new Range(4, 6)).max(), [9, 5]);
     assert.deepEqual(new Index(new Range(0, 10), new Range(4, 6), new Range(3, -1, -1)).max(), [9, 5, 3]);
+    assert.deepEqual(new Index(2).max(), [2]);
+    assert.deepEqual(new Index(new Range(0, 10), new Set([4, 6]), new Range(3, -1, -1)).max(), [9, 6, 3]);
     assert.deepEqual(new Index().max(), []);
   });
 
@@ -69,13 +87,14 @@ describe('Index', function () {
   });
 
   it('should clone an Index', function () {
-    var index1 = new Index(2, new Range(0, 4));
+    var index1 = new Index(2, new Range(0, 4), new Set([0, 2]));
     var index2 = index1.clone(0);
 
     assert.deepEqual(index1, index2);
     assert.notStrictEqual(index1, index2);
-    assert.notStrictEqual(index1._ranges[0], index2._ranges[0]);
-    assert.notStrictEqual(index1._ranges[1], index2._ranges[1]);
+    assert.notStrictEqual(index1._dimensions[0], index2._dimensions[0]);
+    assert.notStrictEqual(index1._dimensions[1], index2._dimensions[1]);
+    assert.notStrictEqual(index1._dimensions[2], index2._dimensions[2]);
   });
 
   it('should stringify an index', function () {
@@ -84,22 +103,25 @@ describe('Index', function () {
     assert.equal(new Index(2, 3, 1).toString(), '[[2], [3], [1]]');
     assert.equal(new Index(2, new Range(0, 3)).toString(), '[[2], 0:3]');
     assert.equal(new Index(new Range(0, 6, 2)).toString(), '[0:2:6]');
+    assert.equal(new Index(new Set([0, 6, 2])).toString(), '[[0, 6, 2]]');
   });
 
   it('toJSON', function () {
-    assert.deepEqual(new Index(new Range(0, 10), 2).toJSON(),
+    assert.deepEqual(new Index(new Range(0, 10), 2, new Set([1, 2, 3])).toJSON(),
         {'mathjs': 'Index', ranges: [
           new Range(0, 10, 1),
-          new Set([2])
+          new Set([2]),
+          new Set([1, 2, 3])
         ]});
   });
 
   it('fromJSON', function () {
     var json = {ranges: [
       new Range(0, 10, 1),
-      new Set([2])
+      new Set([2]),
+      new Set([1, 2, 3])
     ]};
-    var i1 = new Index(new Range(0, 10), 2);
+    var i1 = new Index(new Range(0, 10), 2, new Set([1, 2, 3]));
 
     var i2 = Index.fromJSON(json);
     assert.ok(i2 instanceof Index);
@@ -107,35 +129,40 @@ describe('Index', function () {
   });
 
   it('should get the range for a given dimension', function () {
-    var index = new Index(2, [0, 8, 2], [3,-1,-1]);
+    var index = new Index(2, new Range(0, 8, 2), new Range(3,-1,-1), new Set([1, 2, 3]));
 
-    assert(index.range(0) instanceof Range);
-    assert.deepEqual(index.range(0), new Range(2, 3));
+    assert(index.dimension(0) instanceof Set);
+    assert.deepEqual(index.dimension(0), new Set([2]));
 
-    assert(index.range(1) instanceof Range);
-    assert.deepEqual(index.range(1), new Range(0, 8, 2));
+    assert(index.dimension(1) instanceof Range);
+    assert.deepEqual(index.dimension(1), new Range(0, 8, 2));
 
-    assert(index.range(2) instanceof Range);
-    assert.deepEqual(index.range(2), new Range(3, -1, -1));
-    assert.strictEqual(index.range(3), null);
+    assert(index.dimension(2) instanceof Range);
+    assert.deepEqual(index.dimension(2), new Range(3, -1, -1));
+    
+    assert(index.dimension(3) instanceof Set);
+    assert.deepEqual(index.dimension(3), new Set([1, 2, 3]));
+    
+    assert.strictEqual(index.dimension(4), null);
   });
 
   it('should iterate over all ranges', function () {
-    var index = new Index(2, new Range(0, 8, 2), new Range(3,-1,-1));
+    var index = new Index(2, new Range(0, 8, 2), new Range(3,-1,-1), [1, 2, 3]);
 
     var log = [];
-    index.forEach(function (range, i, obj) {
+    index.forEach(function (d, i, obj) {
       log.push({
-        range: range,
+        dimension: d,
         index: i
       });
       assert.strictEqual(obj, index);
     });
 
     assert.deepEqual(log, [
-      {range: new Set([2]), index: 0},
-      {range: new Range(0, 8, 2), index: 1},
-      {range: new Range(3, -1, -1), index: 2}
+      {dimension: new Set([2]), index: 0},
+      {dimension: new Range(0, 8, 2), index: 1},
+      {dimension: new Range(3, -1, -1), index: 2},
+      {dimension: new Set([1, 2, 3]), index: 3}
     ]);
   });
 
@@ -154,24 +181,29 @@ describe('Index', function () {
       [2, 3, 4]
     ]);
 
-    assert.deepEqual(new Index(new Range(2, 5), new Range(0, 8, 2)).toArray(), [
-        [2, 3, 4],
-        [0, 2, 4, 6]
+    assert.deepEqual(new Index(new Range(2, 5), new Range(0, 8, 2), 2, new Set([1,2])).toArray(), [
+      [2, 3, 4],
+      [0, 2, 4, 6],
+      [2],
+      [1, 2]
     ]);
 
-    assert.deepEqual(new Index(2, new Range(0, 8, 2), new Range(3,-1,-1), new Range(2, 4, 0)).toArray(), [
+    assert.deepEqual(new Index(2, new Range(0, 8, 2), new Range(3,-1,-1), new Range(2, 4, 0), [1, 2]).toArray(), [
       [2],
       [0, 2, 4, 6],
       [3, 2, 1, 0],
-      []
+      [],
+      [1, 2]
     ]);
   });
 
   it('valueOf should return the expanded array', function () {
-    assert.deepEqual(new Index(2, new Range(0, 8, 2), new Range(3,-1,-1)).valueOf(), [
+    assert.deepEqual(new Index(2, new Range(0, 8, 2), new Range(3,-1,-1), [1, 2], new Set([3, 4])).valueOf(), [
       [2],
       [0, 2, 4, 6],
-      [3, 2, 1, 0]
+      [3, 2, 1, 0],
+      [1, 2],
+      [3, 4]
     ]);
   });
 
