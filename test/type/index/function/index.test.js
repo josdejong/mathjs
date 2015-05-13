7e5980c812e71a2d1eb5a942c5f3a7aa142da625
Rogelio J. Baucells <rogelio.javier@baucells.net>
2015-05-13 17:24:38 -0400

    tests - partial

--- test/type/index/function/index.test.js
+++ test/type/index/function/index.test.js
@@ -2,24 +2,25 @@
 var assert = require('assert'),
     error = require('../../../../lib/error/index'),
     math = require('../../../../index'),
-    Range = math.type.Range;
+    Range = math.type.Range,
+    Set = math.type.Set;
 
 describe('index', function() {
 
   it('should create an index', function() {
     var index = math.index(new Range(2,6));
     assert.ok(index instanceof math.type.Index);
-    assert.deepEqual(index._ranges, [{start:2, end:6, step:1}]);
+    assert.deepEqual(index._dimensions, [{start:2, end:6, step:1}]);
 
     var index2 = math.index(new Range(0,4), new Range(5,2,-1));
     assert.ok(index2 instanceof math.type.Index);
-    assert.deepEqual(index2._ranges, [{start:0, end:4, step: 1}, {start:5, end: 2, step:-1}]);
+    assert.deepEqual(index2._dimensions, [{start:0, end:4, step: 1}, {start:5, end: 2, step:-1}]);
   });
 
   it('should create an index from bignumbers (downgrades to numbers)', function() {
     var index = math.index(new Range(math.bignumber(2), math.bignumber(6)), math.bignumber(3));
     assert.ok(index instanceof math.type.Index);
-    assert.deepEqual(index._ranges, [{start:2, end:6, step:1}, {start: 3, end: 4, step: 1}]);
+    assert.deepEqual(index._dimensions, [new Range(2, 6, 1), new Set([3])]);
   });
 
   it('should LaTeX index', function () {
