d589ce54f923f49b22048b4ebfc0eb3e8a9cf8e2
Devan Patel <devanp92@ufl.edu>
2015-08-31 17:56:43 -0400

    Adding more unit tests for Distance

--- test/function/geometry/distance.test.js
+++ test/function/geometry/distance.test.js
@@ -7,6 +7,14 @@ describe('distance', function() {
     assert.equal(math.distance(math.matrix([0,0]),math.matrix([10,10])), 14.142135623730951);
   });
 
+  it('should calculate distance for non-zero values', function() {
+    assert.equal(math.distance([1, 1], [10,10]), 12.727922061357855);
+    assert.equal(math.distance([-1, -1], [10,10]), 15.556349186104045);
+    assert.equal(math.distance([-1, 8], [5,10]), 6.324555320336759);
+    assert.equal(math.distance([-100, 60], [0,500]), 451.22056690713913);
+    assert.equal(math.distance([-100.78, 60.04], [0.3,500.09]), 451.5098768576386);
+  });
+
   it('should throw an error for incompatible parameter types', function() {
     assert.throws(function() {math.distance(0.5)}, TypeError);
     assert.throws(function() {math.distance('1')}, TypeError);
