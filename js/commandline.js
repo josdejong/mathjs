ba8041817d3054feebac9d5417280d511d037117
jos <wjosdejong@gmail.com>
2016-02-02 09:37:47 +0100

    Changed title of result

--- js/commandline.js
+++ js/commandline.js
@@ -506,7 +506,7 @@ function CommandLineEditor (params) {
       try {
         res = parser.eval(expr);
         resStr = math.format(res, { precision: 14 });
-        title = 'Floating point result: ' + math.format(res);
+        title = 'floating-point result: ' + math.format(res);
       }
       catch (err) {
         resStr = err.toString();
