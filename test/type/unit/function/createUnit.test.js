efaa989cad15616cc525f98218db057c40f32db0
Eric <ericman314@gmail.com>
2016-07-19 00:30:57 -0600

    Update API to accept objects and create multiple units

--- test/type/unit/function/createUnit.test.js
+++ test/type/unit/function/createUnit.test.js
@@ -14,7 +14,7 @@ describe('createUnit', function() {
     assert.equal(math.eval('50 in^2 to createUnit("bingo", 25 in^2)').toString(), '2 bingo');
   });
 
-  it('should accept a unit as second parameter', function () {
+  it('should accept a string as second parameter', function () {
     assert.equal(math.eval('50 in^2 to createUnit("zingo", "25 in^2")').toString(), '2 zingo');
   });
 
@@ -24,10 +24,25 @@ describe('createUnit', function() {
   });
 
   it('should accept options', function() {
-    math.eval('createUnit("whosit", 3.14 kN, {prefixes:"long"})');
+    math.eval('createUnit("whosit", { definition: 3.14 kN, prefixes:"long"})');
     assert.equal(math.eval('1e-9 whosit').toString(), '1 nanowhosit');
 
-    math.eval('createUnit("wheresit", 3.14 kN, {offset:2})');
+    math.eval('createUnit("wheresit", { definition: 3.14 kN, offset:2})');
     assert.equal(math.eval('1 wheresit to kN').toString(), '9.42 kN');
+  });
+
+  it('should create multiple units', function() {
+    math.eval('createUnit({"xfoo":{}, "xbar":{}, "xfoobar":"1 xfoo xbar"})');
+    assert.equal(math.eval('5 xfoo').toString(), '5 xfoo');
+  });
+
+  it.skip('should simplify created units', function() {
+    // TODO: New units do not have base units set, therefore simplifying is impossible. Figure out a way to create base units for created units.
+    assert.equal(math.eval('5 xfoo * 5 xbar').toString(), '25 xfoobar');
+  });
+
+  it('should override units', function() {
+    math.eval('createUnit({"bar": 1e12 Pa}, {"override":true})');
+    assert.equal(math.eval('1 bar to Pa').toString(), '1e+12 Pa');
   });
 });
