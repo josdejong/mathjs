842e6f89779be5eecdf312eb39d424218d7e17a1
Ahmed Elnaiem <aelnaiem@gmail.com>
2016-10-12 20:21:33 -0400

    adding tests and fixing code so the tests succeed

--- test/expression/step-solver/solveEquation.test.js
+++ test/expression/step-solver/solveEquation.test.js
@@ -3,6 +3,46 @@
 const assert = require('assert');
 const math = require('../../../index');
 
-describe('solveEquation', function () {
+const solveEquation = require('../../../lib/expression/step-solver/solveEquation.js');
 
+function testSolve(equationString, debug=false) {
+  const equation = solveEquation.createEquationFromString(equationString, '=');
+  const result = solveEquation.solveEquation(equation, debug);
+  if (debug) {
+    console.log(result[0])
+  }
+  return result[1]
+}
+
+describe('solveEquation', function () {
+    it('x = 1 -> x = 1', function () {
+      assert.equal(
+        testSolve('x = 1'),
+        'x = 1');
+    });
+    it('2 = x -> x = 2', function () {
+      assert.equal(
+        testSolve('2 = x'),
+        'x = 2');
+    });
+    it('x + 3 = 4 -> x = 1', function () {
+      assert.equal(
+        testSolve('x + 3 = 4'),
+        'x = 1');
+    });
+    it('2x - 3 = 0 -> x = 3 / 2', function () {
+      assert.equal(
+        testSolve('2x - 3 = 0'),
+        'x = 3 / 2');
+    });
+    it('2x/3 = 2 -> x = 3', function () {
+      assert.equal(
+        testSolve('2x/3 = 2'),
+        'x = 3');
+    });
+    // it('5x + (1/2)x = 27 -> x = 1', function () {
+    //   assert.equal(
+    //     testSolve('5x + (1/2)x = 27 '),
+    //     'x = 54/11');
+    // });
 });
