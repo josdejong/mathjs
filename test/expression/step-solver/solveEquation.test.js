2283d5ca89daddf03850ac26953bebe783b96641
Ahmed Elnaiem <aelnaiem@gmail.com>
2016-10-14 12:42:45 -0400

    adding our 4millionth question as a test for our solver

--- test/expression/step-solver/solveEquation.test.js
+++ test/expression/step-solver/solveEquation.test.js
@@ -67,6 +67,11 @@ describe('solveEquation', function () {
       testSolve('2x - 3 = x', '='),
       'x = 3');
   });
+  it('8 - 2a = a + 3 - 1 -> a = 3', function () {
+    assert.equal(
+      testSolve('8 - 2a = a + 3 - 1', '='),
+      'a = 2');
+  });
   // TODO(bug): x/(2/3) is not the same as (x/2)/3 or x/2/3
   // it('x/(2/3) = 1 -> x = 3/2', function () {
   //   assert.equal(
