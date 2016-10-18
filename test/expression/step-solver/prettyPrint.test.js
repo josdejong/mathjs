9b1cf7cc4182fc302b19c2b2fa099818f40ece7f
Ahmed Elnaiem <aelnaiem@gmail.com>
2016-10-18 14:34:56 -0400

    a few simple tests for the latex printer

--- test/expression/step-solver/prettyPrint.test.js
+++ test/expression/step-solver/prettyPrint.test.js
@@ -5,7 +5,7 @@ const math = require('../../../index');
 const prettyPrint = require('../../../lib/expression/step-solver/prettyPrint.js');
 const flatten = require('../../../lib/expression/step-solver/flattenOperands.js');
 
-describe('prettyPrint', function () {
+describe('prettyPrint asciimath', function () {
   it('2 + 3 + 4', function () {
     assert.deepEqual(
       prettyPrint(math.parse('2+3+4')),
@@ -20,5 +20,23 @@ describe('prettyPrint', function () {
     assert.deepEqual(
       prettyPrint(math.parse('2/3 x^2')),
       '2/3 x^2');
+  });
+});
+
+describe('prettyPrint latex', function () {
+  it('2 + 3 + 4', function () {
+    assert.deepEqual(
+      prettyPrint(math.parse('2+3+4'), true),
+      '2+3+4');
+  });
+  it('2 + (4 - x) + - 4', function () {
+    assert.deepEqual(
+      prettyPrint(math.parse('2 + (4 - x) + - 4'), true),
+      '2+\\left(4- x\\right)-4');
+  });
+  it('2/3 x^2', function () {
+    assert.deepEqual(
+      prettyPrint(math.parse('2/3 x^2'), true),
+      '\\frac{2}{3}~{ x}^{2}');
   });
 });
