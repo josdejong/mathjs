d45eca541842c32b4b1356b2dc4313b43cd60705
Harry Sarson <harry.sarson@hotmail.co.uk>
2018-05-13 23:19:52 +0100

    run tests on browserstack
    credentials must be set using travis's per repository settings.
    currently only chrome and firefox are tested, we should add tesing on ms edge and IE.
    
    Follows on from work in #1105

--- browser-tests/browserstack-karma.js
+++ browser-tests/browserstack-karma.js
@@ -7,6 +7,8 @@ module.exports = function(config) {
   config.set(Object.assign(baseConfig, {
     browsers: [
       'bs_firefox_android',
+      'bs_ie_11',
+      'bs_edge',
       'bs_chrome_mac',
     ],
     reporters: [
@@ -28,6 +30,20 @@ module.exports = function(config) {
         device: 'Samsung Galaxy S8',
         os: 'android',
         os_version: '7.0',
+      },
+      bs_ie_11: {
+        base: 'BrowserStack',
+        browser: 'IE',
+        browser_version: '11',
+        os: 'Windows',
+        os_version: '7',
+      },
+      bs_edge: {
+        base: 'BrowserStack',
+        browser: 'Edge',
+        browser_version: '16',
+        os: 'Windows',
+        os_version: '10',
       },
       bs_chrome_mac: {
         base: 'BrowserStack',
