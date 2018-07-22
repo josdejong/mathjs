2f0cb5de677c160b2a1cac4cc2c95de749f8c686
paulobuchsbaum <pbuchsbaum@globo.com>
2018-07-22 17:13:04 -0300

    typo fix
    typo fix

--- src/function/algebra/simplify.js
+++ src/function/algebra/simplify.js
@@ -57,7 +57,7 @@ function factory (type, config, load, typed, math) {
    *
    * Syntax:
    *
-   *     simplify(expr)n
+   *     simplify(expr)
    *     simplify(expr, rules)
    *     simplify(expr, rules, scope)
    *     simplify(expr, scope)
