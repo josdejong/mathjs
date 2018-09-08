d07ea3bd5cf1e1493b5fbae477a059d70d2eb002
jos <wjosdejong@gmail.com>
2018-09-08 15:21:49 +0200

    Fix cli tests

--- test-dist/cli/cli.test.js
+++ test-dist/cli/cli.test.js
@@ -10,14 +10,14 @@ function run (args, done) {
 describe('command line interface', function () {
   it('should sum numbers', function (done) {
     run('1+1', function (e, result) {
-      assert.strictEqual(result, 2)
+      assert.strictEqual(result, '2')
       done()
     })
   })
 
   it('should multiply matrices', function (done) {
     run('[1,2] * [3,4]', function (e, result) {
-      assert.strictEqual(result, 11)
+      assert.strictEqual(result, '11')
       done()
     })
   })
@@ -31,9 +31,7 @@ describe('command line interface', function () {
 
   it('should interpret scripts', function (done) {
     run('test-dist/cli/script1" "test-dist/cli/script2', function (e, result) {
-      result = result.split('\n')
-      assert.strictEqual(result[0], 2)
-      assert.strictEqual(result[1], 8)
+      assert.strictEqual(result, '2\n8\n')
       done()
     })
   })
