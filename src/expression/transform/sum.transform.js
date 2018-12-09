e45c8805b3e31977bb31045244160f027077fac8
bnlcas <bnlcas@gmail.com>
2018-12-09 04:09:36 -0800

    Add axis for sum request 1244 (#1310)
    * enable sum function to take an argument of dimension for a matrix fulfilling issue #1244
    
    * added sum along a dimension to the unit test coverage for sum
    
    * added information about sum along a dimension to the markdown
    
    * fixed typo in error message
    
    * made formatting chagnes for linter
    
    * add sum to transformed functions

---
+++ src/expression/transform/sum.transform.js
@@ -0,0 +1,39 @@
+'use strict'
+
+const errorTransform = require('./error.transform').transform
+const isCollection = require('../../utils/collection/isCollection')
+
+/**
+ * Attach a transform function to math.sum
+ * Adds a property transform containing the transform function.
+ *
+ * This transform changed the last `dim` parameter of function mean
+ * from one-based to zero based
+ */
+function factory (type, config, load, typed) {
+  const sum = load(require('../../function/statistics/sum'))
+
+  return typed('sum', {
+    '...any': function (args) {
+      // change last argument dim from one-based to zero-based
+      if (args.length === 2 && isCollection(args[0])) {
+        const dim = args[1]
+        if (type.isNumber(dim)) {
+          args[1] = dim - 1
+        } else if (type.isBigNumber(dim)) {
+          args[1] = dim.minus(1)
+        }
+      }
+
+      try {
+        return sum.apply(null, args)
+      } catch (err) {
+        throw errorTransform(err)
+      }
+    }
+  })
+}
+
+exports.name = 'sum'
+exports.path = 'expression.transform'
+exports.factory = factory
