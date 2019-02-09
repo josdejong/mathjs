049f80248d7df95160ccd7c6934b06e1bde0c78f
jos <wjosdejong@gmail.com>
2019-02-09 21:55:04 +0100

    Add embedded docs for `hasNumericValue`

--- src/expression/embeddedDocs/function/utils/isNumeric.js
+++ src/expression/embeddedDocs/function/utils/isNumeric.js
@@ -8,12 +8,13 @@ module.exports = {
     'Returns true when the input is a number, BigNumber, Fraction, or boolean.',
   'examples': [
     'isNumeric(2)',
+    'isNumeric("2")',
+    'hasNumericValue("2")',
     'isNumeric(0)',
     'isNumeric(bignumber(500))',
     'isNumeric(fraction(0.125))',
-    'isNumeric("3")',
     'isNumeric(2 + 3i)',
     'isNumeric([2.3, "foo", false])'
   ],
-  'seealso': ['isInteger', 'isZero', 'isNegative', 'isPositive', 'isNaN']
+  'seealso': ['isInteger', 'isZero', 'isNegative', 'isPositive', 'isNaN', 'hasNumericValue']
 }
