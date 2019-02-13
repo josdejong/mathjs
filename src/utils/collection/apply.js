b60598dbcb480778c7c04cbeabce8f0501870456
Benjamin Lucas <benjaminlucas@Benjamins-MBP.noise>
2019-02-12 20:14:43 -0800

    added apply function

--- src/utils/collection/apply.js
+++ src/utils/collection/apply.js
@@ -1,28 +1,80 @@
 'use strict'
 
-const isMatrix = require('./isMatrix')
+const arraySize = require('../array').size
+const isMatrix = require('../collection/isMatrix')
+const IndexError = require('../../error/IndexError')
 
 /**
- * Apply function that takes an array and returns a scalar to all arrays on a specific
- * axis of a multi dimensional array
- * @param {Array | Matrix} array
- * @param {Function} callback     The callback method is invoked with one
- *                                parameter: the current element in the array
- * @param {number | BigNumber} axis
+ * Apply function that takes an array and returns a Scalar
+ * along a given axis of a matrix or array.
+ * Returns a new matrix or array with one less dimension.
+ * @param {Array | Matrix} mat
+ * @param {number} dim
+ * @param {Function} callback
+ * @return {Array | Matrix} res
  */
-module.exports = function apply (array, callback, axis) {
-  if (isMatrix(array)) {
-    array = array.valueOf()
+module.exports = function (mat, dim, callback) {
+  const size = Array.isArray(mat) ? arraySize(mat) : mat.size()
+  if (dim < 0 || (dim >= size.length)) {
+    // TODO: would be more clear when throwing a DimensionError here
+    throw new IndexError(dim, size.length)
   }
 
-  var array_depth = 
-  for (let i = 0, ii = array.length; i < ii; i++) {
-    const value = array[i]
+  if (isMatrix(mat)) {
+    return mat.create(_reduce(mat.valueOf(), dim, callback))
+  } else {
+    return _reduce(mat, dim, callback)
+  }
+}
 
-    if (Array.isArray(value)) {
-      apply(value, callback)
+/**
+ * Recursively reduce a matrix
+ * @param {Array} mat
+ * @param {number} dim
+ * @param {Function} callback
+ * @returns {Array} ret
+ * @private
+ */
+function _reduce (mat, dim, callback) {
+  let i, ret, tran
+
+  if (dim <= 0) {
+    if (!Array.isArray(mat[0])) {
+      return callback(mat)
     } else {
-      callback(value)
+      tran = _switch(mat)
+      ret = []
+      for (i = 0; i < tran.length; i++) {
+        ret[i] = _reduce(tran[i], dim - 1, callback)
+      }
+      return ret
     }
+  } else {
+    ret = []
+    for (i = 0; i < mat.length; i++) {
+      ret[i] = _reduce(mat[i], dim - 1, callback)
+    }
+    return ret
   }
+}
+
+/**
+ * Transpose a matrix
+ * @param {Array} mat
+ * @returns {Array} ret
+ * @private
+ */
+function _switch (mat) {
+  const I = mat.length
+  const J = mat[0].length
+  let i, j
+  const ret = []
+  for (j = 0; j < J; j++) {
+    const tmp = []
+    for (i = 0; i < I; i++) {
+      tmp.push(mat[i][j])
+    }
+    ret.push(tmp)
+  }
+  return ret
 }
