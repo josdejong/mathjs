29c8fb8474eff9cf5fb8c2963e0cae19cb62da02
Kevin J <qaz111id@gmail.com>
2019-04-24 11:18:18 -0700

    Fix banner/license for custom bundling (#1488)
    * updated gulpfile so lib header sends correct version and date
    
    * change from watch to build
    
    * linted

--- gulpfile.js
+++ gulpfile.js
@@ -22,6 +22,7 @@ const REF_SRC = './lib/'
 const REF_DEST = './docs/reference/functions'
 const REF_ROOT = './docs/reference'
 const MATH_JS = DIST + '/' + FILE
+const COMPILED_HEADER = COMPILE_LIB + '/header.js'
 
 // read the version number from package.json
 function getVersion () {
@@ -133,6 +134,10 @@ function compile () {
     .pipe(babel())
     .pipe(gulp.dest(COMPILE_LIB))
 }
+function writeBanner (cb) {
+  fs.writeFileSync(COMPILED_HEADER, createBanner())
+  cb()
+}
 
 function minify (done) {
   const oldCwd = process.cwd()
@@ -224,4 +229,4 @@ gulp.task('watch', function watch () {
 })
 
 // The default task (called when you run `gulp`)
-gulp.task('default', gulp.series(bundle, compile, minify, validate, generateDocs))
+gulp.task('default', gulp.series(bundle, compile, writeBanner, minify, validate, generateDocs))
