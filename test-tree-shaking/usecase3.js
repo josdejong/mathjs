2dba84ab90ca86bc42c4a2a5850420204994041b
jos <wjosdejong@gmail.com>
2019-05-26 15:30:44 +0200

    Cleanup temporary test-tree-shaking folder

--- test-tree-shaking/usecase3.js
+++
@@ -1,8 +0,0 @@
-// Use case 3
-// use all functions in the expression parser
-
-import { evaluate } from '..'
-
-console.log('\nuse case 3')
-console.log(evaluate('sin(pi / 2) / 3'))
-// number 0.3333333333333333
--- test-tree-shaking/usecase5.js
+++
@@ -1,9 +0,0 @@
-// Use case 5
-// use a few functions with just number support
-
-import { divide, sin, pi } from '../number'
-
-console.log('\nuse case 5')
-console.log(divide(sin(divide(pi, 2)), 3))
-// sin(pi / 2) / 3 =
-// number 0.3333333333333333
