956c0f834758062799308b9ceee2f966dee987a4
Waseem Yusuf <waseem.yusuf.hossain@gmail.com>
2019-08-25 09:33:58 -0600

    Update combinationsWithRep.js
    Instead of always cancelling `n-1 factorial` from the denominator and ignoring `k factorial`, added a conditional to cancel the the larger of the two, therefore further reducing redundant calculations.

--- src/function/probability/combinationsWithRep.js
+++ src/function/probability/combinationsWithRep.js
@@ -41,14 +41,19 @@ export const createCombinationsWithRep = /* #__PURE__ */ factory(name, dependenc
         throw new TypeError('k must be less than or equal to n + k - 1')
       }
 
-      const prodrange = product(n, n + k - 1)
-      return prodrange / product(1, k)
+      if (k < n - 1) {
+        const prodrange = product(n, n + k - 1)
+        return prodrange / product(1, k)
+      }
+      const prodrange = product(k + 1, n + k - 1)
+      return prodrange / product(1, n - 1)
     },
 
     'BigNumber, BigNumber': function (n, k) {
       const BigNumber = n.constructor
-      let result, i
+      let max, result, i, ii
       const one = new BigNumber(1)
+      const nPlusKMinusOne = n.plus(k).minus(one)
 
       if (!isPositiveInteger(n) || !isPositiveInteger(k)) {
         throw new TypeError('Positive integer value expected in function combinationsWithRep')
@@ -57,9 +62,10 @@ export const createCombinationsWithRep = /* #__PURE__ */ factory(name, dependenc
         throw new TypeError('k must be less than or equal to n + k - 1 in function combinationsWithRep')
       }
 
-      const max = n.minus(one)
+      max = n.minus(one)
+      if (k.lt(max)) max = k
       result = one
-      for (i = one; i.lte(k); i = i.plus(1)) {
+      for (i = one, ii = nPlusKMinusOne.minus(max); i.lte(ii); i = i.plus(1)) {
         result = result.times(max.plus(i)).dividedBy(i)
       }
 
