6d0fbd5e9e9a24b460b8e799684105a9115f3a53
Arkajit Mandal <arkajitmandal@gmail.com>
2019-12-27 23:15:19 -0500

    eigs typed function written according to mathjs requirements

---
+++ src/function/matrix/eigs.js
@@ -0,0 +1,56 @@
+import { clone } from '../../utils/object'
+import { factory } from '../../utils/factory'
+
+const name = 'eigs'
+const dependencies = ['typed', 'matrix']
+
+export const createEigs = /* #__PURE__ */ factory(name, dependencies, ({ typed, matrix }) => {
+  /**
+   * Compute eigenvalue and eigenvector of a real symmetric matrix. 
+   * Only applicable to two dimensional symmetric matrices. Uses Jacobi  
+   * Algorithm.
+   *
+   * Syntax:
+   *
+   *     math.eigs(x)
+   *
+   * Examples:
+   *
+   *     const A = [[1, 1], [1, 1]]
+   *     math.eigs(A)               // returns {eigval: [0, 2],eigvec:[[0.5, -0.5],[0.5, 0.5]]}
+   *
+   * See also:
+   *
+   *     inv
+   *
+   * @param {Array | Matrix} x  Matrix to be diagonalized
+   * @return {Object}   The obtect containing eigenvalues and eigenvectors
+   */
+  const eigs = typed('eigs', {
+
+    Array: function (x) {
+      // use dense matrix implementation
+      return transpose(matrix(x)).valueOf()
+    },
+
+    Matrix: function (x) {
+      // matrix size
+      const size = x.size()
+
+      // result
+      let eigval 
+      let eigvec
+
+      // process dimensions
+      switch (size.length) {
+        case 1:   
+          return {eigval:[1],eigvec:[1]}
+
+        default:
+          // multi dimensional
+          throw new RangeError('Matrix must be a two dimensional array')
+      }
+    }
+  })
+})
+
