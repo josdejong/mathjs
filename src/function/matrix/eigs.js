bbfd7ab379a8af11f35cb5a98e89082d5c4df9f5
Arkajit Mandal <arkajitmandal@gmail.com>
2019-12-29 22:15:20 -0500

    lint and bug fix at eigs

--- src/function/matrix/eigs.js
+++ src/function/matrix/eigs.js
@@ -58,44 +58,45 @@ export const createEigs = /* #__PURE__ */ factory(name, dependencies, ({ typed, 
   // diagonalization implementation
   function diag (x, precision = 1E-12) {
     const N = x.length
-    let Ei = Array(N).fill(0) // eigenvalues
     const e0 = Math.abs(precision / N)
+    let psi
     let Sij = new Array(N)
     // Sij is Identity Matrix
     for (let i = 0; i < N; i++) {
       Sij[i] = Array(N).fill(0)
       Sij[i][i] = 1.0
     }
-    // initial error 
+    // initial error
     let Vab = getAij(x)
     while (Math.abs(Vab[1]) >= Math.abs(e0)) {
-      let i =  Vab[0][0]
-      let j =  Vab[0][1]
-      let psi = getTheta(x[i][i], x[j][j], x[i][j]); 
+      const i = Vab[0][0]
+      const j = Vab[0][1]
+      psi = getTheta(x[i][i], x[j][j], x[i][j])
       x = x1(x, psi, i, j)
       Sij = Sij1(Sij, psi, i, j)
       Vab = getAij(x)
     }
-    for (let i = 0; i<N; i++){
-      Ei[i] = Hij[i][i]
+    let Ei = Array(N).fill(0) // eigenvalues
+    for (let i = 0; i < N; i++) {
+      Ei[i] = x[i][i]
     }
     return [Ei,Sij]
   }
 
   // Rotation Matrix
   function Rot (theta) {
-    let s = Math.sin(theta);
-    let c = Math.cos(theta);
-    let Mat = [[c,s],[-s,c]];
+    const s = Math.sin(theta)
+    const c = Math.cos(theta)
+    let Mat = [[c, s], [-s, c]]
     return Mat
   }
 
   // get angle
   function getTheta (aii, ajj, aij) {
-    let  th = 0.0 
-    let denom = (ajj - aii);
-    if (Math.abs(denom) <= 1E-14){
-      th = Math.PI/4.0
+    let th = 0.0
+    const denom = (ajj - aii)
+    if (Math.abs(denom) <= 1E-14) {
+      th = Math.PI / 4.0
     }
     else {
       th = 0.5 * Math.atan(2.0 * aij / (ajj - aii) ) 
@@ -104,59 +105,73 @@ export const createEigs = /* #__PURE__ */ factory(name, dependencies, ({ typed, 
   }
   
   // update eigvec  
-  function Sij1 (Sij, theta,i,j) {
-    let N = Sij.length;
-    let c = Rot(theta)[0][0];
-    let s = Rot(theta)[0][1];
-    let Ski =  new Array(N).fill(0);
-    let Skj =  new Array(N).fill(0);
-    for (var k=0; k<N; k++){
-        Ski[k] = c * Sij[k][i] - s * Sij[k][j];
-        Skj[k] = s * Sij[k][i] + c * Sij[k][j];
+  function Sij1 (Sij, theta, i, j) {
+    const N = Sij.length;
+    const c = Rot(theta)[0][0]
+    const s = Rot(theta)[0][1]
+    var Ski =  new Array(N).fill(0)
+    var Skj =  new Array(N).fill(0)
+    for (var k = 0; k < N; k++){
+        Ski[k] = c * Sij[k][i] - s * Sij[k][j]
+        Skj[k] = s * Sij[k][i] + c * Sij[k][j]
     }
-    for (var k=0; k<N; k++){
-        Sij[k][i] =  Ski[k] ;
-        Sij[k][j] =  Skj[k] ;
+    for (var k = 0; k < N; k++) {
+        Sij[k][i] =  Ski[k] 
+        Sij[k][j] =  Skj[k] 
     }
-    return Sij;
+    return Sij
   }
 
   // update matrix 
-  function x1 (Hij, theta,i,j){
-    let N = Hij.length;
-    let c = Math.cos(theta);
-    let s = Math.sin(theta);
-    let c2 = c * c ; 
-    let s2 = s * s ; 
+  function x1 (Hij, theta, i, j) {
+    let N = Hij.length
+    const c = Math.cos(theta)
+    const s = Math.sin(theta)
+    let c2 = c * c 
+    let s2 = s * s  
     //var Ans = new Array(N).fill(Array(N).fill(0)); 
     let Aki =  new Array(N).fill(0);
     let Akj =  new Array(N).fill(0);
     // Aii
-    let Aii = c2 * Hij[i][i] - 2 * c * s * Hij[i][j] + s2 * Hij[j][j];
-    let Ajj = s2 * Hij[i][i] + 2 * c * s * Hij[i][j] + c2 * Hij[j][j];
+    let Aii = c2 * Hij[i][i] - 2 * c * s * Hij[i][j] + s2 * Hij[j][j]
+    let Ajj = s2 * Hij[i][i] + 2 * c * s * Hij[i][j] + c2 * Hij[j][j]
     // 0  to i
-    for (var k=0; k<N; k++){
-        Aki[k] =  c * Hij[i][k] - s * Hij[j][k] ;
-        //Ans[i][k] =  Ans[k][i] ; 
-        Akj[k] =  s * Hij[i][k] + c * Hij[j][k] ;
-        //Ans[j][k] =  Ans[k][j] ; 
+    for (var k=0; k < N; k++){
+      Aki[k] =  c * Hij[i][k] - s * Hij[j][k] 
+      Akj[k] =  s * Hij[i][k] + c * Hij[j][k] 
     }
     // Modify Hij
-    Hij[i][i] = Aii;
-    Hij[j][j] = Ajj;
-    Hij[i][j] = 0;
-    Hij[j][i] = 0;
+    Hij[i][i] = Aii
+    Hij[j][j] = Ajj
+    Hij[i][j] = 0
+    Hij[j][i] = 0
     // 0  to i
-    for (var k=0; k<N; k++){
-        if (k!==i && k!==j){
-            Hij[i][k] = Aki[k];
-            Hij[k][i] = Aki[k];
-            Hij[j][k] = Akj[k];
-            Hij[k][j] = Akj[k];
-        }
+    for (var k=0; k < N; k++) {
+      if (k !== i && k !== j) {
+        Hij[i][k] = Aki[k]
+        Hij[k][i] = Aki[k]
+        Hij[j][k] = Akj[k]
+        Hij[k][j] = Akj[k]
+      }
     }
     return Hij;
  }
+
+  // get max off-diagonal value from Upper Diagonal
+  function getAij(Mij) {
+    var N = Mij.length
+    var maxMij = 0.0 
+    var maxIJ = [0, 1]
+    for (var i = 0; i < N; i++) {
+      for (var j = i + 1; j < N; j++) { 
+        if (Math.abs(maxMij) < Math.abs(Mij[i][j])) {
+          maxMij = Math.abs(Mij[i][j])
+          maxIJ  = [i,j]
+        } 
+      }
+  }
+  return [maxIJ, maxMij]
+  }
 
   return eigs
 })
