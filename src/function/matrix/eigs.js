c023f1a8bc5997a8c253c6bc8da66d5c6f62b0a0
Arkajit Mandal <arkajitmandal@gmail.com>
2019-12-29 22:32:43 -0500

    linting passed in eigs

--- src/function/matrix/eigs.js
+++ src/function/matrix/eigs.js
@@ -76,18 +76,18 @@ export const createEigs = /* #__PURE__ */ factory(name, dependencies, ({ typed, 
       Sij = Sij1(Sij, psi, i, j)
       Vab = getAij(x)
     }
-    let Ei = Array(N).fill(0) // eigenvalues
+    var Ei = Array(N).fill(0) // eigenvalues
     for (let i = 0; i < N; i++) {
       Ei[i] = x[i][i]
     }
-    return [Ei,Sij]
+    return [Ei, Sij]
   }
 
   // Rotation Matrix
   function Rot (theta) {
     const s = Math.sin(theta)
     const c = Math.cos(theta)
-    let Mat = [[c, s], [-s, c]]
+    const Mat = [[c, s], [-s, c]]
     return Mat
   }
 
@@ -97,48 +97,47 @@ export const createEigs = /* #__PURE__ */ factory(name, dependencies, ({ typed, 
     const denom = (ajj - aii)
     if (Math.abs(denom) <= 1E-14) {
       th = Math.PI / 4.0
+    } else {
+      th = 0.5 * Math.atan(2.0 * aij / (ajj - aii))
     }
-    else {
-      th = 0.5 * Math.atan(2.0 * aij / (ajj - aii) ) 
-    }
-    return th 
+    return th
   }
-  
-  // update eigvec  
+
+  // update eigvec
   function Sij1 (Sij, theta, i, j) {
-    const N = Sij.length;
+    const N = Sij.length
     const c = Rot(theta)[0][0]
     const s = Rot(theta)[0][1]
-    var Ski =  new Array(N).fill(0)
-    var Skj =  new Array(N).fill(0)
-    for (var k = 0; k < N; k++){
-        Ski[k] = c * Sij[k][i] - s * Sij[k][j]
-        Skj[k] = s * Sij[k][i] + c * Sij[k][j]
+    var Ski = new Array(N).fill(0)
+    var Skj = new Array(N).fill(0)
+    for (let k = 0; k < N; k++) {
+      Ski[k] = c * Sij[k][i] - s * Sij[k][j]
+      Skj[k] = s * Sij[k][i] + c * Sij[k][j]
     }
-    for (var k = 0; k < N; k++) {
-        Sij[k][i] =  Ski[k] 
-        Sij[k][j] =  Skj[k] 
+    for (let k = 0; k < N; k++) {
+      Sij[k][i] = Ski[k]
+      Sij[k][j] = Skj[k]
     }
     return Sij
   }
 
-  // update matrix 
+  // update matrix
   function x1 (Hij, theta, i, j) {
-    let N = Hij.length
+    const N = Hij.length
     const c = Math.cos(theta)
     const s = Math.sin(theta)
-    let c2 = c * c 
-    let s2 = s * s  
-    //var Ans = new Array(N).fill(Array(N).fill(0)); 
-    let Aki =  new Array(N).fill(0);
-    let Akj =  new Array(N).fill(0);
-    // Aii
-    let Aii = c2 * Hij[i][i] - 2 * c * s * Hij[i][j] + s2 * Hij[j][j]
-    let Ajj = s2 * Hij[i][i] + 2 * c * s * Hij[i][j] + c2 * Hij[j][j]
+    const c2 = c * c
+    const s2 = s * s
+    //  var Ans = new Array(N).fill(Array(N).fill(0));
+    var Aki = new Array(N).fill(0)
+    var Akj = new Array(N).fill(0)
+    //  Aii
+    const Aii = c2 * Hij[i][i] - 2 * c * s * Hij[i][j] + s2 * Hij[j][j]
+    const Ajj = s2 * Hij[i][i] + 2 * c * s * Hij[i][j] + c2 * Hij[j][j]
     // 0  to i
-    for (var k=0; k < N; k++){
-      Aki[k] =  c * Hij[i][k] - s * Hij[j][k] 
-      Akj[k] =  s * Hij[i][k] + c * Hij[j][k] 
+    for (let k = 0; k < N; k++) {
+      Aki[k] = c * Hij[i][k] - s * Hij[j][k]
+      Akj[k] = s * Hij[i][k] + c * Hij[j][k]
     }
     // Modify Hij
     Hij[i][i] = Aii
@@ -146,7 +145,7 @@ export const createEigs = /* #__PURE__ */ factory(name, dependencies, ({ typed, 
     Hij[i][j] = 0
     Hij[j][i] = 0
     // 0  to i
-    for (var k=0; k < N; k++) {
+    for (let k = 0; k < N; k++) {
       if (k !== i && k !== j) {
         Hij[i][k] = Aki[k]
         Hij[k][i] = Aki[k]
@@ -154,23 +153,23 @@ export const createEigs = /* #__PURE__ */ factory(name, dependencies, ({ typed, 
         Hij[k][j] = Akj[k]
       }
     }
-    return Hij;
- }
+    return Hij
+  }
 
   // get max off-diagonal value from Upper Diagonal
-  function getAij(Mij) {
+  function getAij (Mij) {
     var N = Mij.length
-    var maxMij = 0.0 
+    var maxMij = 0.0
     var maxIJ = [0, 1]
     for (var i = 0; i < N; i++) {
-      for (var j = i + 1; j < N; j++) { 
+      for (var j = i + 1; j < N; j++) {
         if (Math.abs(maxMij) < Math.abs(Mij[i][j])) {
           maxMij = Math.abs(Mij[i][j])
-          maxIJ  = [i,j]
-        } 
+          maxIJ = [i, j]
+        }
       }
-  }
-  return [maxIJ, maxMij]
+    }
+    return [maxIJ, maxMij]
   }
 
   return eigs
