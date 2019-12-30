04158dcf818941a923fee2ad42bd70534c2e5e3b
Arkajit Mandal <arkajitmandal@gmail.com>
2019-12-29 22:43:23 -0500

    tests for input in eigs

--- test/unit-tests/function/matrix/eigs.test.js
+++ test/unit-tests/function/matrix/eigs.test.js
@@ -6,5 +6,9 @@ describe('eigs', function () {
   it('should only accept a square matrix', function () {
     assert.throws(function () { eigs(math.matrix([[1, 2, 3], [4, 5, 6]])) }, /Matrix must be square/)
     assert.throws(function () { eigs([[1, 2, 3], [4, 5, 6]]) }, /Matrix must be square/)
+    assert.throws(function () { eigs([[1, 2], [4, 5, 6]]) }, /DimensionError: Dimension mismatch/)
+    assert.throws(function () { eigs([4, 5, 6]) }, /Matrix must be square/)
+    assert.throws(function () { eigs(1.0) }, /TypeError: Unexpected type of argument/)
+    assert.throws(function () { eigs('random') }, /TypeError: Unexpected type of argument/)
   })
 })
