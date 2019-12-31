76b18cb8bc1f77d65d55ffc6903e2e39b7fb4cd6
Arkajit Mandal <arkajitmandal@gmail.com>
2019-12-31 12:12:45 -0500

    embedding dos for eigs

---
+++ src/expression/embeddedDocs/function/matrix/eigs.js
@@ -0,0 +1,14 @@
+export const eigsDocs = {
+  name: 'eigs',
+  category: 'Matrix',
+  syntax: [
+    'eigs(x)'
+  ],
+  description: 'Calculate the inverse of a matrix',
+  examples: [
+    'inv([[5, 2.3], [2.3, 1]])'
+  ],
+  seealso: [
+    'inv'
+  ]
+}
