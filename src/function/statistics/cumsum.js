3848735d87f96626d32e521a244bb02dbc5b50a7
Hjortur Jonasson <hjorturjonasson@gmail.com>
2020-03-10 14:08:05 +0100

    Move switch to utilities, finish tests

--- src/function/statistics/cumsum.js
+++ src/function/statistics/cumsum.js
@@ -1,13 +1,14 @@
-import { containsCollections, deepForEach, reduce } from '../../utils/collection'
+import { containsCollections } from '../../utils/collection'
 import { factory } from '../../utils/factory'
+import { _switch } from '../../utils/switch'
 import { improveErrorMessage } from './utils/improveErrorMessage'
-import { noBignumber, noFraction } from '../../utils/noop'
+import { isUnit, isMatrix } from '../../utils/is'
 
 const name = 'cumsum'
-const dependencies = ['typed', 'add']
+const dependencies = ['typed', 'add', '?Unit']
 
-export const createCumSum = /* #__PURE__ */ factory(name, dependencies, ({ typed, add }) => {
-    /**
+export const createCumSum = /* #__PURE__ */ factory(name, dependencies, ({ typed, add, Unit }) => {
+  /**
      * Compute the cumulative sum of a matrix or a list with values.
      * In case of a (multi dimensional) array or matrix, the cumulative sum of
      * across a certain dimension will be calculated.
@@ -30,84 +31,77 @@ export const createCumSum = /* #__PURE__ */ factory(name, dependencies, ({ typed
      * @param {... *} args  A single matrix or or multiple scalar values
      * @return {*} The cumulative sum of all values
      */
-    return typed(name, {
-        // sum([a, b, c, d, ...])
-        'Array | Matrix': _cumsum,
+  return typed(name, {
+    // sum([a, b, c, d, ...])
+    'Array | Matrix': _cumsum,
 
-        // sum([a, b, c, d, ...], dim)
-        'Array | Matrix, number | BigNumber': _ncumSumDim,
+    // sum([a, b, c, d, ...], dim)
+    'Array | Matrix, number | BigNumber': _ncumSumDim,
 
-        // cumsum(a, b, c, d, ...)
-        '...': function(args) {
-            if (containsCollections(args)) {
-                throw new TypeError('All values expected to be scalar in function cumsum')
-            }
+    // cumsum(a, b, c, d, ...)
+    '...': function (args) {
+      if (containsCollections(args)) {
+        throw new TypeError('All values expected to be scalar in function cumsum')
+      }
 
-            return _cumsum(args)
-        }
-    })
+      return _cumsum(args)
+    }
+  })
 
-    /**
-     * Recursively calculate the sum of an n-dimensional array
+  /**
+     * Recursively calculate the cumulative sum of an n-dimensional array
      * @param {Array} array
-     * @return {number} sum
+     * @return {number} cumsum
      * @private
      */
-    function _cumsum(array) {
-        return array ? array.map((sum => value => sum = add(sum, value))(0)) : []
+  function _cumsum (array) {
+    try {return array ? _cumsummapRENAME(array) : [] // eslint-disable-line
+    } catch (err) {
+      throw improveErrorMessage(err, name)
     }
+  }
 
-    function _ncumSumDim(array, dim) {
-        try {
-            return _cumsummap(array, dim)
-        } catch (err) {
-            throw improveErrorMessage(err, name)
-        }
+  function _cumsummapRENAME (array) {
+    const arrayIsMatrix = isMatrix(array)
+    const tempArray = arrayIsMatrix ? array.valueOf() : array
+    const cumsumArray = tempArray.map((sum => value => sum = add(sum, value))(_initialCumsumValue(tempArray[0]))) // eslint-disable-line
+    return cumsumArray
+  }
+
+  function _initialCumsumValue (value) {
+    return isUnit(value) ? new Unit(0, value.units[0].unit.name) : 0
+  }
+
+  function _ncumSumDim (array, dim) {
+    try {
+      return _cumsummap(array, dim)
+    } catch (err) {
+      throw improveErrorMessage(err, name)
     }
-    /* TODO: Get this function from collection.js*/
-    /**
-     * Transpose a matrix
-     * @param {Array} mat
-     * @returns {Array} ret
-     * @private
-     */
-    function _switch(mat) {
-        const I = mat.length
-        const J = mat[0].length
-        let i, j
-        const ret = []
-        for (j = 0; j < J; j++) {
-            const tmp = []
-            for (i = 0; i < I; i++) {
-                tmp.push(mat[i][j])
-            }
-            ret.push(tmp)
+  }
+
+  /* Possible TODO: Refactor _reduce in collection.js to be able to work here as well */
+  function _cumsummap (mat, dim) {
+    let i, ret, tran
+
+    if (dim <= 0) {
+      const initialValue = mat[0][0]
+      if (!Array.isArray(initialValue)) {
+        return _cumsummapRENAME(mat) // eslint-disable-line
+      } else {
+        tran = _switch(mat)
+        ret = []
+        for (i = 0; i < tran.length; i++) {
+          ret[i] = _cumsummap(tran[i], dim - 1)
         }
         return ret
+      }
+    } else {
+      ret = []
+      for (i = 0; i < mat.length; i++) {
+        ret[i] = _cumsummap(mat[i], dim - 1)
+      }
+      return ret
     }
-
-    /*Possible TODO: Refactor _reduce in collection.js to be able to work here as well*/
-    function _cumsummap(mat, dim) {
-        let i, ret, tran
-
-        if (dim <= 0) {
-            if (!Array.isArray(mat[0][0])) {
-                return mat.map((sum => value => sum = add(sum, value))(0))
-            } else {
-                tran = _switch(mat)
-                ret = []
-                for (i = 0; i < tran.length; i++) {
-                    ret[i] = _cumsummap(tran[i], dim - 1)
-                }
-                return ret
-            }
-        } else {
-            ret = []
-            for (i = 0; i < mat.length; i++) {
-                ret[i] = _cumsummap(mat[i], dim - 1)
-            }
-            return ret
-        }
-    }
-
+  }
 })
