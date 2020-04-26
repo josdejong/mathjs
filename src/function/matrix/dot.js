879da3fa82b21ab87103a3f66e67292da3fd9af6
GirkovArpa <girkovarpa@gmail.com>
2020-04-26 16:13:44 -0500

    wasm dot prod

--- src/function/matrix/dot.js
+++ src/function/matrix/dot.js
@@ -61,10 +61,21 @@ export const createDot = /* #__PURE__ */ factory(name, dependencies, ({ typed, a
     if (xSize[0] !== ySize[0]) throw new RangeError('Vectors must have equal length (' + xSize[0] + ' != ' + ySize[0] + ')')
     if (len === 0) throw new RangeError('Cannot calculate the dot product of empty vectors')
 
-    let prod = 0
+    const wasmSrc = new Uint8Array([0, 97, 115, 109, 1, 0, 0, 0, 1, 8, 1, 96, 3, 127, 127, 127, 1, 124, 3, 2, 1, 0, 5, 4, 1, 1, 1, 100, 7, 16, 2, 6, 109, 101, 109, 111, 114, 121, 2, 0, 3, 100, 111, 116, 0, 0, 10, 60, 1, 58, 2, 2, 127, 1, 124, 32, 2, 65, 0, 74, 4, 64, 3, 64, 32, 5, 32, 0, 32, 3, 65, 3, 116, 34, 4, 106, 43, 3, 0, 32, 1, 32, 4, 106, 43, 3, 0, 162, 160, 33, 5, 32, 3, 65, 1, 106, 34, 3, 32, 2, 71, 13, 0, 11, 11, 32, 5, 11, 11, 9, 1, 0, 65, 128, 12, 11, 2, 160, 6])
+    const wasmModule = new WebAssembly.Module(wasmSrc)
+    const wasmInst = new WebAssembly.Instance(wasmModule)
+    const { memory, dot } = wasmInst.exports
+
+    while (len * Float64Array.BYTES_PER_ELEMENT > memory.buffer.byteLength) memory.grow(1)
+
+    const float64array = new Float64Array(memory.buffer, 0, len * 2)
+
     for (let i = 0; i < len; i++) {
-      prod = add(prod, multiply(x[i], y[i]))
+      float64array[i] = x[i]
+      float64array[i + len] = y[i]
     }
+
+    const prod = dot(0, len * float64array.BYTES_PER_ELEMENT, len)
 
     return prod
   }
