6fac1a7b10665a221ab5de521c08fa9fcf959eb1
Colin Holzman <colin.holzman@protonmail.com>
2020-10-14 16:07:59 -0400

    start extending bitwise operators for Number for values greater than 32 bits

--- src/plain/number/bitwise.js
+++ src/plain/number/bitwise.js
@@ -3,11 +3,21 @@ import { isInteger } from '../../utils/number'
 const n1 = 'number'
 const n2 = 'number, number'
 
+function largerThan32Bits(x) {
+  return x > 2 ** 31 - 1 || x < -(2 ** 31)
+}
+
 export function bitAndNumber (x, y) {
   if (!isInteger(x) || !isInteger(y)) {
     throw new Error('Integers expected in function bitAnd')
   }
-
+  if (largerThan32Bits(x) || largerThan32Bits(y)) {
+    const xH = x / 2 ** 32
+    const yH = x / 2 ** 32
+    const xL = x & 0xffffffff
+    const yL = y & 0xffffffff
+    return (xH & yH) * 2 ** 32 + xL & yL
+  }
   return x & y
 }
 bitAndNumber.signature = n2
