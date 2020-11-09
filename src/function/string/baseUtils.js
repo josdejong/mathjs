9f816ce311c0497464b138e13cefe5775bded72f
Colin Holzman <colin.holzman@protonmail.com>
2020-11-09 13:31:28 -0500

    Attempted to extend bin, oct, hex format functions to work with BigNumber, but not working fully.
    Apparently Decimal.js toString method doesn't take a base parameter like js number does. I guess I have to do the formatting myself.

--- src/function/string/baseUtils.js
+++ src/function/string/baseUtils.js
@@ -1,7 +1,6 @@
 import { factory } from '../../utils/factory'
-import { isInteger } from '../../utils/number'
 
-function baseFormatter (base) {
+function baseFormatter (base, {typed, add, subtract, pow, smaller, larger, unaryMinus, bignumber, isInteger}) {
   const prefixes = { 2: '0b', 8: '0o', 16: '0x' }
   const prefix = prefixes[base]
   return function (n, size) {
@@ -12,27 +11,27 @@ function baseFormatter (base) {
       if (!isInteger(size)) {
         throw new Error('size must be an integer')
       }
-      if (n > 2 ** (size - 1) - 1 || n < -(2 ** (size - 1))) {
+      if (larger(n, subtract(pow(bignumber(2), size - 1), 1)) || smaller(n, unaryMinus(pow(bignumber(2), (size - 1))))) {
         throw new Error(`Value must be in range [-2^${size-1}, 2^${size-1}-1]`)
       }
       if (!isInteger(n)) {
         throw new Error('Value must be an integer')
       }
-      if (n < 0) {
-        n = n + 2 ** size
+      if (smaller(n, 0)) {
+        n = add(n, pow(2, size))
       }
     }
     return `${prefix}${n.toString(base)}`
   }
 }
 
-const dependencies = ['typed']
+const dependencies = ['typed', 'add', 'subtract', 'pow', 'smaller', 'larger', 'unaryMinus', 'bignumber', 'isInteger']
 
 export function createBaseFormatterFactory (name, base) {
-  return factory(name, dependencies, ({ typed }) => {
-    return typed(name, {
-      number: baseFormatter(base),
-      'number, number': baseFormatter(base)
+  return factory(name, dependencies, (dependencies) => {
+    return dependencies.typed(name, {
+      'number | BigNumber': baseFormatter(base, dependencies),
+      'number | BigNumber, number': baseFormatter(base, dependencies)
     })
   })
 }
