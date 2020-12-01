15627b3cd3f53057c9626886e56000d4e49d37ad
redbar0n <redbar0n@users.noreply.github.com>
2020-12-01 17:08:04 +0100

    Update description to be more general.

--- src/function/utils/clone.js
+++ src/function/utils/clone.js
@@ -6,7 +6,7 @@ const dependencies = ['typed']
 
 export const createClone = /* #__PURE__ */ factory(name, dependencies, ({ typed }) => {
   /**
-   * Clone an object. Arrays will be deep copied.
+   * Clone an object. Will make a deep copy of the data.
    *
    * Syntax:
    *
@@ -17,7 +17,7 @@ export const createClone = /* #__PURE__ */ factory(name, dependencies, ({ typed 
    *    math.clone(3.5)                   // returns number 3.5
    *    math.clone(math.complex('2-4i') // returns Complex 2 - 4i
    *    math.clone(math.unit(45, 'deg'))  // returns Unit 45 deg
-   *    math.clone([[1, 2], [3, 4]])      // returns Array [[1, 2], [3, 4]], as a deep copy
+   *    math.clone([[1, 2], [3, 4]])      // returns Array [[1, 2], [3, 4]]
    *    math.clone("hello world")         // returns string "hello world"
    *
    * @param {*} x   Object to be cloned
