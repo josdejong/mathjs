50a091a7853a7c6005aead14e38918d37495f7e4
Flaviu Tamas <tamasflaviu@gmail.com>
2020-12-20 15:40:20 -0500

    Add LRU to memoize function

--- src/utils/function.js
+++ src/utils/function.js
@@ -1,5 +1,7 @@
 // function utils
 
+import { lruQueue } from './lruQueue.js'
+
 /**
  * Memoize a given function by caching the computed result.
  * The cache of a memoized function can be cleared by deleting the `cache`
@@ -7,26 +9,39 @@
  *
  * @param {function} fn                     The function to be memoized.
  *                                          Must be a pure function.
- * @param {function(args: Array)} [hasher]  A custom hash builder.
- *                                          Is JSON.stringify by default.
+ * @param {Object} [options]
+ * @param {function(args: Array): string} [options.hasher]
+ *    A custom hash builder. Is JSON.stringify by default.
+ * @param {number | undefined} [options.limit]
+ *    Maximum number of values that may be cached. Undefined indicates
+ *    unlimited (default)
  * @return {function}                       Returns the memoized function
  */
-export function memoize (fn, hasher) {
+export function memoize (fn, { hasher, limit } = {}) {
+  limit = limit == null ? Number.POSITIVE_INFINITY : limit
+  hasher = hasher == null ? JSON.stringify : hasher
+
   return function memoize () {
     if (typeof memoize.cache !== 'object') {
-      memoize.cache = {}
+      memoize.cache = {
+        values: {},
+        lru: lruQueue(limit || Number.POSITIVE_INFINITY)
+      }
     }
 
-    const args = []
-    for (let i = 0; i < arguments.length; i++) {
-      args[i] = arguments[i]
+    const args = [...arguments]
+    const hash = hasher(args)
+
+    if (hash in memoize.cache.values) {
+      memoize.cache.lru.hit(hash)
+      return memoize.cache.values[hash]
     }
 
-    const hash = hasher ? hasher(args) : JSON.stringify(args)
-    if (!(hash in memoize.cache)) {
-      memoize.cache[hash] = fn.apply(fn, args)
-    }
-    return memoize.cache[hash]
+    const newVal = fn.apply(fn, args)
+    memoize.cache.values[hash] = newVal
+    delete memoize.cache.values[memoize.cache.lru.hit(hash)]
+
+    return newVal
   }
 }
 
---
+++ src/utils/lruQueue.js
@@ -0,0 +1,50 @@
+// (c) 2018, Mariusz Nowak
+// SPDX-License-Identifier: ISC
+// Derived from https://github.com/medikoo/lru-queue
+export function lruQueue (limit) {
+  let size = 0
+  let base = 1
+  let queue = Object.create(null)
+  let map = Object.create(null)
+  let index = 0
+  const del = function (id) {
+    const oldIndex = map[id]
+    if (!oldIndex) return
+    delete queue[oldIndex]
+    delete map[id]
+    --size
+    if (base !== oldIndex) return
+    if (!size) {
+      index = 0
+      base = 1
+      return
+    }
+    while (!hasOwnProperty.call(queue, ++base)) continue
+  }
+  limit = Math.abs(limit)
+  return {
+    hit: function (id) {
+      const oldIndex = map[id]; const nuIndex = ++index
+      queue[nuIndex] = id
+      map[id] = nuIndex
+      if (!oldIndex) {
+        ++size
+        if (size <= limit) return undefined
+        id = queue[base]
+        del(id)
+        return id
+      }
+      delete queue[oldIndex]
+      if (base !== oldIndex) return undefined
+      while (!hasOwnProperty.call(queue, ++base)) continue
+      return undefined
+    },
+    delete: del,
+    clear: function () {
+      size = index = 0
+      base = 1
+      queue = Object.create(null)
+      map = Object.create(null)
+    }
+  }
+};
