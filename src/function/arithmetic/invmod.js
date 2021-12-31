4ec1804581b94f3e5d50ad3d2d85548f1dc77e35
thetazero <lesha.seletskiy@gmail.com>
2021-12-31 11:54:19 -0800

    implement (most) suggestions

--- src/function/arithmetic/invmod.js
+++ src/function/arithmetic/invmod.js
@@ -1,9 +1,9 @@
 import { factory } from '../../utils/factory.js'
 
 const name = 'invmod'
-const dependencies = ['typed', 'config', 'BigNumber', 'xgcd', 'equal', 'smaller', 'mod', 'floor', 'add']
+const dependencies = ['typed', 'config', 'BigNumber', 'xgcd', 'equal', 'smaller', 'mod', 'add', 'isInteger']
 
-export const createInvmod = /* #__PURE__ */ factory(name, dependencies, ({ typed, config, BigNumber, xgcd, equal, smaller, mod, floor, add }) => {
+export const createInvmod = /* #__PURE__ */ factory(name, dependencies, ({ typed, config, BigNumber, xgcd, equal, smaller, mod, add, isInteger }) => {
   /**
    * Calculate the (modular) multiplicative inverse of a modulo b. Solution to the equation `ax ≣ 1 (mod b)`
    * See https://en.wikipedia.org/wiki/Modular_multiplicative_inverse.
@@ -28,30 +28,20 @@ export const createInvmod = /* #__PURE__ */ factory(name, dependencies, ({ typed
    *                              where `invmod(a,b)*a ≣ 1 (mod b)`
    */
   return typed(name, {
-    'number, number': (a, b) => {
-      if (a !== floor(a) || b !== floor(b)) throw new Error('Parameters in function invmod must be integer numbers')
-      a = a % b
-      if (b === 0) throw new Error('Divisor must be non zero')
-      let res = xgcd(a, b)
-      if (config.matrix === 'Matrix') res = res._data
-      let [gcd, inv] = res
-      console.log(gcd, inv)
-      if (gcd !== 1) return NaN
-      inv = inv % b
-      if (inv < 0) inv += b
-      return inv
-    },
-    'BigNumber, BigNumber': (a, b) => {
-      if (!a.isInt() || !b.isInt()) throw new Error('Parameters in function invmod must be integer numbers')
-      a = mod(a, b)
-      if (equal(b, BigNumber(0))) throw new Error('Divisor must be non zero')
-      let res = xgcd(a, b)
-      if (config.matrix === 'Matrix') res = res._data
-      let [gcd, inv] = res
-      if (!equal(gcd, BigNumber(1))) return NaN
-      inv = mod(inv, b)
-      if (smaller(inv, BigNumber(0))) inv = add(inv, b)
-      return inv
-    }
+    'number, number': invmod,
+    'BigNumber, BigNumber': invmod
   })
+
+  function invmod (a, b) {
+    if (!isInteger(a) || !isInteger(b)) throw new Error('Parameters in function invmod must be integer numbers')
+    a = mod(a, b)
+    if (equal(b, 0)) throw new Error('Divisor must be non zero')
+    let res = xgcd(a, b)
+    res = res.valueOf()
+    let [gcd, inv] = res
+    if (!equal(gcd, BigNumber(1))) return NaN
+    inv = mod(inv, b)
+    if (smaller(inv, BigNumber(0))) inv = add(inv, b)
+    return inv
+  }
 })
