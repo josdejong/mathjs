7beac55201f73bfe2c01615b758069e0f15dddfd
thetazero <lesha.seletskiy@gmail.com>
2022-01-15 02:40:46 -0800

    add invmod (modular multiplicative inverse) (#2368)
    * add invmod (modular multiplicative inverse)
    
    * implement (most) suggestions
    
    * style error
    
    * fix NaN tests
    
    Co-authored-by: Jos de Jong <wjosdejong@gmail.com>

---
+++ src/expression/embeddedDocs/function/arithmetic/invmod.js
@@ -0,0 +1,14 @@
+export const invmodDocs = {
+  name: 'invmod',
+  category: 'Arithmetic',
+  syntax: [
+    'invmod(a, b)'
+  ],
+  description: 'Calculate the (modular) multiplicative inverse of a modulo b. Solution to the equation ax ≣ 1 (mod b)',
+  examples: [
+    'invmod(8, 12)=NaN',
+    'invmod(7, 13)=2',
+    'math.invmod(15151, 15122)=10429'
+  ],
+  seealso: ['gcd', 'xgcd']
+}
