2051fb141265457878d0b6855ce5143952512b88
Glen Whitney <glen@studioinfinity.org>
2022-04-04 08:51:41 -0700

    fix: Allow base argument to `log` in number-only module (#2515)
    Provides a number-only factory for `log` with both signatures 'number'
      and 'number,number'.
    
      Resolves #2514.

--- test/node-tests/commonjsAppNumberOnly.cjs
+++ test/node-tests/commonjsAppNumberOnly.cjs
@@ -1,4 +1,6 @@
-const { sqrt, format } = require('../../lib/cjs/number.js')
+const { e, sqrt, format, log } = require('../../lib/cjs/number.js')
 
 console.log(format(sqrt(4)))
 console.log(format(sqrt(-4)))
+console.log(format(log(e * e)))
+console.log(format(log(625, 5)))
