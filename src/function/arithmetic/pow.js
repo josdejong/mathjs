7480c11bf70bd3b267656dcbbfaa554f6d0f4e24
HanchaiN <85230240+HanchaiN@users.noreply.github.com>
2022-04-09 02:15:54 +0700

    feat: implement negative integer exponents for square matrices (#2517)
      Resolves #2463
    
    Co-authored-by: Glen Whitney <glen@studioinfinity.org>

--- src/function/arithmetic/pow.js
+++ src/function/arithmetic/pow.js
@@ -10,16 +10,20 @@ const dependencies = [
   'identity',
   'multiply',
   'matrix',
+  'inv',
   'fraction',
   'number',
   'Complex'
 ]
 
-export const createPow = /* #__PURE__ */ factory(name, dependencies, ({ typed, config, identity, multiply, matrix, number, fraction, Complex }) => {
+export const createPow = /* #__PURE__ */ factory(name, dependencies, ({ typed, config, identity, multiply, matrix, inv, number, fraction, Complex }) => {
   /**
    * Calculates the power of x to y, `x ^ y`.
-   * Matrix exponentiation is supported for square matrices `x`, and positive
-   * integer exponents `y`.
+   *
+   * Matrix exponentiation is supported for square matrices `x` and integers `y`:
+   * when `y` is nonnegative, `x` may be any square matrix; and when `y` is
+   * negative, `x` must be invertible, and then this function returns
+   * inv(x)^(-y).
    *
    * For cubic roots of negative numbers, the function returns the principal
    * root by default. In order to let the function return the real root,
@@ -39,6 +43,9 @@ export const createPow = /* #__PURE__ */ factory(name, dependencies, ({ typed, c
    *
    *    const b = [[1, 2], [4, 3]]
    *    math.pow(b, 2)               // returns Array [[9, 8], [16, 17]]
+   *
+   *    const c = [[1, 2], [4, 3]]
+   *    math.pow(c, -1)               // returns Array [[-0.6, 0.4], [0.8, -0.2]]
    *
    * See also:
    *
@@ -150,13 +157,13 @@ export const createPow = /* #__PURE__ */ factory(name, dependencies, ({ typed, c
   /**
    * Calculate the power of a 2d array
    * @param {Array} x     must be a 2 dimensional, square matrix
-   * @param {number} y    a positive, integer value
+   * @param {number} y    a integer value (positive if `x` is not invertible)
    * @returns {Array}
    * @private
    */
   function _powArray (x, y) {
-    if (!isInteger(y) || y < 0) {
-      throw new TypeError('For A^b, b must be a positive integer (value is ' + y + ')')
+    if (!isInteger(y)) {
+      throw new TypeError('For A^b, b must be an integer (value is ' + y + ')')
     }
     // verify that A is a 2 dimensional square matrix
     const s = size(x)
@@ -165,6 +172,16 @@ export const createPow = /* #__PURE__ */ factory(name, dependencies, ({ typed, c
     }
     if (s[0] !== s[1]) {
       throw new Error('For A^b, A must be square (size is ' + s[0] + 'x' + s[1] + ')')
+    }
+    if (y < 0) {
+      try {
+        return _powArray(inv(x), -y)
+      } catch (error) {
+        if (error.message === 'Cannot calculate inverse, determinant is zero') {
+          throw new TypeError('For A^b, when A is not invertible, b must be a positive integer (value is ' + y + ')')
+        }
+        throw error
+      }
     }
 
     let res = identity(s[0]).valueOf()
