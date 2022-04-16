611ce4e398eaa29b524db94b27e3aab2b2fe169f
HanchaiN <85230240+HanchaiN@users.noreply.github.com>
2022-04-16 23:51:16 +0700

    Modify tests

--- test/unit-tests/function/matrix/pinv.test.js
+++ test/unit-tests/function/matrix/pinv.test.js
@@ -1,6 +1,5 @@
 // test pinv
 import assert from 'assert'
-
 import approx from '../../../../tools/approx.js'
 import math from '../../../../src/defaultInstance.js'
 const pinv = math.pinv
@@ -23,13 +22,13 @@ function assertValidPinv (A, A_) {
   assert.deepStrictEqual(math.size(A_).valueOf(), [cols, rows])
 
   // A A_ A = A
-  approx.deepEqual(math.multiply(A, math.multiply(A_, A)).valueOf(), A.valueOf())
+  approx.deepEqual(math.add(math.multiply(A, math.multiply(A_, A)), math.Complex(1, 1)).valueOf(), math.add(A, math.Complex(1, 1)).valueOf())
   // A_ A A_ = A_
-  approx.deepEqual(math.multiply(A_, math.multiply(A, A_)).valueOf(), A_.valueOf())
+  approx.deepEqual(math.add(math.multiply(A_, math.multiply(A, A_)), math.Complex(1, 1)).valueOf(), math.add(A_, math.Complex(1, 1)).valueOf())
   // (A A_)* = A A_
-  approx.deepEqual(math.ctranspose(math.multiply(A, A_)).valueOf(), math.multiply(A, A_).valueOf())
+  approx.deepEqual(math.add(math.ctranspose(math.multiply(A, A_)), math.Complex(1, 1)).valueOf(), math.add(math.multiply(A, A_), math.Complex(1, 1)).valueOf())
   // (A_ A)* = A_ A
-  approx.deepEqual(math.ctranspose(math.multiply(A_, A)).valueOf(), math.multiply(A_, A).valueOf())
+  approx.deepEqual(math.add(math.ctranspose(math.multiply(A_, A)), math.Complex(1, 1)).valueOf(), math.add(math.multiply(A_, A), math.Complex(1, 1)).valueOf())
 }
 
 describe('pinv', function () {
@@ -38,21 +37,20 @@ describe('pinv', function () {
     if (A_) {
       if (strict) assert.deepStrictEqual(pinvA, A_)
       else approx.deepEqual(pinvA, A_)
-    } else {
-      switch (math.size(A).valueOf().length) {
-        case 0:
-          assertValidPinv([[A]], [[pinvA]])
-          break
-        case 1:
-          assertValidPinv(math.reshape(A, [1, -1]), math.reshape(pinvA, [-1, 1]))
-          break
-        case 2:
-          assertValidPinv(A, pinvA)
-          break
-        default:
-          assert.ok(math.size(A).valueOf().length < 3)
-          break
-      }
+    }
+    switch (math.size(A).valueOf().length) {
+      case 0:
+        assertValidPinv([[A]], [[pinvA]])
+        break
+      case 1:
+        assertValidPinv(math.reshape(A, [1, -1]), math.reshape(pinvA, [-1, 1]))
+        break
+      case 2:
+        assertValidPinv(A, pinvA)
+        break
+      default:
+        assert.ok(math.size(A).valueOf().length < 3)
+        break
     }
   }
   it('should return the inverse of a number', function () {
@@ -117,10 +115,39 @@ describe('pinv', function () {
     test(math.matrix([[1, 2], [3, 4]], 'sparse'), math.matrix([[-2, 1], [1.5, -0.5]], 'sparse'), true)
   })
 
+  it('should return the Moore–Penrose inverse of complex matrices', function () {
+    test(
+      math.evaluate(`[
+          [0.4032 + 0.0876i,   0.1678 + 0.0390i,   0.5425 + 0.5118i],
+          [0.3174 + 0.3352i,   0.9784 + 0.4514i,  -0.4416 - 1.3188i],
+          [0.4008 - 0.0504i,   0.0979 - 0.2558i,   0.2983 + 0.7800i]
+      ]`)
+      // , math.evaluate(`[
+      //     [0.4318 - 0.1398i,   0.2869 - 0.1360i,   0.3897 - 0.0370i],
+      //     [0.3507 - 0.0725i,   0.4354 - 0.1329i,   0.2877 + 0.0565i],
+      //     [0.3116 - 0.2626i,   0.0042 + 0.2584i,   0.2388 - 0.2806i]
+      // ]`)
+    )
+  })
+
   it('should return the Moore–Penrose inverse of non-square matrices', function () {
+    test([[0, 0]], [[0], [0]], true)
+
     test([1, 2, 3], [1 / 14, 2 / 14, 3 / 14])
+
     test([[1, 2, 3], [4, 5, 6]], [[-17 / 18, 8 / 18], [-2 / 18, 2 / 18], [13 / 18, -4 / 18]])
     test([[1, 4], [2, 5], [3, 6]], [[-17 / 18, -2 / 18, 13 / 18], [8 / 18, 2 / 18, -4 / 18]])
+
+    test([
+      [64, 2, 3, 61, 60, 6],
+      [9, 55, 54, 12, 13, 51],
+      [17, 47, 46, 20, 21, 43],
+      [40, 26, 27, 37, 36, 30],
+      [32, 34, 35, 29, 28, 38],
+      [41, 23, 22, 44, 45, 19],
+      [49, 15, 14, 52, 53, 11],
+      [8, 58, 59, 5, 4, 62]
+    ])
   })
 
   it('should throw an error in case of multi dimensional matrices', function () {
