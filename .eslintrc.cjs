bfeaf990f2a072941741f56ddd1136d7dacb8e74
Christopher Chudzicki <christopher.chudzicki@gmail.com>
2022-04-23 08:10:36 -0400

    setup linting with eslint-config-standard, prettier

---
+++ .eslintrc.cjs
@@ -0,0 +1,53 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+    node: true
+  },
+  extends: ['eslint:recommended'],
+  parserOptions: {
+    ecmaVersion: 12,
+    sourceType: 'module'
+  },
+  ignorePatterns: ['*.generated.js'],
+  rules: {},
+  overrides: [
+    {
+      files: ['*.js', '*.cjs', '*.mjs'],
+      extends: [
+        'standard'
+      ]
+    },
+    {
+      files: ['*.test.js'],
+      plugins: ['mocha'],
+      extends: [
+        'standard',
+        'plugin:mocha/recommended'
+      ],
+      rules: {
+        'mocha/no-setup-in-describe': 'off',
+        'mocha/no-identical-title': 'off' // TODO: Remove this
+      }
+    },
+    {
+      files: ['types/*.ts'],
+      extends: [
+        'plugin:@typescript-eslint/recommended',
+        'plugin:prettier/recommended' // this should come last
+      ],
+      rules: {
+        '@typescript-eslint/no-unused-vars': [
+          'error',
+          { argsIgnorePattern: '^_', varsIgnorePattern: '^_' }
+        ]
+      },
+      plugins: ['@typescript-eslint'],
+      parserOptions: {
+        ecmaVersion: 12,
+        sourceType: 'module',
+        project: 'types/tsconfig.json'
+      }
+    }
+  ]
+}
