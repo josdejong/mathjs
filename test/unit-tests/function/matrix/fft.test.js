9b90b28a23009c3ed489cc52f6edf76ece8aa129
HanchaiN <85230240+HanchaiN@users.noreply.github.com>
2022-05-11 18:47:48 +0700

    Fixup tests

--- test/unit-tests/function/matrix/fft.test.js
+++ test/unit-tests/function/matrix/fft.test.js
@@ -6,35 +6,35 @@ const fft = math.fft
 
 describe('fft', function () {
   it('should calculate 1-dimensional fourier transformation', function () {
-    const in1 = math.evaluate('[1, 2-i, -i, -1+2i]')
-    const out1 = math.evaluate('[2, -2-2i, -2i, 4+4i]')
+    const in1 = [1, math.complex(2, -1), math.complex(0, -1), math.complex(-1, 2)]
+    const out1 = [2, math.complex(-2, -2), math.complex(0, -2), math.complex(4, 4)]
     approx.deepEqual(fft(in1.valueOf()), out1.valueOf())
     approx.deepEqual(fft(math.matrix(in1)), math.matrix(out1))
   })
 
   it('should calculate multidimensional fourier transformation', function () {
-    const in1 = math.evaluate(`[
+    const in1 = [
       [1, 0],
       [1, 0]
-    ]`)
-    const out1 = math.evaluate(`[
+    ]
+    const out1 = [
       [2, 2],
       [0, 0]
-    ]`)
+    ]
     approx.deepEqual(fft(in1.valueOf()), out1.valueOf())
     approx.deepEqual(fft(math.matrix(in1)), math.matrix(out1))
-    const in2 = math.evaluate(`[
+    const in2 = [
       [0, 0, 1, 1],
       [0, 0, 1, 1],
       [1, 1, 0, 0],
       [1, 1, 0, 0]
-    ]`)
-    const out2 = math.evaluate(`[
+    ]
+    const out2 = [
       [8, 0, 0, 0],
-      [0, 4i, 0, -4],
+      [0, math.complex(0, 4), 0, -4],
       [0, 0, 0, 0],
-      [0, -4, 0, -4i]
-    ]`)
+      [0, -4, 0, math.complex(0,-4)]
+    ]
     approx.deepEqual(fft(in2.valueOf()), out2.valueOf())
     approx.deepEqual(fft(math.matrix(in2)), math.matrix(out2))
   })
--- test/unit-tests/function/matrix/ifft.test.js
+++ test/unit-tests/function/matrix/ifft.test.js
@@ -6,15 +6,14 @@ const ifft = math.ifft
 
 describe('ifft', function () {
   it('should calculate 1-dimensional inverse fourier transformation', function () {
-    approx.deepEqual(ifft(math.evaluate('[2, -2-2i, -2i, 4+4i]')), math.evaluate('[1, 2-i, -i, -1+2i]'))
-    approx.deepEqual(ifft(math.evaluate('[2, -2-2i, -2i, 4+4i]')), math.evaluate('[1, 2-i, -i, -1+2i]'))
+    approx.deepEqual(ifft([2, math.complex(-2, -2), math.complex(0, -2), math.complex(4, 4)]), [2, math.complex(-2, -2), math.complex(0, -2), math.complex(4, 4)])
   })
 
   it('should calculate multidimensional inverse fourier transformation', function () {
-    const in1 = math.evaluate(`[
+    const in1 =[
       [1, 0],
       [1, 0]
-    ]`)
+    ]
     approx.deepEqual(math.fft(ifft(in1.valueOf())), in1.valueOf())
     approx.deepEqual(math.fft(ifft(math.matrix(in1))), math.matrix(in1))
   })
