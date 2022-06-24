f3cf2d42fe125ca1a5d538e635e62919b0b3ee21
Jos de Jong <wjosdejong@gmail.com>
2022-06-24 22:06:42 +0200

    Revert "Improve types for factory function (#2592)"
    This reverts commit 3a59a768c39f65f5243e8fc3bd5ac80a4df1a270.

--- types/index.d.ts
+++ types/index.d.ts
@@ -161,10 +161,6 @@ declare namespace math {
     isHexDigit(c: string): boolean
   }
 
-  interface NodeCtor {
-    new (): Node
-  }
-
   interface AccessorNode extends MathNodeCommon {
     type: 'AccessorNode'
     isAccessorNode: true
@@ -438,7 +434,6 @@ declare namespace math {
     tau: number
 
     // Class-like constructors
-    Node: NodeCtor
     AccessorNode: AccessorNodeCtor
     ArrayNode: ArrayNodeCtor
     AssignmentNode: AssignmentNodeCtor
@@ -3293,12 +3288,10 @@ declare namespace math {
       factories: FactoryFunctionMap,
       config?: ConfigOptions
     ) => MathJsStatic
-    factory: <T, TDeps extends readonly MathJsFunctionName[]>(
+    factory: <T>(
       name: string,
-      dependencies: TDeps,
-      create: (
-        injected: Pick<MathJsStatic, Extract<MathJsFunctionName, TDeps[number]>>
-      ) => T,
+      dependencies: MathJsFunctionName[],
+      create: (injected: Partial<MathJsStatic>) => T,
       // eslint-disable-next-line @typescript-eslint/no-explicit-any
       meta?: any
     ) => FactoryFunction<T>
