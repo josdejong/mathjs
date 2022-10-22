bdf864d36b2957e25cd57c44e9e042b842a11a5d
Chris Chudzicki <christopher.chudzicki@gmail.com>
2022-10-22 16:53:26 -0400

    add a test for discrimination

--- test/typescript-tests/nodeExtensions.ts
+++ test/typescript-tests/nodeExtensions.ts
@@ -1,9 +1,18 @@
 import { expectTypeOf } from 'expect-type'
-import { MathNode, MathNodeCommon, ConstantNode, Node } from 'mathjs'
+import {
+  MathNode,
+  BaseNode,
+  MathNodeCommon,
+  ConstantNode,
+  Node,
+  FunctionAssignmentNode,
+} from 'mathjs'
 
+interface MyNode extends BaseNode {
+  type: 'MyNode'
+  a: MathNode
+}
 declare module 'mathjs' {
-  type MyNode = BaseNode
-
   interface MathNodeTypes {
     MyNode: MyNode
   }
@@ -13,31 +22,31 @@ declare module 'mathjs' {
 MathNode examples
 */
 {
-  class CustomNode extends Node {
+  class CustomNode extends Node implements MyNode {
     a: MathNode
+    type: 'MyNode'
     constructor(a: MathNode) {
       super()
       this.a = a
     }
   }
 
-  // Basic node
-  const instance1 = new Node()
-
   // Built-in subclass of Node
-  const instance2 = new ConstantNode(2)
+  const instance1 = new ConstantNode(2)
 
   // Custom subclass of node
-  const instance3 = new CustomNode(new ConstantNode(2))
+  const instance2 = new CustomNode(new ConstantNode(2))
 
   expectTypeOf(instance1).toMatchTypeOf<MathNode>()
   expectTypeOf(instance1).toMatchTypeOf<MathNodeCommon>()
+  expectTypeOf(instance1).toMatchTypeOf<ConstantNode>()
 
   expectTypeOf(instance2).toMatchTypeOf<MathNode>()
   expectTypeOf(instance2).toMatchTypeOf<MathNodeCommon>()
-  expectTypeOf(instance2).toMatchTypeOf<ConstantNode>()
+  expectTypeOf(instance2).toMatchTypeOf<CustomNode>()
 
-  expectTypeOf(instance3).toMatchTypeOf<MathNode>()
-  expectTypeOf(instance3).toMatchTypeOf<MathNodeCommon>()
-  expectTypeOf(instance3).toMatchTypeOf<CustomNode>()
+  let instance3: MathNode
+  if (instance3.type === 'FunctionAssignmentNode') {
+    expectTypeOf(instance3).toMatchTypeOf<FunctionAssignmentNode>()
+  }
 }
