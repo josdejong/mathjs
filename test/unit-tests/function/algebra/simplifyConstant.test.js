b00679499edf9e70b9186d55cb33efc409126aa9
samueltlg <samueltlgg@gmail.com>
2022-11-03 21:26:46 +0000

    Simplify: improve constant op. eval. in non-commutative contexts
    Addresses the constant evaluation aspect as discussed in #2825.
    
    Changes in simplifyConstant.js are accredited to @gwhitney

--- test/unit-tests/function/algebra/simplifyConstant.test.js
+++ test/unit-tests/function/algebra/simplifyConstant.test.js
@@ -22,9 +22,47 @@ describe('simplifyConstant', function () {
     testSimplifyConstant('0.5 x', '1 / 2 x')
   })
 
-  it('should coalesce constants in a multi-argument expression', function () {
-    testSimplifyConstant('3 + x + 7 + y', '10 + x + y')
-    testSimplifyConstant('3 * x * 7 * y', '21 * x * y')
+  describe('should coalesce constants in a multi-argument expression', function () {
+    it('in the default context', function () {
+      testSimplifyConstant('3 + x + 7 + y', '10 + x + y')
+      testSimplifyConstant('3 * x * 7 * y', '21 * x * y')
+
+      // including an 'unevaluable' constant
+      // todo?: output here not currently as expected; instead outputting as '9 + "foo" + 11'.
+      // testSimplifyConstant('2 + 7 + "foo" + 3 + 8', '20 + "foo"')
+    })
+
+    it('in non-commutative contexts', function () {
+      const context = {
+        add: { commutative: false },
+        multiply: { commutative: false }
+      }
+      const opts = { context }
+
+      // leading consts.
+      testSimplifyConstant('2 + 2 + a', '4 + a', undefined, opts)
+      testSimplifyConstant('2 * 2 * b', '4 * b', undefined, opts)
+      // trailing consts.
+      testSimplifyConstant('a + 3 + 2', 'a + 5', undefined, opts)
+      testSimplifyConstant('a * 3 * 2', 'a * 6', undefined, opts)
+      // 'wedged' constants
+      testSimplifyConstant('a + 3 + 2 + a + 4 + 4 + b', 'a + 5 + a + 8 + b', undefined, opts)
+      testSimplifyConstant('c * 3 * 2 * d * 4 * 1 * d', 'c * 6 * d * 4 * d', undefined, opts)
+
+      // including an 'unevaluable' constant
+      testSimplifyConstant('2 + 7 + "foo" + 3 + 8', '9 + "foo" + 11', undefined, opts)
+      // collapsing of constants with a 'falsy' value (e.g. '0')
+      testSimplifyConstant('a + 0 + 7 + b', 'a + 7 + b', undefined, opts)
+
+      // note:
+      // - tests with inverses of commutative operations. will not be as expected (i.e. 'fully
+      // simplified') if passed directly to simplifyConstant, and further they should not be needed.
+      // In the absence of prior application of standard rules which flatten substraction and division
+      // ops. (such as rule 'n / n1 -> n * n1 ^ (-1)' for div. and 'n - n1 -> n + -n1' for
+      // subtraction), constant args are not 'flattened' and consequently included in 'multi-arg
+      // expressions', therefore prohibiting evaluation of adjacent constants (in a non-commutative
+      // context) here.
+    })
   })
 
   it('should respect simplify options', function () {
