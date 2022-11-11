0137837ceeb5bfb46370e10205532ebf12f34d1c
Lucas Egidio <lucasegidio1@gmail.com>
2022-11-11 21:07:52 +0100

    rm a trailing space

--- src/function/algebra/lyap.js
+++ src/function/algebra/lyap.js
@@ -21,7 +21,7 @@ export const createLyap = /* #__PURE__ */ factory(name, dependencies, (
   /**
    *
    * Solves the Continuous-time Lyapunov equation AP+PA'+Q=0 for P, where
-   * Q is an input matrix. When Q is symmetric, P is also symmetric. Notice 
+   * Q is an input matrix. When Q is symmetric, P is also symmetric. Notice
    * that different equivalent definitions exist for the Continuous-time
    * Lyapunov equation.
    * https://en.wikipedia.org/wiki/Lyapunov_equation
