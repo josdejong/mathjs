josdejong/mathjs

commit 1bd7ade59c36cc8d19db3fb69295d4ea00c9d823

matrix*vector multiplication returns a vector (TS) (#2623)
* vector*matrix multiplication returns a vector

* in multiply, matrix has precedence and have specific case for js arrays which is distinct from matrix

* add test examples

* type distinguishing javascript function

* linter style

types/index.d.ts
@@ -1215,9 +1215,13 @@ declare namespace math {
      * @param y The second value to multiply
      * @returns Multiplication of x and y
      */
-    multiply<T extends Matrix | MathArray>(x: T, y: MathType): T
+
+    multiply<T extends Matrix>(x: T, y: MathType): Matrix
+    multiply<T extends Matrix>(x: MathType, y: T): Matrix
+
     multiply(x: Unit, y: Unit): Unit
     multiply(x: number, y: number): number
+    multiply(x: MathArray, y: MathArray): MathArray
     multiply(x: MathType, y: MathType): MathType
 
     /**

types/index.ts
@@ -1282,6 +1282,81 @@ Matrices examples
 }
 
 /*
+Math types examples: Type results after multiplying  'MathTypes' with matrices
+ */
+{
+  const math = create(all, {})
+
+  const abc: MathArray = [1, 2, 3, 4]
+  const bcd: MathArray = [
+    [1, 2, 3, 4],
+    [2, 3, 4, 5],
+    [4, 5, 6, 7],
+    [5, 6, 7, 8],
+  ]
+
+  const Mbcd = math.matrix(bcd)
+  const Mabc = math.matrix(abc)
+
+  // Number
+  const r1 = math.multiply(1, 2)
+  console.log(r1)
+
+  // Unit
+  const a = math.unit(45, 'cm') // 450 mm
+  const b = math.unit(45, 'cm') // 450 mm
+  const r2 = math.multiply(a, b)
+  console.log(r2)
+
+  // 1D JS Array
+  const r3 = math.multiply(abc, bcd)
+  console.log(r3)
+  r3[1] // By default least promised valid syntax
+
+  // 2D JS Array
+  const r12 = math.multiply(bcd, bcd)
+  console.log(r12)
+  // Example to sort ambiguity between multidimensional & singledimensional arrays
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  const multiDimensional = (x: any): x is any[][] => x.length && x[0].length
+  if (multiDimensional(r12)) {
+    r12[1][1]
+  }
+  r12[1] // Valid syntax
+
+  // Matrix: matrix * vector
+  const r7 = math.multiply(Mabc, bcd)
+  console.log(r7)
+  r7.toArray() // Matrix-es have toArray function
+
+  // Matrix
+  const r8 = math.multiply(Mabc, 4)
+  console.log(r8)
+
+  // Matrix
+  const r11 = math.multiply(4, Mabc)
+  console.log(r11)
+
+  // Matrix of units
+  const r9 = math.multiply(Mabc, a)
+  console.log(r9)
+  const r10 = math.multiply(a, Mabc)
+  console.log(r10)
+
+  // Matrix
+  const r6 = math.multiply(abc, Mbcd)
+  console.log(r6)
+
+  // 2D JS Array
+  const r5 = math.multiply(bcd, abc)
+  console.log(r5)
+
+  // Number
+  const r4 = math.multiply(abc, math.transpose(abc))
+  console.log(r4)
+}
+
+/*
 Sparse matrices examples
 */
 {

commit 42666cad9f35461311665b00e0993eb7398894bb

Fix correct types for multiply()
Fix correct types for `multiply( number[][], number[] )`, `multiply( number[], number[][] )` and `multiply( number[][], number[][] )`

Type errors were introduced with https://github.com/josdejong/mathjs/pull/2623/files which used MathArray directly (where MathArray could be number[] or number[][]), causing the return type to be number[] | number[][].

types/index.d.ts
@@ -1296,10 +1296,14 @@ declare namespace math {
 
     multiply<T extends Matrix>(x: T, y: MathType): Matrix
     multiply<T extends Matrix>(x: MathType, y: T): Matrix
+    
+    multiply<T extends MathNumericType[]>(x: T, y: T[]): T
+    multiply<T extends MathNumericType[]>(x: T[], y: T): T
 
+    multiply<T extends MathArray>(x: T, y: T): T
+    
     multiply(x: Unit, y: Unit): Unit
     multiply(x: number, y: number): number
-    multiply(x: MathArray, y: MathArray): MathArray
     multiply(x: MathType, y: MathType): MathType
 
     /**