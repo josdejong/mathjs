17cfd8b944ab1d917cb2315158815ba38ec8045c
Evan Miller <emmiller@gmail.com>
2022-12-22 16:07:57 -0500

    Add type signature for lusolve(LUDecomposition, ...)

--- types/index.d.ts
+++ types/index.d.ts
@@ -898,6 +898,11 @@ declare namespace math {
       order?: number,
       threshold?: number
     ): MathArray
+    
+    lusolve(
+      A: LUDecomposition,
+      b: Matrix | MathArray,
+    ): Matrix
 
     /* Finds the roots of a polynomial of degree three or less. Coefficients are given constant first
      * followed by linear and higher powers in order; coefficients beyond the degree of the polynomial
@@ -4516,6 +4521,11 @@ declare namespace math {
       order?: number,
       threshold?: number
     ): MathJsChain<MathArray>
+
+    lusolve(
+      this: MathJsChain<LUDecomposition>,
+      b: Matrix | MathArray,
+    ): MathJsChain<Matrix>
 
     /**
      * Calculate the Matrix QR decomposition. Matrix A is decomposed in two
