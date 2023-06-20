8e1e0a7f98b9f7434b52916805ef3f5784031f32
Jos de Jong <wjosdejong@gmail.com>
2023-06-20 10:33:00 +0200

    chore: refactor `zeta` (WIP)

--- src/function/special/zeta.js
+++ src/function/special/zeta.js
@@ -1,9 +1,9 @@
 import { factory } from '../../utils/factory.js'
 
 const name = 'zeta'
-const dependencies = ['typed', 'multiply', 'pow', 'divide', 'factorial', 'equal', 'gamma', 'sin', 'subtract', 'add', '?Complex', '?BigNumber', 'pi']
+const dependencies = ['typed', 'multiply', 'pow', 'divide', 'factorial', 'equal', 'smallerEq', 'isNegative', 'gamma', 'sin', 'subtract', 'add', '?Complex', '?BigNumber', 'pi']
 
-export const createZeta = /* #__PURE__ */ factory(name, dependencies, ({ typed, multiply, pow, divide, factorial, equal, gamma, sin, subtract, add, Complex, BigNumber, pi }) => {
+export const createZeta = /* #__PURE__ */ factory(name, dependencies, ({ typed, multiply, pow, divide, factorial, equal, smallerEq, isNegative, gamma, sin, subtract, add, Complex, BigNumber, pi }) => {
   /**
    * Compute the Riemann Zeta function of a value using an infinite series for
    * all of the complex plane using Riemann's Functional equation.
@@ -28,77 +28,77 @@ export const createZeta = /* #__PURE__ */ factory(name, dependencies, ({ typed, 
    * @param {number | Complex | BigNumber} s   A Real, Complex or BigNumber parameter to to the Riemann Zeta Function
    * @return {number | Complex | BigNumber}    The Riemann Zeta of `s`
    */
-  function zeta (s) {
-    if (s.re === 0 && s.im === 0) {
-      return -0.5
-    }
-    if (s.re === 1) {
-      return NaN
-    }
-    if (s.re === Infinity && s.im === 0) {
-      return 1
-    }
-    if (s.im === Infinity || s.re === -Infinity) {
-      return NaN
-    }
-    // The "15" is number of decimal places of accuracy (approx)
-    const n = Math.round(1.3 * 15 + 0.9 * Math.abs(s.im))
-    if (s.re > -(n - 1) / 2) { return f(s, n) }
+  return typed(name, {
+    'number': (s) => {
+      if (equal(s, 0)) {
+        return -0.5 // FIXME: return BigNumber when needed
+      }
+      if (equal(s, 1)) {
+        return NaN // FIXME: return BigNumber when needed
+      }
+      if (!isFinite(s)) {
+        return isNegative(s) ? NaN : 1 // FIXME: return BigNumber when needed
+      }
 
-    // Functional Equation for reflection to re(s) < -( n - 1) / 2
-    let c = multiply(pow(2, s), pow(Math.PI, subtract(s, 1)))
-    c = multiply(c, (sin(multiply(Math.PI / 2, s))))
-    c = multiply(c, gamma(subtract(1, s)))
-    return multiply(c, zeta(subtract(1, s)))
-  }
+      return zeta(s, value => value, () => 20, s => s)
+    },
+
+    'BigNumber': (s) => {
+      if (equal(s, 0)) {
+        return -0.5 // FIXME: return BigNumber when needed
+      }
+      if (equal(s, 1)) {
+        return NaN // FIXME: return BigNumber when needed
+      }
+      if (!isFinite(s)) {
+        return isNegative(s) ? NaN : 1 // FIXME: return BigNumber when needed
+      }
+
+      return zeta(s, value => new BigNumber(value), () => 20, s => s)
+    },
+
+    Complex: s => {
+      if (s.re === 0 && s.im === 0) {
+        return new Complex(-0.5)
+      }
+      if (s.re === 1) {
+        return new Complex(NaN, NaN)
+      }
+      if (s.re === Infinity && s.im === 0) {
+        return new Complex(1)
+      }
+      if (s.im === Infinity || s.re === -Infinity) {
+        return new Complex(NaN, NaN)
+      }
+
+      return zeta(s, value => value, s => Math.round(1.3 * 15 + 0.9 * Math.abs(s.im)), s => s.re)
+    }
+  })
 
   // Big Number alias
-  function zetaBigNumber (x) {
-    if (x === 0) {
-      return -0.5
+  function zeta (s, createValue, determineDigits, getRe) {
+    const n = determineDigits(s)
+    if (getRe(s) > -(n - 1) / 2) {
+      return f(s, createValue(n), createValue)
+    } else {
+      // Function Equation for reflection to x < 1
+      let c = multiply(pow(2, s), pow(createValue(pi), subtract(s, 1)))
+      c = multiply(c, (sin(multiply(divide(createValue(pi), 2), s))))
+      c = multiply(c, gamma(subtract(1, s)))
+      return multiply(c, zeta(subtract(1, s), createValue, determineDigits, getRe))
     }
-    if (equal(x, 1)) {
-      return NaN
-    }
-    if (!x.isFinite() && !x.isNegative()) {
-      return 1
-    }
-    if (!x.isFinite() && x.isNegative()) {
-      return NaN
-    }
-    // 15 decimals of accuracy
-    const n = 20
-    if (x > -(n - 1) / 2) { return fBigNumber(x, n) }
-
-    // Function Equation for reflection to x < 1
-    let c = multiply(pow(2, x), pow(new BigNumber(pi), subtract(x, 1)))
-    c = multiply(c, (sin(multiply(new BigNumber(pi / 2), x))))
-    c = multiply(c, gamma(subtract(1, x)))
-    return multiply(c, zetaBigNumber(subtract(1, x)))
   }
 
   /**
    * Calculate a portion of the sum
-   * @param {number} k   a positive integer
-   * @param {number} n   a positive integer
+   * @param {number | BigNumber} k   a positive integer
+   * @param {number | BigNumber} n   a positive integer
    * @return {number}    the portion of the sum
    **/
   function d (k, n) {
-    let S = 0
-    for (let j = k; j <= n; j++) {
-      S += factorial(n + j - 1) * (4 ** j) / (factorial(n - j) * factorial(2 * j))
-    }
-
-    return n * S
-  }
-
-  // Big Number alias
-  function dBigNumber (k, n) {
-    let S = new BigNumber(0)
-    const bn = new BigNumber(n)
-    for (let j = k; j <= n; j++) {
-      const bj = new BigNumber(j)
-      const factor = divide(multiply(factorial(add(bn, subtract(bj, 1))), pow(4, bj)), multiply(factorial(subtract(bn, bj)), factorial(multiply(2, bj))))
+    let S = k
+    for (let j = k; smallerEq(j, n); j = add(j, 1)) {
+      const factor = divide(multiply(factorial(add(n, subtract(j, 1))), pow(4, j)), multiply(factorial(subtract(n, j)), factorial(multiply(2, j))))
       S = add(S, factor)
     }
 
@@ -109,36 +109,15 @@ export const createZeta = /* #__PURE__ */ factory(name, dependencies, ({ typed, 
    * Calculate the positive Riemann Zeta function
    * @param {number} s   a real or complex number with s.re > 1
    * @param {number} n   a positive integer
+   * @param {(number) => number | BigNumber | Complex} createValue
    * @return {number}    Riemann Zeta of s
    **/
-  function f (s, n) {
-    const c = divide(1, multiply(d(0, n), subtract(1, pow(2, subtract(1, s)))))
-    let S = new Complex(0, 0)
-    for (let k = 1; k <= n; k++) {
-      S = S.add(
-        divide((-1) ** (k - 1) * d(k, n), pow(k, s))
-      )
-    }
-
-    return multiply(c, S)
-  }
-
-  // Big Number alias
-  function fBigNumber (s, n) {
-    const c = divide(1, multiply(dBigNumber(0, n), subtract(1, pow(2, subtract(1, s)))))
-    let S = new BigNumber(0)
-    for (let k = 1; k <= n; k++) {
-      S = S.add(
-        divide(multiply((-1) ** (k - 1), dBigNumber(k, n)), pow(k, s))
-      )
+  function f (s, n, createValue) {
+    const c = divide(1, multiply(d(createValue(0), n), subtract(1, pow(2, subtract(1, s)))))
+    let S = createValue(0)
+    for (let k = createValue(1); smallerEq(k, n); k = add(k, 1)) {
+      S = add(S, divide(multiply((-1) ** (k - 1), d(k, n)), pow(k, s)))
     }
     return multiply(c, S)
   }
-
-  // Return the value of the function
-  return typed(name, {
-    number: s => zeta(new Complex(s)),
-    Complex: zeta,
-    BigNumber: zetaBigNumber
-  })
 })
