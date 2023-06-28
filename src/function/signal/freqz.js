1a74fdad85cb44dc0b5fc7356b142d1f21aa8b3f
Aly Khaled <alykhaled2001@live.com>
2023-06-28 16:32:30 +0300

    Fix format

--- src/function/signal/freqz.js
+++ src/function/signal/freqz.js
@@ -11,63 +11,62 @@ const dependencies = [
 ]
 
 export const createFreqz = /* #__PURE__ */ factory(name, dependencies, ({ typed, add, multiply, Complex, divide }) => {
-    /**
+  /**
      * Calculates the frequency response of a filter given its numerator and denominator coefficients.
      * @param {Array.<number>} b The numerator coefficients of the filter.
      * @param {Array.<number>} a The denominator coefficients of the filter.
      * @param {Array.<number>} [w] A vector of frequencies (in radians/sample) at which the frequency response is to be computed.
      * @returns {Array.<number>} The frequency response.
-     * 
+     *
      * @example
      *  freqz([1, 2], [1, 2, 3]); // returns [0.5, 0.5]
      *  freqz([1, 2], [1, 2, 3], [0, 1]); // returns [0.5, 0.5]
-     * 
+     *
      */
-    return typed(name, {
-        'Array, Array': function (b, a) {
-            return _freqz(b, a);
-        },
-        'Array, Array, Array': function (b, a, w) {
-            return _freqz(b, a, w);
-        },
-        'Array, Array, number': function (b, a, w) {
-            return _freqz(b, a, w);
-        }
-    });
-
-    function _freqz(b, a, w) {
-        if (w === undefined) {
-            w = [];
-            for (let i = 0; i < 512; i++) {
-                w.push(i / 512 * Math.PI);
-            }
-        }
-        if (typeof w === 'number') {
-            const w2 = [];
-            for (let i = 0; i < w; i++) {
-                w2.push(i / w * Math.PI);
-            }
-            w = w2;
-        }
-        const num = [];
-        const den = [];
-        for (let i = 0; i < w.length; i++) {
-            let sumNum = Complex(0, 0);
-            let sumDen = Complex(0, 0);
-            for (let j = 0; j < b.length; j++) {
-                sumNum = add(sumNum, multiply(b[j], Complex(Math.cos(-j * w[i]), Math.sin(-j * w[i]))));
-            }
-            for (let j = 0; j < a.length; j++) {
-                sumDen = add(sumDen, multiply(a[j], Complex(Math.cos(-j * w[i]), Math.sin(-j * w[i]))));
-            }
-            num.push(sumNum);
-            den.push(sumDen);
-        }
-        const h = [];
-        for (let i = 0; i < num.length; i++) {
-            h.push(divide(num[i], den[i]));
-        }
-        return {h, w}
+  return typed(name, {
+    'Array, Array': function (b, a) {
+      return _freqz(b, a)
+    },
+    'Array, Array, Array': function (b, a, w) {
+      return _freqz(b, a, w)
+    },
+    'Array, Array, number': function (b, a, w) {
+      return _freqz(b, a, w)
     }
-})
+  })
 
+  function _freqz (b, a, w) {
+    if (w === undefined) {
+      w = []
+      for (let i = 0; i < 512; i++) {
+        w.push(i / 512 * Math.PI)
+      }
+    }
+    if (typeof w === 'number') {
+      const w2 = []
+      for (let i = 0; i < w; i++) {
+        w2.push(i / w * Math.PI)
+      }
+      w = w2
+    }
+    const num = []
+    const den = []
+    for (let i = 0; i < w.length; i++) {
+      let sumNum = Complex(0, 0)
+      let sumDen = Complex(0, 0)
+      for (let j = 0; j < b.length; j++) {
+        sumNum = add(sumNum, multiply(b[j], Complex(Math.cos(-j * w[i]), Math.sin(-j * w[i]))))
+      }
+      for (let j = 0; j < a.length; j++) {
+        sumDen = add(sumDen, multiply(a[j], Complex(Math.cos(-j * w[i]), Math.sin(-j * w[i]))))
+      }
+      num.push(sumNum)
+      den.push(sumDen)
+    }
+    const h = []
+    for (let i = 0; i < num.length; i++) {
+      h.push(divide(num[i], den[i]))
+    }
+    return { h, w }
+  }
+})
--- test/unit-tests/function/signal/freqz.test.js
+++ test/unit-tests/function/signal/freqz.test.js
@@ -4,25 +4,24 @@ import math from '../../../../src/defaultInstance.js'
 const freqz = math.freqz
 
 describe('freqz', function () {
-    it('should return the frequency response of a zero-pole-gain model', function () {
-        console.log(freqz([math.complex(1, 0),math.complex(-3,0),math.complex(2,0)], [math.complex(1, 0),math.complex(3,0),math.complex(2,0)], 5))
-        approx.deepEqual(freqz([math.complex(1, 0), math.complex(-1,-5)], [math.complex(1, 0), math.complex(5, 0), math.complex(6, 0)], 5), {
-            h: [math.complex(0, -0.4166666666666667),
-                math.complex(0.08934733914100848, -0.3891571989056336),
-                math.complex(0.19350242447606233, -0.43679084380970035),
-                math.complex(0.5068505396172553, -0.5776505671214675),
-                math.complex(1.5607298559987381, -0.26338443553329166),
-            ],
-            w: [0, 0.62831853, 1.25663706, 1.88495559, 2.51327412]
-        })
-        approx.deepEqual(freqz([math.complex(1, 0),math.complex(-3,0),math.complex(2,0)], [math.complex(1, 0),math.complex(3,0),math.complex(2,0)], 5), {
-            h: [math.complex(0, 0),
-                math.complex(-0.09275445814522237, -0.11835248219923325),
-                math.complex(-0.4432171093183538, -0.3495195356882487),
-                math.complex(-1.3911165095967133, -1.0970298445163509),
-                math.complex(-4.102237436136192, -5.234357386651877),
-            ],
-            w: [0, 0.62831853, 1.25663706, 1.88495559, 2.51327412]
-        })
+  it('should return the frequency response of a zero-pole-gain model', function () {
+    approx.deepEqual(freqz([math.complex(1, 0), math.complex(-1, -5)], [math.complex(1, 0), math.complex(5, 0), math.complex(6, 0)], 5), {
+      h: [math.complex(0, -0.4166666666666667),
+        math.complex(0.08934733914100848, -0.3891571989056336),
+        math.complex(0.19350242447606233, -0.43679084380970035),
+        math.complex(0.5068505396172553, -0.5776505671214675),
+        math.complex(1.5607298559987381, -0.26338443553329166)
+      ],
+      w: [0, 0.62831853, 1.25663706, 1.88495559, 2.51327412]
     })
+    approx.deepEqual(freqz([math.complex(1, 0), math.complex(-3, 0), math.complex(2, 0)], [math.complex(1, 0), math.complex(3, 0), math.complex(2, 0)], 5), {
+      h: [math.complex(0, 0),
+        math.complex(-0.09275445814522237, -0.11835248219923325),
+        math.complex(-0.4432171093183538, -0.3495195356882487),
+        math.complex(-1.3911165095967133, -1.0970298445163509),
+        math.complex(-4.102237436136192, -5.234357386651877)
+      ],
+      w: [0, 0.62831853, 1.25663706, 1.88495559, 2.51327412]
+    })
+  })
 })
