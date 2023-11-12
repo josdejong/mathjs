c5e5f1cb12b63d07fc78ef10ff66b47afc1d6c13
Brooks Smith <brooks.smith@clearcalcs.com>
2023-11-12 21:44:23 +1100

    Use `isCollection()` to simplify bitwise transform functions

--- src/expression/transform/bitAnd.transform.js
+++ src/expression/transform/bitAnd.transform.js
@@ -1,16 +1,16 @@
 import { createBitAnd } from '../../function/bitwise/bitAnd.js'
 import { factory } from '../../utils/factory.js'
-import { isBigNumber, isBoolean, isComplex, isNumber, isUnit } from '../../utils/is.js'
+import { isCollection } from '../../utils/is.js'
 
 const name = 'bitAnd'
 const dependencies = ['typed', 'matrix', 'zeros', 'add', 'equalScalar', 'not', 'concat']
 
 export const createBitAndTransform = /* #__PURE__ */ factory(name, dependencies, ({ typed, matrix, equalScalar, zeros, not, concat }) => {
-  const and = createBitAnd({ typed, matrix, equalScalar, zeros, not, concat })
+  const bitAnd = createBitAnd({ typed, matrix, equalScalar, zeros, not, concat })
 
   function bitAndTransform (args, math, scope) {
     const condition1 = args[0].compile().evaluate(scope)
-    if (isNumber(condition1) || isBoolean(condition1) || isBigNumber(condition1) || isComplex(condition1) || isUnit(condition1)) {
+    if (!isCollection(condition1)) {
       if (isNaN(condition1)) {
         return NaN
       }
@@ -19,7 +19,7 @@ export const createBitAndTransform = /* #__PURE__ */ factory(name, dependencies,
       }
     }
     const condition2 = args[1].compile().evaluate(scope)
-    return and(condition1, condition2)
+    return bitAnd(condition1, condition2)
   }
 
   bitAndTransform.rawArgs = true
--- src/expression/transform/bitOr.transform.js
+++ src/expression/transform/bitOr.transform.js
@@ -1,16 +1,16 @@
 import { createBitOr } from '../../function/bitwise/bitOr.js'
 import { factory } from '../../utils/factory.js'
-import { isBigNumber, isBoolean, isComplex, isNumber, isUnit } from '../../utils/is.js'
+import { isCollection } from '../../utils/is.js'
 
 const name = 'bitOr'
 const dependencies = ['typed', 'matrix', 'equalScalar', 'DenseMatrix', 'concat']
 
 export const createBitOrTransform = /* #__PURE__ */ factory(name, dependencies, ({ typed, matrix, equalScalar, DenseMatrix, concat }) => {
-  const or = createBitOr({ typed, matrix, equalScalar, DenseMatrix, concat })
+  const bitOr = createBitOr({ typed, matrix, equalScalar, DenseMatrix, concat })
 
   function bitOrTransform (args, math, scope) {
     const condition1 = args[0].compile().evaluate(scope)
-    if (isNumber(condition1) || isBoolean(condition1) || isBigNumber(condition1) || isComplex(condition1) || isUnit(condition1)) {
+    if (!isCollection(condition1)) {
       if (isNaN(condition1)) {
         return NaN
       }
@@ -22,7 +22,7 @@ export const createBitOrTransform = /* #__PURE__ */ factory(name, dependencies, 
       }
     }
     const condition2 = args[1].compile().evaluate(scope)
-    return or(condition1, condition2)
+    return bitOr(condition1, condition2)
   }
 
   bitOrTransform.rawArgs = true
