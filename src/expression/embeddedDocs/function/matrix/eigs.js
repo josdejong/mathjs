105ca205416cb4d7e922e5d1dcfd47f4101fcef3
Jos de Jong <wjosdejong@gmail.com>
2023-11-21 15:59:56 +0100

    fix: #3096 embedded docs of `eigs` throwing an error

--- src/expression/embeddedDocs/function/matrix/eigs.js
+++ src/expression/embeddedDocs/function/matrix/eigs.js
@@ -7,7 +7,7 @@ export const eigsDocs = {
   description: 'Calculate the eigenvalues and optionally eigenvectors of a square matrix',
   examples: [
     'eigs([[5, 2.3], [2.3, 1]])',
-    'eigs([[1, 2, 3], [4, 5, 6], [7, 8, 9]], { precision: 1e-6, eigenvectors: false }'
+    'eigs([[1, 2, 3], [4, 5, 6], [7, 8, 9]], { precision: 1e-6, eigenvectors: false })'
   ],
   seealso: [
     'inv'
