2fc796063a974898fd6cbf76aa39c6472476eed4
Jos de Jong <wjosdejong@gmail.com>
2024-02-08 09:21:17 +0100

    chore: fix linting issue

--- test/benchmark/scope_variables.js
+++ test/benchmark/scope_variables.js
@@ -8,13 +8,11 @@ const f = math.evaluate('f(x, y) = a + b + c + x + y', scope)
 
 console.log('f(5, 6) = ' + f(5, 6))
 
-const results = []
-
 const suite = new Benchmark.Suite()
 let res = 0
 suite
   .add('evaluate f(x, y)', function () {
-    res = f(-res, res) // just make it dynamic, using res as argument
+    res = f(-res, res) // make it dynamic, using res as argument
   })
   .on('cycle', function (event) {
     console.log(String(event.target))
