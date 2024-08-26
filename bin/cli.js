6aabc4b784208bd212aed14b708753599db02a4f
Jos de Jong <wjosdejong@gmail.com>
2024-08-26 12:56:57 +0200

    chore: keep functions like `simplify` enabled in the CLI

--- bin/cli.js
+++ bin/cli.js
@@ -69,12 +69,7 @@ function getMath () {
   math.import({
     'import':     function () { throw new Error('Function import is disabled') },
     'createUnit': function () { throw new Error('Function createUnit is disabled') },
-    'evaluate':   function () { throw new Error('Function evaluate is disabled') },
-    'parse':      function () { throw new Error('Function parse is disabled') },
-    'simplify':   function () { throw new Error('Function simplify is disabled') },
-    'derivative': function () { throw new Error('Function derivative is disabled') },
-    'resolve': function () { throw new Error('Function resolve is disabled') },
-    'reviver': function () { throw new Error('Function reviver is disabled') }
+    'reviver':    function () { throw new Error('Function reviver is disabled') }
   }, { override: true })
 
   return { math, parse, evaluate }
