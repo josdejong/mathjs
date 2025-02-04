5eaa11e8120253f2b27e8b075e9fbe0dcbd92cd9
David Contreras <dvd.cnt@gmail.com>
2025-02-03 21:42:15 -0600

    fix: add input validation to map and forEach functions

--- src/utils/iterable.js
+++ src/utils/iterable.js
@@ -9,22 +9,20 @@
  * @returns {Array} The new array with the results of the callback function.
  */
 export function map (array, callback, isHomogeneous = false, callbackIsIndexed = true, thisArg) {
+  if (!Array.isArray(array)) throw new TypeError('First argument to map must be an array')
+  if (typeof callback !== 'function') throw new TypeError('Second argument to map must be a function')
+  if (array.length === 0) return []
   if (callbackIsIndexed) {
     thisArg = thisArg || array
     if (isHomogeneous) {
-      return array.map((value, index) => recurseIndexedHomogeneous(value, [index], 1))
+      return recurseIndexedHomogeneous(array, [], 0)
     } else {
       return array.map((value, index) => recurseIndexedHeterogeneous(value, [index], 1))
     }
   } else {
+    // not indexed
     if (isHomogeneous) {
-      return array.map(function rec (value) {
-        if (Array.isArray(value[0])) {
-          return value.map(rec)
-        } else {
-          return value.map(callback)
-        }
-      })
+      return recurseHomogeneous(array)
     } else {
       return array.map(function rec (value) {
         if (Array.isArray(value)) {
@@ -36,9 +34,17 @@ export function map (array, callback, isHomogeneous = false, callbackIsIndexed =
     }
   }
 
+  function recurseHomogeneous (value) {
+    if (Array.isArray(value[0])) {
+      return value.map(recurseHomogeneous)
+    } else {
+      return value.map(callback)
+    }
+  }
+
   function recurseIndexedHeterogeneous (value, index, depth) {
     if (Array.isArray(value)) {
-      return value.map(function (child, i) {
+      return value.map((child, i) => {
         index[depth] = i
         const results = recurseIndexedHeterogeneous(child, index, depth + 1)
         index[depth] = null
@@ -67,7 +73,7 @@ export function map (array, callback, isHomogeneous = false, callbackIsIndexed =
 }
 
 /**
-   * Applies a callback function to each element of the array using .forEach.
+   * Applies a callback function to each element of the array using.
    *
    * @param {Array} array - The array to be iterated over.
    * @param {Function|TypedFunction} callback - The function to execute on each element.
@@ -76,6 +82,10 @@ export function map (array, callback, isHomogeneous = false, callbackIsIndexed =
    * @param {Object} [thisArg] - The value of this provided for the call to the callback function.
    */
 export function forEach (array, callback, isHomogeneous = false, callbackIsIndexed = true, thisArg) {
+  if (!Array.isArray(array)) throw new TypeError('First argument to map must be an array')
+  if (typeof callback !== 'function') throw new TypeError('Second argument to map must be a function')
+  if (array.length === 0) return
+
   if (callbackIsIndexed) {
     thisArg = thisArg || array
     if (isHomogeneous) {
