3e881d268b2df9683f1ee3b916b3b0a2c8d85721
Orel Ben Neriah <orel503@gmail.com>
2025-02-25 14:58:51 -0800

    chore: remove commented out line

--- types/index.d.ts
+++ types/index.d.ts
@@ -1454,7 +1454,6 @@ export interface MathJsInstance extends MathJsFactory {
   multiply<T extends Matrix>(x: T, y: MathType): Matrix
   multiply<T extends Matrix>(x: MathType, y: T): Matrix
 
-  // multiply<T extends MathGeneric[][]>(x: T, y: T): T
   multiply<T extends MathArray>(x: T, y: T[]): T
   multiply<T extends MathArray>(x: T[], y: T): T
   multiply<T extends MathArray>(x: T[], y: T[]): T[]
