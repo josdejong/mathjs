a7682b08a36ce4e075083807be27f4c013f1922f
David Contreras <dvd.cnt@gmail.com>
2025-04-23 20:48:00 -0600

    Extended documentation for `parse`

--- src/expression/embeddedDocs/function/expression/parse.js
+++ src/expression/embeddedDocs/function/expression/parse.js
@@ -2,15 +2,22 @@ export const parseDocs = {
   name: 'parse',
   category: 'Expression',
   syntax: [
-    'node1 = parse(expression)',
-    'node1.compile().evaluate()'
+    'parse(expr)',
+    'parse(expr, options)',
+    'parse([expr1, expr2, expr3, ...])',
+    'parse([expr1, expr2, expr3, ...], options)'
   ],
-  description: 'Parse an expression. Returns a node tree, which can be evaluated by invoking node.evaluate().',
+  description: 'Parse an expression. Returns a node tree, which can be evaluated by invoking node.evaluate() or transformed into a functional object via node.compile().',
   examples: [
     'node1 = parse("sqrt(3^2 + 4^2)")',
-    'node1.compile().evaluate()',
+    'node1.evaluate()',
+    'code1 = node1.compile()',
+    'code1.evaluate()',
+    'scope = {a: 3, b: 4}',
     'node2 = parse("a * b")',
-    'node2.compile().evaluate({a:3, b:4})'
+    'node2.evaluate(scope)',
+    'code2 = node2.compile()',
+    'code2.evaluate(scope)'
   ],
   seealso: ['parser', 'evaluate', 'compile']
 }
