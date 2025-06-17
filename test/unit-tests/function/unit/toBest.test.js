f13947d144018105d4f60db49682dab87fa12112
Mundi93 <jacopojca@gmail.com>
2025-06-17 15:30:31 +0200

    fix: added a helper function to simplify the test readability

--- test/unit-tests/function/unit/toBest.test.js
+++ test/unit-tests/function/unit/toBest.test.js
@@ -3,58 +3,44 @@ import assert from 'assert'
 
 const Unit = math.create().Unit
 
+function assertUnit (actualUnit, expectedName, expectedPrefix, expectedValue) {
+  assert.equal(actualUnit.units[0].unit.name, expectedName)
+  assert.equal(actualUnit.units[0].prefix.name, expectedPrefix)
+  assert.equal(actualUnit.value, expectedValue)
+}
+
 describe('toBest', function () {
   it('should return the best unit without any parameters', function () {
-    assert.equal((new Unit(2 / 3, 'cm').toBest()).units[0].unit.name, 'm')
-    assert.equal((new Unit(2 / 3, 'cm').toBest()).units[0].prefix.name, 'c')
-    assert.equal((new Unit(2 / 3, 'cm').toBest()).value, 0.6666666666666666)
+    assertUnit(new Unit(2 / 3, 'cm').toBest(), 'm', 'c', 0.6666666666666666)
   })
 
   it('should format a unit without any value', function () {
-    assert.equal(new Unit(null, 'cm').toBest().units[0].unit.name, 'm')
-    assert.equal(new Unit(null, 'cm').toBest().units[0].prefix.name, 'c')
-    assert.equal(new Unit(null, 'cm').toBest().value, 0)
+    assertUnit(new Unit(null, 'cm').toBest(), 'm', 'c', 0)
   })
 
   it('should return the best unit with only given unit array - valorized and empty', function () {
     const unit1 = new Unit(10, 'm')
-    assert.equal(unit1.toBest(['km', 'mm', 'cm']).units[0].unit.name, 'm')
-    assert.equal(unit1.toBest(['km', 'mm', 'cm']).units[0].prefix.name, 'm')
-    assert.equal(unit1.toBest(['km', 'mm', 'cm']).value, 10000)
+    assertUnit(unit1.toBest(['km', 'mm', 'cm']), 'm', 'm', 10000)
 
     const unit2 = new Unit(5, 'm')
-
-    assert.equal(unit2.toBest(['cm', 'mm']).units[0].unit.name, 'm')
-    assert.equal(unit2.toBest(['cm', 'mm']).units[0].prefix.name, 'c')
-    assert.equal(unit2.toBest(['cm', 'mm']).value, 500)
+    assertUnit(unit2.toBest(['cm', 'mm']), 'm', 'c', 500)
   })
 
   it('should return the best unit with valueless unit as parameter', function () {
-    assert.equal(new Unit(1000, 'cm').toBest([new Unit(null, 'km')]).units[0].unit.name, 'm')
-    assert.equal(new Unit(1000, 'cm').toBest([new Unit(null, 'km')]).units[0].prefix.name, 'k')
-    assert.equal(new Unit(1000, 'cm').toBest([new Unit(null, 'km')]).value, 0.01)
+    assertUnit(new Unit(1000, 'cm').toBest([new Unit(null, 'km')]), 'm', 'k', 0.01)
   })
 
   it('should return the best unit with given array and offset', function () {
-    assert.equal(new Unit(10, 'm').toBest(['mm', 'km'], { offset: 1.5 }).units[0].unit.name, 'm')
-    assert.equal(new Unit(10, 'm').toBest(['mm', 'km'], { offset: 1.5 }).units[0].prefix.name, 'm')
-    assert.equal(new Unit(10, 'm').toBest(['mm', 'km'], { offset: 1.5 }).value, 10000)
+    assertUnit(new Unit(10, 'm').toBest(['mm', 'km'], { offset: 1.5 }), 'm', 'm', 10000)
   })
 
   it('should handle negative values correctly', function () {
-    assert.equal(new Unit(-1000, 'cm').toBest().units[0].unit.name, 'm')
-    assert.equal(new Unit(-1000, 'cm').toBest().units[0].prefix.name, 'c')
-    assert.equal(new Unit(-1000, 'cm').toBest().value, -1000)
+    assertUnit(new Unit(-1000, 'cm').toBest(), 'm', 'c', -1000)
   })
 
   it('should handle zero values correctly', function () {
-    assert.equal(new Unit(0, 'km').toBest().units[0].unit.name, 'm')
-    assert.equal(new Unit(0, 'km').toBest().units[0].prefix.name, 'k')
-    assert.equal(new Unit(0, 'km').toBest().value, 0)
-
-    assert.equal(new Unit(0, 'cm').toBest(['km', 'm', 'cm', 'mm']).units[0].unit.name, 'm')
-    assert.equal(new Unit(0, 'cm').toBest(['km', 'm', 'cm', 'mm']).units[0].prefix.name, 'k')
-    assert.equal(new Unit(0, 'cm').toBest(['km', 'm', 'cm', 'mm']).value, 0)
+    assertUnit(new Unit(0, 'km').toBest(), 'm', 'k', 0)
+    assertUnit(new Unit(0, 'cm').toBest(['km', 'm', 'cm', 'mm']), 'm', 'k', 0)
   })
 
   it('should throw error for first parameter not being an array', function () {
