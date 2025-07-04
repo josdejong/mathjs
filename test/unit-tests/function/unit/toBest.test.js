ef8182337219ce0a9dc016c12f3f869e8ece408a
Elia Alesiani <elia.alesiani@gmail.com>
2025-07-04 18:11:52 +0200

    changed order of params of assertUnit

--- test/unit-tests/function/unit/toBest.test.js
+++ test/unit-tests/function/unit/toBest.test.js
@@ -3,45 +3,45 @@ import assert from 'assert'
 
 const Unit = math.create().Unit
 
-function assertUnit (actualUnit, expectedName, expectedPrefix, expectedValue) {
-  assert.equal(actualUnit.units[0].unit.name, expectedName)
-  assert.equal(actualUnit.units[0].prefix.name, expectedPrefix)
-  assert.equal(actualUnit.units.length, 1)
+function assertUnit (actualUnit, expectedValue, expectedPrefix, expectedName) {
   assert.equal(actualUnit.value, expectedValue)
+  assert.equal(actualUnit.units[0].prefix.name, expectedPrefix)
+  assert.equal(actualUnit.units[0].unit.name, expectedName)
+  assert.equal(actualUnit.units.length, 1)
 }
 
 describe('toBest', function () {
   it('should return the best unit without any parameters', function () {
-    assertUnit(new Unit(2 / 3, 'cm').toBest(), 'm', 'c', 0.6666666666666666)
+    assertUnit(new Unit(2 / 3, 'cm').toBest(), 0.6666666666666666, 'c', 'm')
   })
 
   it('should format a unit without any value', function () {
-    assertUnit(new Unit(null, 'cm').toBest(), 'm', 'c', 0)
+    assertUnit(new Unit(null, 'cm').toBest(), 0, 'c', 'm')
   })
 
   it('should return the best unit with only given unit array - valorized and empty', function () {
     const unit1 = new Unit(10, 'm')
-    assertUnit(unit1.toBest(['km', 'mm', 'cm']), 'm', 'm', 10000)
+    assertUnit(unit1.toBest(['km', 'mm', 'cm']), 10000, 'm', 'm')
 
     const unit2 = new Unit(5, 'm')
-    assertUnit(unit2.toBest(['cm', 'mm']), 'm', 'c', 500)
+    assertUnit(unit2.toBest(['cm', 'mm']), 500, 'c', 'm')
   })
 
   it('should return the best unit with valueless unit as parameter', function () {
-    assertUnit(new Unit(1000, 'cm').toBest([new Unit(null, 'km')]), 'm', 'k', 0.01)
+    assertUnit(new Unit(1000, 'cm').toBest([new Unit(null, 'km')]), 0.01, 'k', 'm')
   })
 
   it('should return the best unit with given array and offset', function () {
-    assertUnit(new Unit(10, 'm').toBest(['mm', 'km'], { offset: 1.5 }), 'm', 'm', 10000)
+    assertUnit(new Unit(10, 'm').toBest(['mm', 'km'], { offset: 1.5 }), 10000, 'm', 'm')
   })
 
   it('should handle negative values correctly', function () {
-    assertUnit(new Unit(-1000, 'cm').toBest(), 'm', 'c', -1000)
+    assertUnit(new Unit(-1000, 'cm').toBest(), -1000, 'c', 'm')
   })
 
   it('should handle zero values correctly', function () {
-    assertUnit(new Unit(0, 'km').toBest(), 'm', 'k', 0)
-    assertUnit(new Unit(0, 'cm').toBest(['km', 'm', 'cm', 'mm']), 'm', 'k', 0)
+    assertUnit(new Unit(0, 'km').toBest(), 0, 'k', 'm')
+    assertUnit(new Unit(0, 'cm').toBest(['km', 'm', 'cm', 'mm']), 0, 'k', 'm')
   })
 
   it('should throw error for first parameter not being an array', function () {
