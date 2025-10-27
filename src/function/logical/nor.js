946c22b1f4019f04a4b93739a8e453e6177319aa
Kryonn <kryojin@outlook.com>
2025-10-27 20:48:13 -0300

    code: final update nor

--- src/function/logical/nor.js
+++ src/function/logical/nor.js
@@ -1,6 +1,6 @@
 import { createMatAlgo03xDSf } from '../../type/matrix/utils/matAlgo03xDSf.js'
 import { createMatAlgo12xSfs } from '../../type/matrix/utils/matAlgo12xSfs.js'
-import { createMatAlgo05xSfSf } from '../../type/matrix/utils/matAlgo05xSfSf.js'
+import { createMatAlgo07xSSf } from '../../type/matrix/utils/matAlgo07xSSf.js'
 import { factory } from '../../utils/factory.js'
 import { createMatrixAlgorithmSuite } from '../../type/matrix/utils/matrixAlgorithmSuite.js'
 import { norNumber } from '../../plain/number/logical.js'
@@ -16,37 +16,37 @@ const dependencies = [
 
 export const createNor = /* #__PURE__ */ factory(name, dependencies, ({ typed, matrix, equalScalar, DenseMatrix, concat }) => {
   const matAlgo03xDSf = createMatAlgo03xDSf({ typed })
-  const matAlgo05xSfSf = createMatAlgo05xSfSf({ typed, equalScalar })
+  const matAlgo07xSSf = createMatAlgo07xSSf({ typed, equalScalar })
   const matAlgo12xSfs = createMatAlgo12xSfs({ typed, DenseMatrix })
   const matrixAlgorithmSuite = createMatrixAlgorithmSuite({ typed, matrix, concat })
 
   /**
-   * Logical `or`. Test if at least one value is defined with a nonzero/nonempty value.
+   * Logical `nor`. Test if both os values are defined with zero.
    * For matrices, the function is evaluated element wise.
    *
    * Syntax:
    *
-   *    math.or(x, y)
+   *    math.nor(x, y)
    *
    * Examples:
    *
-   *    math.or(2, 4)   // returns true
+   *    math.nor(2, 4)   // returns false
    *
    *    a = [2, 5, 0]
    *    b = [0, 22, 0]
    *    c = 0
    *
-   *    math.or(a, b)   // returns [true, true, false]
-   *    math.or(b, c)   // returns [false, true, false]
+   *    math.or(a, b)   // returns [false, false, true]
+   *    math.or(b, c)   // returns [true, false, true]
    *
    * See also:
    *
-   *    and, not, xor
+   *    and,nand, or, not, xor
    *
    * @param  {number | BigNumber | bigint | Complex | Unit | Array | Matrix} x First value to check
    * @param  {number | BigNumber | bigint | Complex | Unit | Array | Matrix} y Second value to check
    * @return {boolean | Array | Matrix}
-   *            Returns true when one of the inputs is defined with a nonzero/nonempty value.
+   *            Returns true when both inputs are zero
    */
   return typed(
     name,
@@ -67,7 +67,7 @@ export const createNor = /* #__PURE__ */ factory(name, dependencies, ({ typed, m
         (x, y) => self(x.value || 0, y.value || 0))
     },
     matrixAlgorithmSuite({
-      SS: matAlgo05xSfSf,
+      SS: matAlgo07xSSf,
       DS: matAlgo03xDSf,
       Ss: matAlgo12xSfs
     })
