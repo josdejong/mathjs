8578e5460fe04b790474782ebac38a1ca3b855b3
Sachit Dhiman <dhimansachit7@gmail.com>
2025-11-27 22:06:51 +0530

    fixing formatting

--- test/unit-tests/function/set/setDistinct.test.js
+++ test/unit-tests/function/set/setDistinct.test.js
@@ -11,37 +11,24 @@ describe('setDistinct', function () {
 
   it('should return the elements of a set in insertion order', function () {
     assert.deepStrictEqual(math.setDistinct([3, 1, 2, 1, 2]), [3, 1, 2])
-    assert.deepStrictEqual(math.setDistinct(['h', 'e', 'l', 'l', 'o']), [
-      'h',
-      'e',
-      'l',
-      'o'
-    ])
+    assert.deepStrictEqual(math.setDistinct(['h', 'e', 'l', 'l', 'o']), ['h', 'e', 'l', 'o'])
   })
 
   it('should return the distinct elements of a multiset', function () {
     assert.deepStrictEqual(math.setDistinct([1, 1, 2, 2]), [1, 2])
     assert.deepStrictEqual(math.setDistinct([1, 2, 1, 2]), [1, 2])
-    assert.deepStrictEqual(
-      math.setDistinct([1, 2, math.complex(3, 3), 2, math.complex(3, 3)]),
-      [1, 2, math.complex(3, 3)]
-    )
+    assert.deepStrictEqual(math.setDistinct([1, 2, math.complex(3, 3), 2, math.complex(3, 3)]), [math.complex(3, 3), 1, 2])
   })
 
   it('should return the same type of output as the inputs', function () {
     assert.strictEqual(math.typeOf(math.setDistinct([1, 2, 3])), 'Array')
     assert.strictEqual(
       math.typeOf(math.setDistinct(math.matrix([1, 2, 3]))),
-      'DenseMatrix'
-    )
+      'DenseMatrix')
   })
 
   it('should throw an error in case of invalid number of arguments', function () {
-    assert.throws(function () {
-      math.setDistinct()
-    }, /TypeError: Too few arguments/)
-    assert.throws(function () {
-      math.setDistinct([], [])
-    }, /TypeError: Too many arguments/)
+    assert.throws(function () { math.setDistinct() }, /TypeError: Too few arguments/)
+    assert.throws(function () { math.setDistinct([], []) }, /TypeError: Too many arguments/)
   })
 })
