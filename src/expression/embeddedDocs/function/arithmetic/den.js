af362d7a7b8080716887f3b44fb83d0cab773e85
Chibuike Anselm <chibuikeanselm@Chibuikes-MacBook-Pro.local>
2025-12-06 17:39:47 +0000

    deleted den files from arithmetic folder

--- src/expression/embeddedDocs/function/arithmetic/den.js
+++
@@ -1,8 +0,0 @@
-export const denDocs = {
-  name: 'den',
-  category: 'Arithmetic',
-  syntax: ['den(x)'],
-  description: 'Get the denominator of a fraction.',
-  examples: ['den(fraction(2, 3))', 'den(fraction(5, 8))'],
-  seealso: ['num', 'fraction']
-}
--- src/expression/embeddedDocs/function/arithmetic/num.js
+++
@@ -1,8 +0,0 @@
-export const numDocs = {
-  name: 'num',
-  category: 'Arithmetic',
-  syntax: ['num(x)'],
-  description: 'Get the numerator of a fraction.',
-  examples: ['num(fraction(2, 3))', 'num(fraction(5, 8))'],
-  seealso: ['den', 'fraction']
-}
