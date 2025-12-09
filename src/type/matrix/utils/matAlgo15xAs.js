c9d57a11f29ce7186b56d5902406ba82235c88d3
David Contreras <dvd.cnt@gmail.com>
2025-12-08 21:20:50 -0600

    Included an algorithm for Array and scalars

---
+++ src/type/matrix/utils/matAlgo15xAs.js
@@ -0,0 +1,26 @@
+import { factory } from '../../../utils/factory.js'
+import { deepMap as map } from '../../../utils/array.js'
+
+const name = 'matAlgo15xAs'
+const dependencies = []
+
+export const createMatAlgo15xAs = /* #__PURE__ */ factory(name, dependencies, () => {
+  /**
+   * Iterates over Array items and invokes the callback function f(Aij..z, b).
+   * Callback function invoked MxN times.
+   *
+   * C(i,j,...z) = f(Aij..z, b)
+   *
+   * @param {Array}    a                 The Array instance (A)
+   * @param {Scalar}   b                 The Scalar value
+   * @param {Function} cf                The f(Aij..z,b) operation to invoke
+   * @param {boolean}  inverse           A true value indicates callback should be invoked f(b,Aij..z)
+   *
+   * @return {Array}                    Array (C)
+   *
+   * https://github.com/josdejong/mathjs/pull/346#issuecomment-97659042
+   */
+  return function matAlgo14xDs (a, b, cf, inverse) {
+    return inverse ? map(a, v => cf(b, v)) : map(a, v => cf(v, b))
+  }
+})
