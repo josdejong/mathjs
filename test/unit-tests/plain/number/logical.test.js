61f8c45859091eed496afbaf41beefed273624f5
Glen Whitney <glen@studioinfinity.org>
2025-12-09 09:22:11 -0800

    test: include nand in test/unit-tests/plain/number/logical

--- test/unit-tests/plain/number/logical.test.js
+++ test/unit-tests/plain/number/logical.test.js
@@ -1,17 +1,30 @@
 import assert from 'assert'
-import { norNumber } from '../../../../src/plain/number/logical.js'
+import { nandNumber, norNumber } from '../../../../src/plain/number/logical.js'
 
-describe('nor', function () {
-  it('should return true when both are 0', function () {
+describe('nor/nand', function () {
+  it('nor should return true when both are 0', function () {
     assert.strictEqual(norNumber(0, 0), true)
   })
 
-  it('should return false when one is 0 and the other is 1', function () {
+  it('nor should return false when one is 0 and the other is 1', function () {
     assert.strictEqual(norNumber(1, 0), false)
     assert.strictEqual(norNumber(0, 1), false)
   })
 
-  it('should return false when both are 1', function () {
+  it('nor should return false when both are 1', function () {
     assert.strictEqual(norNumber(1, 1), false)
+  })
+
+  it('nand should return true when both are 0', function () {
+    assert.strictEqual(nandNumber(0, 0), true)
+  })
+
+  it('nand should return true when one is 0 and the other is 1', function () {
+    assert.strictEqual(nandNumber(1, 0), true)
+    assert.strictEqual(nandNumber(0, 1), true)
+  })
+
+  it('nand should return false when both are 1', function () {
+    assert.strictEqual(nandNumber(1, 1), false)
   })
 })
