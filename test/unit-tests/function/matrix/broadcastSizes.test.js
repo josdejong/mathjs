43156517dc47ccb795153e4f21dedb1da5f155c3
David Contreras <dvd.cnt@gmail.com>
2025-12-28 13:40:25 -0600

    Added test for broadcastTo

--- test/unit-tests/function/matrix/broadcastSizes.test.js
+++ test/unit-tests/function/matrix/broadcastSizes.test.js
@@ -2,23 +2,26 @@ import assert from 'assert'
 import math from '../../../../src/defaultInstance.js'
 
 describe('broadcastSizes', function () {
+  const broadcastSizes = math.broadcastSizes
+  const matrix = math.matrix
+
   it('should broadcast sizes', function () {
-    assert.deepStrictEqual(math.broadcastSizes([2, 3]), [2, 3])
-    assert.deepStrictEqual(math.broadcastSizes([3, 3], [3, 1]), [3, 3])
-    assert.deepStrictEqual(math.broadcastSizes([2, 1], [1, 3]), [2, 3])
-    assert.deepStrictEqual(math.broadcastSizes([5, 4, 3], [1, 4, 1]), [5, 4, 3])
-    assert.deepStrictEqual(math.broadcastSizes([3], [2, 3]), [2, 3])
-    assert.deepStrictEqual(math.broadcastSizes([1, 3], [2, 1]), [2, 3])
+    assert.deepStrictEqual(broadcastSizes([2, 3]), [2, 3])
+    assert.deepStrictEqual(broadcastSizes([3, 3], [3, 1]), [3, 3])
+    assert.deepStrictEqual(broadcastSizes([2, 1], [1, 3]), [2, 3])
+    assert.deepStrictEqual(broadcastSizes([5, 4, 3], [1, 4, 1]), [5, 4, 3])
+    assert.deepStrictEqual(broadcastSizes([3], [2, 3]), [2, 3])
+    assert.deepStrictEqual(broadcastSizes([1, 3], [2, 1]), [2, 3])
   })
 
   it('should throw an error if sizes are not compatible', function () {
-    assert.throws(function () { math.broadcastSizes([2, 3], [3, 2]) }, /Error: shape mismatch: /)
-    assert.throws(function () { math.broadcastSizes([2, 3], [2, 3, 4]) }, /Error: shape mismatch: /)
+    assert.throws(function () { broadcastSizes([2, 3], [3, 2]) }, /Error: shape mismatch: /)
+    assert.throws(function () { broadcastSizes([2, 3], [2, 3, 4]) }, /Error: shape mismatch: /)
   })
 
   it('should broadcast sizes of mixed arrays and matrices', function () {
-    assert.deepStrictEqual(math.broadcastSizes([3, 3], math.matrix([3, 1])), [3, 3])
-    assert.deepStrictEqual(math.broadcastSizes(math.matrix([2, 1]), [1, 3]), [2, 3])
-    assert.deepStrictEqual(math.broadcastSizes(math.matrix([5, 4, 3]), math.matrix([1, 4, 1])), [5, 4, 3])
+    assert.deepStrictEqual(broadcastSizes([3, 3], matrix([3, 1])), [3, 3])
+    assert.deepStrictEqual(broadcastSizes(matrix([2, 1]), [1, 3]), [2, 3])
+    assert.deepStrictEqual(broadcastSizes(matrix([5, 4, 3]), matrix([1, 4, 1])), [5, 4, 3])
   })
 })
