c99c48a8fd35f0895cdb61d3a891148428c86a0b
David Contreras <dvd.cnt@gmail.com>
2025-12-28 14:00:50 -0600

    Added more tests to broadcastTo

--- test/unit-tests/function/matrix/broadcastTo.test.js
+++ test/unit-tests/function/matrix/broadcastTo.test.js
@@ -15,6 +15,13 @@ describe('broadcastTo', function () {
     assert.deepStrictEqual(broadcastTo(matrix([2, 3]), [2, 2]), matrix([[2, 3], [2, 3]]))
   })
 
+  it('should broadcast mixed arrays and matrices to a given size', function () {
+    assert.deepStrictEqual(broadcastTo([1, 2, 3], matrix([2, 3])), [[1, 2, 3], [1, 2, 3]])
+    assert.deepStrictEqual(broadcastTo(matrix([2, 3]), [2, 2]), matrix([[2, 3], [2, 3]]))
+    assert.deepStrictEqual(broadcastTo(matrix([1, 2, 3]), matrix([2, 3])), matrix([[1, 2, 3], [1, 2, 3]]))
+    assert.deepStrictEqual(broadcastTo([2, 3], matrix([2, 2])), [[2, 3], [2, 3]])
+  })
+
   it('should throw an error if sizes are not compatible', function () {
     assert.throws(function () { broadcastTo([1, 2], [2, 3]) }, /Error: shape mismatch: /)
     assert.throws(function () { broadcastTo(matrix([1, 2]), [2, 3]) }, /Error: shape mismatch: /)
