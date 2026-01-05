7e4baa1c6ea571261dfc6355502768fd113ec33e
Anadian <willanad@yandex.com>
2026-01-05 02:50:06 -0500

    docs(src/function/relational/*): Fixed invalid documentation comments in `equal.js`, `deepEqual.js`, `unequal.js`, and `compareNatural.js`.

--- src/function/relational/compareNatural.js
+++ src/function/relational/compareNatural.js
@@ -52,8 +52,7 @@ export const createCompareNatural = /* #__PURE__ */ factory(name, dependencies, 
    *
    *    math.compareNatural('Answer: 10', 'Answer: 2') // returns 1
    *    math.compareText('Answer: 10', 'Answer: 2')    // returns -1
-   *    math.compare('Answer: 10', 'Answer: 2')
-   *        // Error: Cannot convert "Answer: 10" to a number
+   *    math.compare('Answer: 10', 'Answer: 2')	       // Error: Cannot convert "Answer: 10" to a number
    *
    *    const a = math.unit('5 cm')
    *    const b = math.unit('40 mm')
@@ -73,6 +72,10 @@ export const createCompareNatural = /* #__PURE__ */ factory(name, dependencies, 
    * See also:
    *
    *    compare, compareText
+   *
+   * History:
+   *
+   *     v15.1.1   Fixed invalid documentation comment.
    *
    * @param  {*} x First value to compare
    * @param  {*} y Second value to compare
