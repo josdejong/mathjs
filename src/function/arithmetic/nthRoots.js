31e69099d625e614e33758598c7c384a3fad001e
Glen Whitney <glen@studioinfinity.org>
2026-01-08 14:03:32 -0500

    doc: Correct several documentation examples and add History  (#3630)
    Corrects the documentation examples and adds History for arithemetic functions
     `add`, `cbrt`, `ceil`, `exp`, `expm1`, `fix`, `floor`, `mod`, `nthRoot`, and `nthRoots`;
    for relational functions `compareNatural`, `deepEqual`, `equal`, and `unequal`; and
    for utility function `isFinite`.
    
    Also adds `...` continuation notation for doc example tests and allows Complex 
    arrays and matrices as expected values. 
    ---------
    Co-authored-by: Anadian <willanad@yandex.com>

--- src/function/arithmetic/nthRoots.js
+++ src/function/arithmetic/nthRoots.js
@@ -76,21 +76,18 @@ export const createNthRoots = /* #__PURE__ */ factory(name, dependencies, ({ typ
    *
    * Examples:
    *
-   *    math.nthRoots(1)
-   *    // returns [
-   *    //   {re: 1, im: 0},
-   *    //   {re: -1, im: 0}
-   *    // ]
+   *    math.nthRoots(1) // returns [ {re: 1, im: 0}, {re: -1, im: 0} ]
+   *
    *    math.nthRoots(1, 3)
-   *    // returns [
-   *    //   { re: 1, im: 0 },
-   *    //   { re: -0.4999999999999998, im: 0.8660254037844387 },
-   *    //   { re: -0.5000000000000004, im: -0.8660254037844385 }
-   *    // ]
+   *    // Complex [1, "-0.5+0.866025403784439i", "-0.5-0.866025403784439i"]
    *
    * See also:
    *
    *    nthRoot, pow, sqrt
+   *
+   * History:
+   *
+   *    v1.1   Created
    *
    * @param {number | BigNumber | Fraction | Complex} x Number to be rounded
    * @param {number} [root=2] Optional root, default value is 2
