2518715b35d28b74a73e4d5c744aaf052ff5f5d8
Jos de Jong <wjosdejong@gmail.com>
2026-02-06 13:02:30 +0100

    chore: rename functions in the benchmark for more clarity

--- test/benchmark/expression_parser_functions.js
+++ test/benchmark/expression_parser_functions.js
@@ -8,22 +8,22 @@ import { formatTaskResult } from './utils/formatTaskResult.js'
 const math = create(all)
 
 const cubeJsPlain = (x) => x * x * x
-const cubeJsWithMathjs = (x) => math.multiply(x, math.multiply(x, x))
-const cubeMathjsExpr = math.evaluate('f(x) = x * x * x')
+const cubeJsWithMathjsFn = (x) => math.multiply(x, math.multiply(x, x))
+const cubeMathjsCustomFn = math.evaluate('f(x) = x * x * x')
 
 assert.strictEqual(cubeJsPlain(3), 27)
-assert.strictEqual(cubeJsWithMathjs(3), 27)
-assert.strictEqual(cubeMathjsExpr(3), 27)
+assert.strictEqual(cubeJsWithMathjsFn(3), 27)
+assert.strictEqual(cubeMathjsCustomFn(3), 27)
 
 const bench = new Bench({ time: 100, iterations: 1000 })
   .add('cubeJsPlain', function () {
     cubeJsPlain(3)
   })
-  .add('cubeJsWithMathjs', function () {
-    cubeJsWithMathjs(3)
+  .add('cubeJsWithMathjsFn', function () {
+    cubeJsWithMathjsFn(3)
   })
-  .add('cubeMathjsExpr', function () {
-    cubeMathjsExpr(3)
+  .add('cubeMathjsCustomFn', function () {
+    cubeMathjsCustomFn(3)
   })
 
 bench.addEventListener('cycle', (event) => console.log(formatTaskResult(bench, event.task, 3)))
