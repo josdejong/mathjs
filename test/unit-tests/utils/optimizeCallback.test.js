88e1aa79f1ac02ed062faf77334a7f4defa21215
David Contreras <dvd.cnt@gmail.com>
2026-03-21 03:27:26 +0000

    added more tests to `optimizeCallback`

--- test/unit-tests/utils/optimizeCallback.test.js
+++ test/unit-tests/utils/optimizeCallback.test.js
@@ -18,6 +18,8 @@ describe('optimizeCallback', function () {
   })
 
   const arrayOfNumbers = [1, 2, 3]
+  const matrixOfNumbers = math.matrix(arrayOfNumbers)
+  const sparseMatrixOfNumbers = math.matrix(arrayOfNumbers, 'sparse')
   const name = 'myFunction'
 
   it('should find the right number of arguments for a function', function () {
@@ -25,6 +27,14 @@ describe('optimizeCallback', function () {
     assert.strictEqual(optimizedUnary.isUnary, true)
     const optimizedTernary = optimizeCallback(ternaryCallback, arrayOfNumbers, name, false)
     assert.strictEqual(optimizedTernary.isUnary, false)
+    const optimizedUnaryMatrix = optimizeCallback(unaryCallback, matrixOfNumbers, name, false)
+    assert.strictEqual(optimizedUnaryMatrix.isUnary, true)
+    const optimizedTernaryMatrix = optimizeCallback(ternaryCallback, matrixOfNumbers, name, false)
+    assert.strictEqual(optimizedTernaryMatrix.isUnary, false)
+    const optimizedUnarySparseMatrix = optimizeCallback(unaryCallback, sparseMatrixOfNumbers, name, false)
+    assert.strictEqual(optimizedUnarySparseMatrix.isUnary, true)
+    const optimizedTernarySparseMatrix = optimizeCallback(ternaryCallback, sparseMatrixOfNumbers, name, false)
+    assert.strictEqual(optimizedTernarySparseMatrix.isUnary, false)
   })
 
   it('should find the right number of arguments for a typed function', function () {
@@ -32,6 +42,14 @@ describe('optimizeCallback', function () {
     assert.strictEqual(optimizedTypedUnary.isUnary, true)
     const optimizedTypedTernary = optimizeCallback(typedTernaryCallback, arrayOfNumbers, name, false)
     assert.strictEqual(optimizedTypedTernary.isUnary, false)
+    const optimizedTypedUnaryMatrix = optimizeCallback(typedUnaryCallback, matrixOfNumbers, name, false)
+    assert.strictEqual(optimizedTypedUnaryMatrix.isUnary, true)
+    const optimizedTypedTernaryMatrix = optimizeCallback(typedTernaryCallback, matrixOfNumbers, name, false)
+    assert.strictEqual(optimizedTypedTernaryMatrix.isUnary, false)
+    const optimizedTypedUnarySparseMatrix = optimizeCallback(typedUnaryCallback, sparseMatrixOfNumbers, name, false)
+    assert.strictEqual(optimizedTypedUnarySparseMatrix.isUnary, true)
+    const optimizedTypedTernarySparseMatrix = optimizeCallback(typedTernaryCallback, sparseMatrixOfNumbers, name, false)
+    assert.strictEqual(optimizedTypedTernarySparseMatrix.isUnary, false)
   })
 
   it('should return unary functions as unary when isUnary is set to true', function () {
@@ -45,5 +63,28 @@ describe('optimizeCallback', function () {
     assert.strictEqual(optimizedTypedTernary.isUnary, true)
   })
   
+  it('should run the optimized callback', function () {
+    const optimizedUnary = optimizeCallback(unaryCallback, arrayOfNumbers, name, false)
+    assert.strictEqual(optimizedUnary.fn(1), 2)
+    const optimizedTernary = optimizeCallback(ternaryCallback, arrayOfNumbers, name, false)
+    assert.strictEqual(optimizedTernary.fn(1, 2, 3), 7)
+    const optimizedTypedUnary = optimizeCallback(typedUnaryCallback, arrayOfNumbers, name, false)
+    assert.strictEqual(optimizedTypedUnary.fn(1), 2)
+    const optimizedTypedTernary = optimizeCallback(typedTernaryCallback, arrayOfNumbers, name, false)
+    assert.strictEqual(optimizedTypedTernary.fn(1, 2, 3), 7)
+    const optimizedUnaryMatrix = optimizeCallback(unaryCallback, matrixOfNumbers, name, false)
+    assert.strictEqual(optimizedUnaryMatrix.fn(1), 2)
+    const optimizedTernaryMatrix = optimizeCallback(ternaryCallback, matrixOfNumbers, name, false)
+    assert.strictEqual(optimizedTernaryMatrix.fn(1, 2, 3), 7)
+    const optimizedTypedUnaryMatrix = optimizeCallback(typedUnaryCallback, matrixOfNumbers, name, false)
+    assert.strictEqual(optimizedTypedUnaryMatrix.fn(1), 2)
+    const optimizedTypedTernaryMatrix = optimizeCallback(typedTernaryCallback, matrixOfNumbers, name, false)
+    assert.strictEqual(optimizedTypedTernaryMatrix.fn(1, 2, 3), 7)
+    const optimizedUnarySparseMatrix = optimizeCallback(unaryCallback, sparseMatrixOfNumbers, name, false)
+    assert.strictEqual(optimizedUnarySparseMatrix.fn(1), 2)
+    const optimizedTernarySparseMatrix = optimizeCallback(ternaryCallback, sparseMatrixOfNumbers, name, false)
+    assert.strictEqual(optimizedTernarySparseMatrix.fn(1, 2, 3), 7)
+})
+
 }
 )
