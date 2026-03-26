aee79cd745441053648f92f2bb59af925e663331
Andrei Alecu <andreialecu@users.noreply.github.com>
2026-03-26 19:19:08 +0200

    Replace tiny-emitter package with inline ESM implementation

--- src/utils/emitter.js
+++ src/utils/emitter.js
@@ -1,4 +1,5 @@
-import Emitter from 'tiny-emitter'
+// Inline ESM replacement for the 'tiny-emitter' package (v2.1.0)
+// Original: https://github.com/scottcorgan/tiny-emitter (MIT license)
 
 /**
  * Extend given object with emitter functions `on`, `off`, `once`, `emit`
@@ -6,14 +7,44 @@ import Emitter from 'tiny-emitter'
  * @return {Object} obj
  */
 export function mixin (obj) {
-  // create event emitter
-  const emitter = new Emitter()
+  const events = {}
 
-  // bind methods to obj (we don't want to expose the emitter.e Array...)
-  obj.on = emitter.on.bind(emitter)
-  obj.off = emitter.off.bind(emitter)
-  obj.once = emitter.once.bind(emitter)
-  obj.emit = emitter.emit.bind(emitter)
+  obj.on = function (name, callback, ctx) {
+    (events[name] || (events[name] = [])).push({ fn: callback, ctx })
+    return obj
+  }
+
+  obj.off = function (name, callback) {
+    if (!callback) {
+      delete events[name]
+      return obj
+    }
+
+    const listeners = events[name]
+    if (listeners) {
+      const live = listeners.filter(e => e.fn !== callback && e.fn._ !== callback)
+      live.length ? (events[name] = live) : delete events[name]
+    }
+
+    return obj
+  }
+
+  obj.once = function (name, callback, ctx) {
+    function listener (...args) {
+      obj.off(name, listener)
+      callback.apply(ctx, args)
+    }
+    listener._ = callback
+    return obj.on(name, listener, ctx)
+  }
+
+  obj.emit = function (name, ...args) {
+    const listeners = (events[name] || []).slice()
+    for (let i = 0; i < listeners.length; i++) {
+      listeners[i].fn.apply(listeners[i].ctx, args)
+    }
+    return obj
+  }
 
   return obj
 }
