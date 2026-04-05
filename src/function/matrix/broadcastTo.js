3a19f481b22741534ed74c147f462621bdf92742
David Contreras <dvd.cnt@gmail.com>
2026-04-05 12:34:09 -0600

    Format

--- src/function/matrix/broadcastTo.js
+++ src/function/matrix/broadcastTo.js
@@ -7,7 +7,7 @@ const dependencies = ['typed']
 export const createBroadcastTo = /* #__PURE__ */ factory(name, dependencies, ({ typed }) => {
   /**
      * Broadcast an array to a specified size.
-     * The broadcasting rules can be found in [Matrices#Broadcasting](./datatype/matrices#Broadcasting).   
+     * The broadcasting rules can be found in [Matrices#Broadcasting](./datatype/matrices#Broadcasting).
   *
      * Syntax:
      *
